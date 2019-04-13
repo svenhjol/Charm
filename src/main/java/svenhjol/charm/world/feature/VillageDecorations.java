4bba154fa4ad65de523d7674d8f949b31264c666
Sven <sven.hjol@protonmail.com>
2019-04-13 16:27:58 +0000

    Slightly higher chance of crops and lights by default

--- src/main/java/svenhjol/charm/world/feature/VillageDecorations.java
+++ src/main/java/svenhjol/charm/world/feature/VillageDecorations.java
@@ -122,7 +122,7 @@ public class VillageDecorations extends Feature
         cropsChance = propDouble(
                 "Crops outside",
                 "Chance (out of 1.0) of a village having tilled soil, crops and water wells within its boundary.",
-                0.75
+                0.8
         );
         flowersChance = propDouble(
                 "Flowers outside",
@@ -132,7 +132,7 @@ public class VillageDecorations extends Feature
         lightsChance = propDouble(
                 "Lights outside",
                 "Chance (out of 1.0) of a village having torches and lanterns scattered within its boundary.",
-                0.75
+                0.8
         );
         mobsChance = propDouble(
                 "Mobs outside",
