ccfae3ccb84db1ce9145b83d763aae89c15dfa67
Sven <sven.hjol@protonmail.com>
2019-05-03 08:48:13 +0000

    Disable enchantments on Totem of Shielding

--- src/main/java/svenhjol/charm/loot/item/ItemTotemOfShielding.java
+++ src/main/java/svenhjol/charm/loot/item/ItemTotemOfShielding.java
@@ -2,6 +2,7 @@ package svenhjol.charm.loot.item;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import svenhjol.charm.Charm;
 import svenhjol.charm.loot.feature.TotemOfShielding;
 import svenhjol.meson.IMesonItem;
@@ -20,5 +21,11 @@ public class ItemTotemOfShielding extends Item implements IMesonItem
     public String getModId()
     {
         return Charm.MOD_ID;
+    }
+
+    @Override
+    public boolean isEnchantable(ItemStack stack)
+    {
+        return false;
     }
 }
