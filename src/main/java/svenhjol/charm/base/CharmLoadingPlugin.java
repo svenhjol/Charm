7509d9f8e1f3f32ad99fe9ed64d7c5e4b243008c
Sven <sven.hjol@protonmail.com>
2019-05-11 02:06:14 +0000

    Injecting charm config file into CharmLoadingPlugin. Allows transformers to read config to prevent unneeded ASM.

--- src/main/java/svenhjol/charm/base/CharmLoadingPlugin.java
+++ src/main/java/svenhjol/charm/base/CharmLoadingPlugin.java
@@ -1,12 +1,26 @@
 package svenhjol.charm.base;
 
+import net.minecraftforge.common.config.Configuration;
 import svenhjol.meson.asm.MesonLoadingPlugin;
+
+import java.io.File;
+import java.util.Map;
 
 public class CharmLoadingPlugin extends MesonLoadingPlugin
 {
+    public static Configuration config;
+
     @Override
     public String[] getASMTransformerClass()
     {
-        return new String[]{ "svenhjol.charm.base.ClassTransformer" };
+        return new String[]{ "svenhjol.charm.base.CharmClassTransformer" };
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+        super.injectData(data);
+
+        File configFile = new File(((File) data.get("mcLocation")).getPath() + "/config/charm.cfg");
+        config = new Configuration(configFile);
     }
 }
