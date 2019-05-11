a1182a01c04a6846650e16dc2efde0833ea4f0b0
Sven <sven.hjol@protonmail.com>
2019-05-11 01:59:04 +0000

    Meson: config loader check for empty comment before overwriting. Meson: added method to check transformers are enabled. Meson: moved config change check and saving to saveChanges().

--- src/main/java/svenhjol/meson/helper/ConfigHelper.java
+++ src/main/java/svenhjol/meson/helper/ConfigHelper.java
@@ -9,35 +9,35 @@ public class ConfigHelper
     public static int propInt(Configuration config, String name, String category, String comment, int def)
     {
         Property prop = config.get(category, name, def);
-        prop.setComment(comment);
+        if (!comment.isEmpty()) prop.setComment(comment);
         return prop.getInt(def);
     }
 
     public static double propDouble(Configuration config, String name, String category, String comment, double def)
     {
         Property prop = config.get(category, name, def);
-        prop.setComment(comment);
+        if (!comment.isEmpty()) prop.setComment(comment);
         return prop.getDouble(def);
     }
 
     public static boolean propBoolean(Configuration config, String name, String category, String comment, boolean def)
     {
         Property prop = config.get(category, name, def);
-        prop.setComment(comment);
+        if (!comment.isEmpty()) prop.setComment(comment);
         return prop.getBoolean(def);
     }
 
     public static String propString(Configuration config, String name, String category, String comment, String def)
     {
         Property prop = config.get(category, name, def);
-        prop.setComment(comment);
+        if (!comment.isEmpty()) prop.setComment(comment);
         return prop.getString();
     }
 
     public static String[] propStringList(Configuration config, String name, String category, String comment, String[] def)
     {
         Property prop = config.get(category, name, def);
-        prop.setComment(comment);
+        if (!comment.isEmpty()) prop.setComment(comment);
         return prop.getStringList();
     }
 
@@ -48,5 +48,21 @@ public class ConfigHelper
             modsLoaded = modsLoaded && Loader.isModLoaded(mod);
         }
         return modsLoaded;
+    }
+
+    public static boolean checkTransformers(Configuration config, String ...transformers)
+    {
+        boolean loaded = true;
+        for (String transformer : transformers) {
+            loaded = loaded && propBoolean(config, transformer, "Transformers", "", true);
+        }
+        return loaded;
+    }
+
+    public static void saveChanges(Configuration config)
+    {
+        if (config.hasChanged()) {
+            config.save();
+        }
     }
 }
