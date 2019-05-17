c224d58d8f5d0fc72289a25aa16d289d5c1e11c8
Sven <sven.hjol@protonmail.com>
2019-05-17 23:01:03 +0000

    Moved interfaces to own folder

--- src/main/java/svenhjol/meson/MesonItem.java
+++ src/main/java/svenhjol/meson/MesonItem.java
@@ -1,6 +1,7 @@
 package svenhjol.meson;
 
 import net.minecraft.item.Item;
+import svenhjol.meson.iface.IMesonItem;
 
 @SuppressWarnings("unused")
 public abstract class MesonItem extends Item implements IMesonItem
