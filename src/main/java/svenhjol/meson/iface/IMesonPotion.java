7e6ca3e7cce6cc19fc2e580e109b38a4e767bf4f
Sven <sven.hjol@protonmail.com>
2019-05-17 23:06:26 +0000

    Create potion interface, moved default stuff out of base potion

---
+++ src/main/java/svenhjol/meson/iface/IMesonPotion.java
@@ -0,0 +1,20 @@
+package svenhjol.meson.iface;
+
+import net.minecraft.potion.Potion;
+import net.minecraft.util.ResourceLocation;
+import svenhjol.meson.ProxyRegistry;
+
+public interface IMesonPotion
+{
+    default void register(String name)
+    {
+        Potion self = (Potion) this;
+
+        self.setPotionName(name);
+        self.setRegistryName(new ResourceLocation(getModId(), name));
+
+        ProxyRegistry.register(self);
+    }
+
+    String getModId();
+}
