41da278cc059119d40f806c2bc60553d12c84e8b
Sven <sven.hjol@protonmail.com>
2019-05-17 23:02:38 +0000

    MesonItemBlock no longer abstract, base implementation can be created by MesonBlock when it's registered. MesonItemBlock handles block variants, adds subtypes. Return metadata "damage" when selected in inventory.

--- src/main/java/svenhjol/meson/MesonItemBlock.java
+++ src/main/java/svenhjol/meson/MesonItemBlock.java
@@ -2,26 +2,74 @@ package svenhjol.meson;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.ItemMeshDefinition;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
+import svenhjol.meson.iface.IMesonBlock;
+import svenhjol.meson.iface.IMesonItemBlock;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("unused")
-public abstract class MesonItemBlock extends ItemBlock
+public class MesonItemBlock extends ItemBlock implements IMesonItemBlock
 {
+    public IMesonBlock block;
     public List<ResourceLocation> variants = new ArrayList<>();
 
     public MesonItemBlock(Block block, ResourceLocation name)
     {
         super(block);
-        setRegistryName(name);
 
-        variants.add(this.getRegistryName());
-        ProxyRegistry.items.add(this);
+        this.block = (IMesonBlock)block;
+
+        if (this.block.getVariants().length > 0) {
+            setHasSubtypes(true);
+        }
+        setRegistryName(name);
     }
 
+    @Override
+    public String getModId()
+    {
+        return block.getModId();
+    }
+
+    @Override
+    public int getMetadata(int damage)
+    {
+        return damage;
+    }
+
+    @Override
+    public String getUnlocalizedNameInefficiently(ItemStack stack)
+    {
+        int meta = stack.getItemDamage();
+        String[] variants = block.getVariants();
+        String name;
+
+        if (meta >= variants.length) {
+            name = block.getName();
+        } else {
+            name = block.getName() + "_" + variants[meta];
+        }
+
+        return "tile." + block.getModId() + ":" + name;
+    }
+
+    @Override
+    public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items)
+    {
+        String[] variants = block.getVariants();
+        if (isInCreativeTab(tab)) {
+            for (int i = 0; i < variants.length; i++) {
+                items.add(new ItemStack(this, 1, i));
+            }
+        }
+    }
+
+    @SuppressWarnings("unused")
     public ItemMeshDefinition getCustomMeshDefinition()
     {
         return null;
