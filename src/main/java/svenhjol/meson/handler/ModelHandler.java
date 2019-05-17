d38d9df68a943e50d274c1abaf345e4e252e7abe
Sven <sven.hjol@protonmail.com>
2019-05-17 23:07:19 +0000

    Rework interfaces to use variants, simplify, WIP

---
+++ src/main/java/svenhjol/meson/handler/ModelHandler.java
@@ -0,0 +1,108 @@
+package svenhjol.meson.handler;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import svenhjol.meson.ProxyRegistry;
+import svenhjol.meson.iface.IMesonBlock;
+import svenhjol.meson.iface.IMesonBlock.IHasCustomInventoryItemModel;
+import svenhjol.meson.iface.IMesonBlock.IHasCustomStateMapper;
+import svenhjol.meson.iface.IMesonItem.IItemCustomModel;
+import svenhjol.meson.iface.IMesonItem.IItemVariants;
+
+import java.util.List;
+import java.util.Objects;
+
+public final class ModelHandler
+{
+    @SubscribeEvent
+    public static void onRegister(ModelRegistryEvent event)
+    {
+        ProxyRegistry.items.forEach(ModelHandler::registerModels);
+//        ProxyRegistry.blocks.forEach(ModelHandler::registerBlockModels);
+    }
+
+//    @SubscribeEvent
+//    public static void onItemColorRegister(ColorHandlerEvent.Item event)
+//    {
+//        for (Item item : ProxyRegistry.items) {
+//            if (!(item instanceof IMesonItem.IItemColorHandler)) continue;
+//            event.getItemColors().registerItemColorHandler(((IMesonItem.IItemColorHandler)item).getItemColor(), item);
+//        }
+//    }
+
+    public static void registerModels(Item item)
+    {
+        int meta = 0;
+
+        if (item instanceof IItemCustomModel) {
+            ((IItemCustomModel) item).registerModels(item);
+        } else {
+            ModelResourceLocation loc = new ModelResourceLocation(Objects.requireNonNull(item.getRegistryName()), "inventory");
+            ModelLoader.setCustomModelResourceLocation(item, meta, loc);
+        }
+
+        if (item instanceof IItemVariants) {
+            List<ResourceLocation> variants = ((IItemVariants) item).getVariants();
+            for (int i = 0; i < variants.size(); i++) {
+                ModelResourceLocation loc = new ModelResourceLocation(variants.get(i), "inventory");
+                ModelLoader.setCustomModelResourceLocation(item, i, loc);
+            }
+            ModelLoader.registerItemVariants(item, variants.toArray(new ResourceLocation[0]));
+        }
+
+        if (item instanceof ItemBlock && ((ItemBlock)item).getBlock() instanceof IMesonBlock) {
+            IMesonBlock block = (IMesonBlock)((ItemBlock)item).getBlock();
+
+            String[] variants = block.getVariants();
+
+            if (variants.length > 0) {
+                for (int i = 0; i < variants.length; i++) {
+                    String variant = variants[i];
+                    String name = block.getModId() + ":" + block.getName();
+                    ModelResourceLocation inv = new ModelResourceLocation(name, "inventory,variant=" + variant);
+                    ModelLoader.setCustomModelResourceLocation(item, i, inv);
+                }
+            }
+
+            if (block instanceof IHasCustomStateMapper) {
+                ((IHasCustomStateMapper) block).setStateMapper();
+            }
+
+            if (block instanceof IHasCustomInventoryItemModel) {
+                ((IHasCustomInventoryItemModel) block).setInventoryItemModel();
+            } else {
+                ModelResourceLocation loc = new ModelResourceLocation(Objects.requireNonNull(item.getRegistryName()), "inventory");
+                ModelLoader.setCustomModelResourceLocation(item, 0, loc);
+            }
+        }
+    }
+
+    public static void registerBlockModels(Block block)
+    {
+        if (block instanceof IHasCustomStateMapper) {
+            ((IHasCustomStateMapper) block).setStateMapper();
+        }
+
+        if (block instanceof IHasCustomInventoryItemModel) {
+            ((IHasCustomInventoryItemModel) block).setInventoryItemModel();
+        } else {
+//            int numVariants = 0;
+            Item item = Item.getItemFromBlock(block);
+            ModelResourceLocation loc = new ModelResourceLocation(Objects.requireNonNull(item.getRegistryName()), "inventory");
+
+//            if (block instanceof IMesonBlock) {
+//                numVariants = ((IMesonBlock) block).getVariants().length;
+//            }
+
+//            for (int i = 0; i < numVariants; i++) {
+                ModelLoader.setCustomModelResourceLocation(item, 0, loc);
+//            }
+        }
+    }
+}
