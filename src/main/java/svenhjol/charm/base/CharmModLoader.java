e491ea50fa00af3064849c343856b7f23d2e98a5
Sven <sven.hjol@protonmail.com>
2019-05-17 23:08:24 +0000

    Moved config creation to concrete class

--- src/main/java/svenhjol/charm/base/CharmModLoader.java
+++ src/main/java/svenhjol/charm/base/CharmModLoader.java
@@ -1,14 +1,28 @@
 package svenhjol.charm.base;
 
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import svenhjol.charm.Charm;
 import svenhjol.meson.ModLoader;
+
+import java.io.File;
 
 public final class CharmModLoader extends ModLoader
 {
     public static final ModLoader INSTANCE = new CharmModLoader();
 
     @Override
-    protected void setupConfig()
+    protected Configuration setupConfig(FMLPreInitializationEvent event)
     {
+        // set up configuration
+        File configFile = new File(event.getModConfigurationDirectory(), Charm.MOD_ID + "-1.1.cfg");
+
+//        configFile.delete();
+        config = new Configuration(configFile, Charm.MOD_VERSION, true);
+        config.load();
+
         CharmSounds.registerSounds();
+
+        return config;
     }
 }
