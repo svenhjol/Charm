0c65b33dd77d8741766737e3d90973a61405f4c1
Sven <sven.hjol@protonmail.com>
2019-05-17 23:03:14 +0000

    Moved interfaces

--- src/main/java/svenhjol/meson/MesonTile.java
+++ src/main/java/svenhjol/meson/MesonTile.java
@@ -7,6 +7,7 @@ import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import svenhjol.meson.iface.IMesonTile;
 
 import javax.annotation.Nullable;
 
