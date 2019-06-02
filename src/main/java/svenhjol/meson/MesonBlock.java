f680cc31e16099d62eab5c4d27decd8d2bded29a
Sven <sven.hjol@protonmail.com>
2019-06-02 21:54:49 +0000

    Added color variants

--- src/main/java/svenhjol/meson/MesonBlock.java
+++ src/main/java/svenhjol/meson/MesonBlock.java
@@ -54,4 +54,49 @@ public abstract class MesonBlock extends Block implements IMesonBlock
             return byMetadata(new Random().nextInt(values().length));
         }
     }
+
+
+    public enum ColorVariant implements IMesonEnum
+    {
+        WHITE,
+        ORANGE,
+        MAGENTA,
+        LIGHT_BLUE,
+        YELLOW,
+        LIME,
+        PINK,
+        GRAY,
+        SILVER,
+        CYAN,
+        PURPLE,
+        BLUE,
+        BROWN,
+        GREEN,
+        RED,
+        BLACK;
+
+        private static final ColorVariant[] METADATA_LOOKUP = new ColorVariant[values().length];
+
+        static {
+            ColorVariant[] values = values();
+            for (ColorVariant v : values) {
+                METADATA_LOOKUP[v.ordinal()] = v;
+            }
+        }
+
+        public static ColorVariant byMetadata(int meta)
+        {
+            if (meta < 0 || meta >= METADATA_LOOKUP.length) {
+                meta = 0;
+            }
+            return METADATA_LOOKUP[meta];
+        }
+
+        public static ColorVariant random()
+        {
+            return byMetadata(new Random().nextInt(values().length));
+        }
+    }
+
+
 }
