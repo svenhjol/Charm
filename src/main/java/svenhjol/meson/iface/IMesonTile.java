3a17c6f435048c53178eef23c4511218286e792a
Sven <sven.hjol@protonmail.com>
2019-06-05 18:08:46 +0000

    Required methods for IMesonTile interface

--- src/main/java/svenhjol/meson/iface/IMesonTile.java
+++ src/main/java/svenhjol/meson/iface/IMesonTile.java
@@ -1,6 +1,12 @@
 package svenhjol.meson.iface;
 
+import net.minecraft.nbt.NBTTagCompound;
+
 public interface IMesonTile
 {
     String getModId();
+
+    void writeTag(NBTTagCompound tag);
+
+    void readTag(NBTTagCompound tag);
 }
