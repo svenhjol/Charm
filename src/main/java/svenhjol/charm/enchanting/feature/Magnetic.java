e0b8b67e215411eac1b35a5d075853e721b3ce27
Sven <sven.hjol@protonmail.com>
2019-06-19 12:39:19 +0000

    Start and stop counting of harvested blocks

--- src/main/java/svenhjol/charm/enchanting/feature/Magnetic.java
+++ src/main/java/svenhjol/charm/enchanting/feature/Magnetic.java
@@ -2,8 +2,11 @@ package svenhjol.charm.enchanting.feature;
 
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -12,6 +15,7 @@ import svenhjol.charm.enchanting.enchantment.EnchantmentMagnetic;
 import svenhjol.meson.Feature;
 import svenhjol.meson.helper.EnchantmentHelper;
 import svenhjol.meson.helper.PlayerHelper;
+import svenhjol.meson.helper.WorldHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -69,10 +73,41 @@ public class Magnetic extends Feature
     {
         if (event.phase == TickEvent.Phase.START
             && event.player != null
+            && !drops.isEmpty()
             && event.player.world.getWorldTime() % 10 == 0
             && harvesting.isEmpty()
         ) {
             drops.clear();
+        }
+    }
+
+    public static void startCollectingDrops(EntityPlayerMP player)
+    {
+        UUID uid = player.getUniqueID();
+        ItemStack tool = player.getHeldItemMainhand();
+
+        if (EnchantmentHelper.hasEnchantment(enchantment, tool)) {
+            if (!harvesting.contains(uid)) {
+                harvesting.add(uid);
+            }
+        }
+    }
+
+    public static void stopCollectingDrops(EntityPlayerMP player)
+    {
+        UUID uid = player.getUniqueID();
+
+        if (!drops.isEmpty()) {
+            for (EntityItem drop : drops) {
+                if (WorldHelper.getDistanceSq(player.getPosition(), drop.getPosition()) < 20) {
+                    EntityItem fake = new EntityItem(player.world, player.posX, player.posY, player.posZ);
+                    fake.setItem(drop.getItem());
+                    if (!MinecraftForge.EVENT_BUS.post(new EntityItemPickupEvent(player, fake))) {
+                        PlayerHelper.addOrDropStack(player, drop.getItem());
+                    }
+                }
+            }
+            harvesting.remove(uid);
         }
     }
 
