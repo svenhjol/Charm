8c0643baacdd4a2b47b0b53401690b08fa14d9e1
Sven <sven.hjol@protonmail.com>
2019-06-19 15:41:47 +0000

    Don't assume EntityLiving from EntityLivingBase. Ref #97

--- src/main/java/svenhjol/charm/tweaks/feature/TamedAnimalsNoDamage.java
+++ src/main/java/svenhjol/charm/tweaks/feature/TamedAnimalsNoDamage.java
@@ -1,15 +1,15 @@
 package svenhjol.charm.tweaks.feature;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.EntityTameable;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.util.SoundCategory;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import svenhjol.meson.helper.SoundHelper;
 import svenhjol.meson.Feature;
+import svenhjol.meson.helper.SoundHelper;
 
 public class TamedAnimalsNoDamage extends Feature
 {
@@ -29,7 +29,7 @@ public class TamedAnimalsNoDamage extends Feature
             Entity attacker = event.getSource().getImmediateSource();
 
             if (attacker instanceof EntityPlayer) {
-                EntityLiving target = (EntityLiving) event.getEntityLiving();
+                EntityLivingBase target = event.getEntityLiving();
                 if (target instanceof EntityTameable && ((EntityTameable) target).isTamed()) {
                     event.setCanceled(true);
                     if (attacker.world.isRemote) {
