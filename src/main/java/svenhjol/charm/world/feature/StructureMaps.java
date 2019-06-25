ed0eec8f69e0c93b0ecadd7dd3428d7a4680f2e4
Sven <sven.hjol@protonmail.com>
2019-06-25 01:31:34 +0000

    Improvements to structure maps config

--- src/main/java/svenhjol/charm/world/feature/StructureMaps.java
+++ src/main/java/svenhjol/charm/world/feature/StructureMaps.java
@@ -25,36 +25,92 @@ import svenhjol.meson.helper.WorldHelper.StructureType;
 import svenhjol.meson.registry.VillagerRegistry;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class StructureMaps extends Feature
 {
+    public static int tradeLevel;
+    public static int biomeMinCost;
+    public static int biomeMaxCost;
+    public static int generalMinCost;
+    public static int generalMaxCost;
+    public static List<String> biomeStructures = new ArrayList<>();
+    public static List<String> generalStructures = new ArrayList<>();
     public static List<Structure> structures = new ArrayList<>();
+
+    @Override
+    public String getDescription()
+    {
+        return "Cartographers sell Structure Maps that can be used to find overworld structures.";
+    }
+
+    @Override
+    public void setupConfig()
+    {
+        biomeStructures = Arrays.asList(
+            propStringList(
+                "Biome-specific structures",
+                "List of biome-specific structures that are available as map locations.",
+                new String[] {
+                    StructureType.DESERT_PYRAMID.getName(),
+                    StructureType.IGLOO.getName(),
+                    StructureType.JUNGLE_PYRAMID.getName(),
+                    StructureType.SWAMP_HUT.getName(),
+                })
+        );
+        generalStructures = Arrays.asList(
+            propStringList(
+                "General structures",
+                "List of general structures that are available as map locations.",
+                new String[] {
+                    StructureType.MINESHAFT.getName(),
+                    StructureType.VILLAGE.getName()
+                }
+            )
+        );
+
+        tradeLevel = propInt("Trade level",
+            "The level at which a cartographer will be unlocked for trading structure maps.",
+            3);
+        generalMinCost = propInt("General structures minimum cost",
+            "Minimum emerald cost of a general structure map.",
+            4);
+        generalMaxCost = propInt("General structures maximum cost",
+            "Maximum emerald cost of a general structure map.",
+            7);
+        biomeMinCost = propInt("Biome-specific minimum cost",
+            "Minimum emerald cost of a biome-specific structure map.",
+            16);
+        biomeMaxCost = propInt("Biome-specific maximum cost",
+            "Maximum emerald cost of a biome-specific structure map.",
+            22);
+    }
 
     @Override
     public void preInit(FMLPreInitializationEvent event)
     {
-        structures.add(new Structure(StructureType.MINESHAFT, 0x774400));
-        structures.add(new Structure(StructureType.VILLAGE, 0xFF2200));
-        structures.add(new Structure(StructureType.IGLOO, 0xE0E0FF));
-        structures.add(new Structure(StructureType.DESERT_PYRAMID, 0xFFEF60));
-        structures.add(new Structure(StructureType.JUNGLE_PYRAMID, 0x20B020));
-        structures.add(new Structure(StructureType.SWAMP_HUT, 0x107000));
+        if (generalStructures.contains("Mineshaft")) structures.add(new Structure(StructureType.MINESHAFT, 0x774400));
+        if (generalStructures.contains("Village")) structures.add(new Structure(StructureType.VILLAGE, 0xCC2000));
+        if (biomeStructures.contains("Igloo")) structures.add(new Structure(StructureType.IGLOO, 0xA0C0FF));
+        if (biomeStructures.contains("DesertPyramid")) structures.add(new Structure(StructureType.DESERT_PYRAMID, 0x866600));
+        if (biomeStructures.contains("JunglePyramid")) structures.add(new Structure(StructureType.JUNGLE_PYRAMID, 0x20B020));
+        if (biomeStructures.contains("SwampHut")) structures.add(new Structure(StructureType.SWAMP_HUT, 0x107000));
     }
 
     @SubscribeEvent
     public void onRegisterVillagers(RegistryEvent.Register<VillagerProfession> event)
     {
         VillagerCareer cartographer = VillagerRegistry.cartographer;
-        cartographer.addTrade(3, new StructureMapTrade());
+        cartographer.addTrade(tradeLevel, new StructureMapTrade());
     }
 
     public static ItemStack createMap(World world, BlockPos pos, Structure structure)
     {
         BlockPos offset = pos.offset(EnumFacing.random(world.rand), 250);
         BlockPos structurePos = WorldHelper.getNearestStructure(world, offset, structure.type);
-        if (structurePos == null) return ItemStack.EMPTY;
+        if (structurePos == null) return new ItemStack(Items.MAP, 2);
 
         int id = world.getUniqueDataId("map");
         ItemStack stack = new ItemStack(Items.FILLED_MAP, 1, id);
@@ -110,8 +166,15 @@ public class StructureMaps extends Feature
         @Override
         public void addMerchantRecipe(IMerchant merchant, MerchantRecipeList recipeList, Random random)
         {
-            ItemStack map = createMap(merchant.getWorld(), merchant.getPos(), structures.get(random.nextInt(structures.size())));
-            int emeraldCount = random.nextInt(3) + 3;
+            Structure structure = structures.get(random.nextInt(structures.size()));
+            ItemStack map = createMap(merchant.getWorld(), merchant.getPos(), structure);
+
+            int emeraldCount;
+            if (generalStructures.contains(structure.type.getName())) {
+                emeraldCount = random.nextInt(generalMaxCost - generalMinCost) + generalMinCost;
+            } else {
+                emeraldCount = random.nextInt(biomeMaxCost - biomeMinCost) + biomeMinCost;
+            }
             recipeList.add(new MerchantRecipe(new ItemStack(Items.EMERALD, emeraldCount), new ItemStack(Items.COMPASS), map, 0, 1));
         }
     }
