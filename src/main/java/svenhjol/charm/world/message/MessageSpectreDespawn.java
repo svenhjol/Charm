ee4660519d7577fbd5177de526a10235d77bd409
Sven <sven.hjol@protonmail.com>
2019-07-08 20:21:45 +0000

    Message to send when spectre despawns

--- src/main/java/svenhjol/charm/world/message/MessageSpectreDespawn.java
+++ src/main/java/svenhjol/charm/world/message/MessageSpectreDespawn.java
@@ -1,17 +1,19 @@
 package svenhjol.charm.world.message;
 
 import io.netty.buffer.ByteBuf;
+import net.minecraft.client.Minecraft;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import svenhjol.charm.world.feature.Spectre;
 import svenhjol.meson.MesonMessage;
 
 public class MessageSpectreDespawn extends MesonMessage
 {
-    public int dimension;
     public BlockPos pos;
 
-    public MessageSpectreDespawn(int dimension, BlockPos pos)
+    public MessageSpectreDespawn(BlockPos pos)
     {
-        this.dimension = dimension;
         this.pos = pos;
     }
 
@@ -23,14 +25,22 @@ public class MessageSpectreDespawn extends MesonMessage
     @Override
     public void toBytes(ByteBuf buf)
     {
-        buf.writeInt(dimension);
         buf.writeLong(pos.toLong());
     }
 
     @Override
     public void fromBytes(ByteBuf buf)
     {
-        dimension = buf.readInt();
         pos = BlockPos.fromLong(buf.readLong());
+    }
+
+    @Override
+    public IMessage handle(MessageContext context)
+    {
+        Minecraft.getMinecraft().addScheduledTask(() -> {
+            Spectre.effectDespawn(pos);
+        });
+
+        return null;
     }
 }
