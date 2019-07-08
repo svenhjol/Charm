ebe46dd086df67be5d26f5bfdfb27aa44ca45592
Sven <sven.hjol@protonmail.com>
2019-07-08 20:21:29 +0000

    Spectre eyes use similar spider eye code

--- src/main/java/svenhjol/charm/world/client/layer/LayerSpectreEyes.java
+++ src/main/java/svenhjol/charm/world/client/layer/LayerSpectreEyes.java
@@ -8,42 +8,50 @@ import net.minecraft.util.ResourceLocation;
 import svenhjol.charm.world.client.render.RenderSpectre;
 import svenhjol.charm.world.entity.EntitySpectre;
 
-import java.util.Random;
-
 public class LayerSpectreEyes implements LayerRenderer<EntitySpectre>
 {
     public static final ResourceLocation SPECTRE_EYES = new ResourceLocation("charm","textures/entity/spectre/spectre_eyes.png");
     public final RenderSpectre spectreRenderer;
-    public Random rng;
 
     public LayerSpectreEyes(RenderSpectre spectreRenderer)
     {
         this.spectreRenderer = spectreRenderer;
-        rng = new Random();
     }
 
     @Override
     public void doRenderLayer(EntitySpectre spectre, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale)
     {
-        boolean blinking = (ageInTicks % 300 < 8);
+        boolean blinking = (ageInTicks % 200 < 6);
         if (blinking) return;
 
         this.spectreRenderer.bindTexture(SPECTRE_EYES);
         GlStateManager.enableBlend();
         GlStateManager.disableAlpha();
         GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
-        GlStateManager.disableLighting();
+//        GlStateManager.disableLighting();
+
         GlStateManager.depthMask(!spectre.isInvisible());
-        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 61680.0F, 0.0F);
-        GlStateManager.enableLighting();
+
+        int i = '\uf0f0';
+        int j = i % 65536;
+        int k = i / 65536;
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)j, (float)k);
+
+//        GlStateManager.enableLighting();
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
         Minecraft.getMinecraft().entityRenderer.setupFogColor(true);
         this.spectreRenderer.getMainModel().render(spectre, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
         Minecraft.getMinecraft().entityRenderer.setupFogColor(false);
-        this.spectreRenderer.setLightmap(spectre);
 
+        i = spectre.getBrightnessForRender();
+        j = i % 65536;
+        k = i / 65536;
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)j, (float)k);
+        this.spectreRenderer.setLightmap(spectre);
+//        GlStateManager.depthMask(true);
         GlStateManager.disableBlend();
         GlStateManager.enableAlpha();
+
     }
 
     @Override
