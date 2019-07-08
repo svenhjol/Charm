414a23056d43944122172387fb6acbe6c9ecdfe2
Sven <sven.hjol@protonmail.com>
2019-07-08 16:30:12 +0000

    Spectre haunting feature in progress

--- src/main/java/svenhjol/charm/world/feature/SpectreHaunting.java
+++ src/main/java/svenhjol/charm/world/feature/SpectreHaunting.java
@@ -1,6 +1,7 @@
 package svenhjol.charm.world.feature;
 
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import svenhjol.charm.Charm;
@@ -8,12 +9,38 @@ import svenhjol.meson.Feature;
 import svenhjol.meson.Meson;
 import svenhjol.meson.helper.EntityHelper;
 
+import static svenhjol.meson.helper.WorldHelper.*;
+
 public class SpectreHaunting extends Feature
 {
+    public static StructureType structure;
+    public static int distance;
+    public static int checkInterval;
+    public static int spawnMinDistance;
+    public static int spawnRange;
+    public static float spawnChance;
+
+    @Override
+    public String getDescription()
+    {
+        return "Spectres will spawn in greater numbers within range of specific structure.";
+    }
+
     @Override
     public boolean checkSelf()
     {
         return Charm.hasFeature(Spectre.class);
+    }
+
+    @Override
+    public void setupConfig()
+    {
+        checkInterval = 30;
+        distance = 5000;
+        spawnChance = 0.25f;
+        spawnMinDistance = 15;
+        spawnRange = 6;
+        structure = StructureType.STRONGHOLD;
     }
 
     @SubscribeEvent
@@ -21,19 +48,16 @@ public class SpectreHaunting extends Feature
     {
         if (event.phase == TickEvent.Phase.END
             && event.side.isServer()
-            && event.player.world.getTotalWorldTime() % 30 == 0
-            && event.player.world.rand.nextFloat() < 0.1f
+            && event.player.world.getTotalWorldTime() % checkInterval == 0
+            && event.player.world.rand.nextFloat() < spawnChance
             && event.player.world.getLightBrightness(event.player.getPosition()) < ((float)Spectre.despawnLight)/16.0
-            && event.player.getPosition().getY() <= Spectre.spawnDepth
         ) {
-            Meson.debug("Spectre spawned ", event.player.getPosition());
-            EntityHelper.spawnEntityNearPlayer(event.player, 15,6, new ResourceLocation(Charm.MOD_ID, "spectre"));
-
-            // check mineshaft
-//            BlockPos mineshaft = WorldHelper.getNearestStructure(event.player.world, event.player.getPosition(), WorldHelper.StructureType.MINESHAFT);
-//            if (mineshaft != null && WorldHelper.getDistanceSq(event.player.getPosition(), mineshaft) < 1000) {
-//
-//            }
+            // check structure
+            BlockPos nearest = getNearestStructure(event.player.world, event.player.getPosition(), structure);
+            if (nearest != null && getDistanceSq(event.player.getPosition(), nearest) < distance) {
+                Meson.debug("Spectre haunting ", event.player.getPosition());
+                EntityHelper.spawnEntityNearPlayer(event.player, spawnMinDistance, spawnRange, new ResourceLocation(Charm.MOD_ID, "spectre"));
+            }
         }
     }
 
