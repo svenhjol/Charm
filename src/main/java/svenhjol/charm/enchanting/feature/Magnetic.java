82d46097159d5f525e38bf47cf96486b7e0a9eaa
Sven <sven.hjol@protonmail.com>
2019-07-17 19:55:29 +0000

    Network message when magnetic successful so that item pickup sound can be played. Fix #123

--- src/main/java/svenhjol/charm/enchanting/feature/Magnetic.java
+++ src/main/java/svenhjol/charm/enchanting/feature/Magnetic.java
@@ -2,16 +2,24 @@ package svenhjol.charm.enchanting.feature;
 
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.server.management.PlayerInteractionManager;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import svenhjol.charm.enchanting.enchantment.EnchantmentMagnetic;
+import svenhjol.charm.enchanting.message.MessageMagneticPickup;
 import svenhjol.meson.Feature;
+import svenhjol.meson.handler.ClientHandler;
+import svenhjol.meson.handler.NetworkHandler;
 import svenhjol.meson.helper.EnchantmentHelper;
 import svenhjol.meson.helper.PlayerHelper;
+import svenhjol.meson.helper.SoundHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +47,7 @@ public class Magnetic extends Feature
     public void preInit(FMLPreInitializationEvent event)
     {
         enchantment = new EnchantmentMagnetic();
+        NetworkHandler.register(MessageMagneticPickup.class, Side.CLIENT);
     }
 
     @SubscribeEvent
@@ -65,7 +74,6 @@ public class Magnetic extends Feature
     public static void stopCollectingDrops()
     {
         if (manager != null && !drops.isEmpty()) {
-
             ArrayList<EntityItem> copy = new ArrayList<>(drops);
             drops.clear();
 
@@ -74,12 +82,21 @@ public class Magnetic extends Feature
                 EntityItem fake = new EntityItem(player.world, player.posX, player.posY, player.posZ);
                 fake.setItem(drop.getItem());
                 if (!MinecraftForge.EVENT_BUS.post(new EntityItemPickupEvent(player, fake))) {
-                    PlayerHelper.addOrDropStack(player, drop.getItem());
+                    boolean result = PlayerHelper.addOrDropStack(player, drop.getItem());
+                    if (result) {
+                        NetworkHandler.INSTANCE.sendTo(new MessageMagneticPickup(player.getPosition()), player);
+                    }
                 }
             }
 
             manager = null;
         }
+    }
+
+    @SideOnly(Side.CLIENT)
+    public static void effectPickup(BlockPos pos)
+    {
+        SoundHelper.playSoundAtPos(ClientHandler.getWorldClient(), pos, SoundEvents.ENTITY_ITEM_PICKUP, 1.0f, 1.0f);
     }
 
     @Override
