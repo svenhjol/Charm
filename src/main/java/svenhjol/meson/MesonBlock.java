36b9b3d5a9b51dbc08c06ec29446748959024ad4
Sven <sven.hjol@protonmail.com>
2019-07-24 18:32:07 +0000

    Block item getter moved to interface to save duplication when adding an interface to a vanilla block

--- src/main/java/svenhjol/meson/MesonBlock.java
+++ src/main/java/svenhjol/meson/MesonBlock.java
@@ -1,8 +1,6 @@
 package svenhjol.meson;
 
 import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import svenhjol.meson.handler.RegistrationHandler;
 import svenhjol.meson.iface.IMesonBlock;
@@ -28,12 +26,5 @@ public abstract class MesonBlock extends Block implements IMesonBlock
     public ItemGroup getItemGroup()
     {
         return ItemGroup.BUILDING_BLOCKS;
-    }
-
-    @Override
-    public BlockItem getBlockItem()
-    {
-        final Item.Properties props = new Item.Properties().group(getItemGroup());
-        return new BlockItem(this, props);
     }
 }
--- src/main/java/svenhjol/meson/iface/IMesonBlock.java
+++ src/main/java/svenhjol/meson/iface/IMesonBlock.java
@@ -1,6 +1,8 @@
 package svenhjol.meson.iface;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 
 public interface IMesonBlock
@@ -11,5 +13,12 @@ public interface IMesonBlock
 
     ItemGroup getItemGroup();
 
-    BlockItem getBlockItem();
+    default BlockItem getBlockItem()
+    {
+        Item.Properties props = new Item.Properties();
+        ItemGroup group = getItemGroup();
+        if (group != null) props.group(group);
+
+        return new BlockItem((Block)this, props);
+    }
 }
