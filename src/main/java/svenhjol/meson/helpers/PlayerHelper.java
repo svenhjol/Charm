1802b95b986faf75956df31cdbc188532faef97b
Sven <sven.hjol@protonmail.com>
2019-07-26 15:15:53 +0000

    Added setHeldItem and addOrDropStack methods to PlayerHelper

--- src/main/java/svenhjol/meson/helpers/PlayerHelper.java
+++ src/main/java/svenhjol/meson/helpers/PlayerHelper.java
@@ -1,11 +1,43 @@
 package svenhjol.meson.helpers;
 
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.dimension.DimensionType;
 
 public class PlayerHelper
 {
+    /**
+     * Tries to add item stack to player, drops if not possible.
+     * @param player The player
+     * @param stack The stack to add/drop
+     * @return True if able to add to player inv, false if dropped
+     */
+    public static boolean addOrDropStack(PlayerEntity player, ItemStack stack)
+    {
+        if (!player.inventory.addItemStackToInventory(stack)) {
+            player.dropItem(stack, false);
+            return false;
+        }
+        return true;
+    }
+
+    public static void setHeldItem(PlayerEntity player, Hand hand, ItemStack item)
+    {
+        ItemStack stack = player.getHeldItem(hand);
+        if (stack.getCount() == 1) {
+            player.setHeldItem(hand, item);
+        } else {
+            stack.shrink(1);
+            if (stack.getCount() == 0) {
+                player.setHeldItem(hand, item);
+            } else {
+                addOrDropStack(player, item);
+            }
+        }
+    }
+
     /**
      * Basic way to teleport a player to co-ordinate in a dimension.
      * If the player is not in the specified dimension they will be transferred first.
@@ -23,5 +55,4 @@ public class PlayerHelper
 
         player.setPositionAndUpdate(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5);
     }
-
 }
