d23788857b00384a30a41ede83a1508cecaf8aa9
Sven <sven.hjol@protonmail.com>
2019-07-26 11:15:31 +0000

    WorldHelper getDistanceSq from 1.12

---
+++ src/main/java/svenhjol/meson/helpers/WorldHelper.java
@@ -0,0 +1,16 @@
+package svenhjol.meson.helpers;
+
+import net.minecraft.util.math.BlockPos;
+
+public class WorldHelper
+{
+    public static double getDistanceSq(BlockPos pos1, BlockPos pos2)
+    {
+        double d0 = pos1.getX();
+        double d1 = pos1.getZ();
+        double d2 = d0 - pos2.getX();
+        double d3 = d1 - pos2.getZ();
+        return d2 * d2 + d3 * d3;
+    }
+
+}
