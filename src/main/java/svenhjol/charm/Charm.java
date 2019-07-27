2ac3a301489f63b42285cc64cbd43bdd95113a43
Sven <sven.hjol@protonmail.com>
2019-07-27 22:25:31 +0000

    Charm mod loader for new features and lang update

--- src/main/java/svenhjol/charm/Charm.java
+++ src/main/java/svenhjol/charm/Charm.java
@@ -2,14 +2,17 @@ package svenhjol.charm;
 
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import svenhjol.charm.automation.CharmAutomation;
 import svenhjol.charm.base.CharmModLoader;
 import svenhjol.charm.base.ClientProxy;
 import svenhjol.charm.base.ServerProxy;
+import svenhjol.charm.brewing.CharmBrewing;
 import svenhjol.charm.crafting.CharmCrafting;
 import svenhjol.charm.tweaks.CharmTweaks;
 import svenhjol.charm.world.CharmWorld;
+import svenhjol.meson.Feature;
 import svenhjol.meson.Meson;
-import svenhjol.meson.iface.IProxy;
+import svenhjol.meson.iface.IMesonSidedProxy;
 
 @Mod(Charm.MOD_ID)
 public class Charm
@@ -18,13 +21,15 @@ public class Charm
     public static final String MOD_ID = "charm";
     public static final String VERSION = "1.2.3";
 
-    public static IProxy proxy = DistExecutor.runForDist(() -> ClientProxy::new, () -> ServerProxy::new);
+    public static IMesonSidedProxy proxy = DistExecutor.runForDist(() -> ClientProxy::new, () -> ServerProxy::new);
 
     public Charm()
     {
         Meson.init();
 
         CharmModLoader.INSTANCE.registerModLoader(MOD_ID).setup(
+            new CharmAutomation(),
+            new CharmBrewing(),
             new CharmCrafting(),
             new CharmTweaks(),
             new CharmWorld()
@@ -32,5 +37,10 @@ public class Charm
 
         // add the Charm modloader to event bus
 //        FMLJavaModLoadingContext.get().getModEventBus().addListener();
+    }
+
+    public static boolean hasFeature(Class<? extends Feature> feature)
+    {
+        return CharmModLoader.INSTANCE.enabledFeatures.contains(feature);
     }
 }
