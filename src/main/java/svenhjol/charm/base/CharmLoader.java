99ae33e5da9e5a87ea95e91a9d664ada3a186b66
Sven <sven.hjol@protonmail.com>
2019-07-28 18:55:15 +0000

    Refactor CharmModLoader to CharmLoader to better reflect what it does

---
+++ src/main/java/svenhjol/charm/base/CharmLoader.java
@@ -0,0 +1,8 @@
+package svenhjol.charm.base;
+
+import svenhjol.meson.MesonLoader;
+
+public final class CharmLoader extends MesonLoader
+{
+    public static final MesonLoader INSTANCE = new CharmLoader();
+}
--- src/main/java/svenhjol/charm/base/CharmModLoader.java
+++
@@ -1,8 +0,0 @@
-package svenhjol.charm.base;
-
-import svenhjol.meson.ModLoader;
-
-public final class CharmModLoader extends ModLoader
-{
-    public static final ModLoader INSTANCE = new CharmModLoader();
-}
