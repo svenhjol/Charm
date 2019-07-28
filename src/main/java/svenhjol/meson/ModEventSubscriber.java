d0df1dbfd0aa72aeb99aafeda76e94a1f4314bf1
Sven <sven.hjol@protonmail.com>
2019-07-28 18:54:02 +0000

    Centralised registration

--- src/main/java/svenhjol/meson/ModEventSubscriber.java
+++ src/main/java/svenhjol/meson/ModEventSubscriber.java
@@ -1,95 +1,67 @@
 package svenhjol.meson;
 
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.potion.Effect;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionUtils;
-import net.minecraftforge.common.brewing.BrewingRecipe;
-import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.registries.IForgeRegistry;
-import svenhjol.meson.handler.RegistrationHandler;
-import svenhjol.meson.iface.IMesonBlock;
-import svenhjol.meson.iface.IMesonEffect;
-import svenhjol.meson.iface.IMesonItem;
-import svenhjol.meson.iface.IMesonPotion;
-
-import static net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.MOD;
-
-@Mod.EventBusSubscriber(bus = MOD)
+//@Mod.EventBusSubscriber(bus = MOD)
 public class ModEventSubscriber
 {
-    @SubscribeEvent
-    public static void onRegisterBlocks(final RegistryEvent.Register<Block> event)
-    {
-        final IForgeRegistry<Block> registry = event.getRegistry();
-
-        // register blocks
-        for (IMesonBlock block : RegistrationHandler.BLOCKS) {
-            registry.register(((Block)block).setRegistryName(block.getModId(), block.getBaseName()));
-        }
-
-        Meson.log("Block registration done");
-    }
-
-    @SubscribeEvent
-    public static void onRegisterItems(final RegistryEvent.Register<Item> event)
-    {
-        final IForgeRegistry<Item> registry = event.getRegistry();
-
-        // register items
-        for (IMesonItem item : RegistrationHandler.ITEMS) {
-            registry.register(((Item)item).setRegistryName(item.getModId(), item.getBaseName()));
-        }
-
-        // register block items
-        for (IMesonBlock block : RegistrationHandler.BLOCKS) {
-            registry.register(block.getBlockItem().setRegistryName(block.getModId(), block.getBaseName()));
-        }
-
-        Meson.log("Item registration done");
-    }
-
-    @SubscribeEvent
-    public static void onRegisterEffects(final RegistryEvent.Register<Effect> event)
-    {
-        final IForgeRegistry<Effect> registry = event.getRegistry();
-
-        // register effects
-        for (IMesonEffect effect : RegistrationHandler.EFFECTS) {
-            registry.register(((Effect)effect).setRegistryName(effect.getModId(), effect.getBaseName()));
-        }
-
-        Meson.log("Effect registration done");
-    }
-
-    @SubscribeEvent
-    public static void onRegisterPotions(final RegistryEvent.Register<Potion> event)
-    {
-        final IForgeRegistry<Potion> registry = event.getRegistry();
-
-        // register potions
-        for (IMesonPotion p : RegistrationHandler.POTIONS) {
-            Potion potion = ((Potion)p).setRegistryName(p.getModId(), p.getBaseName());
-            registry.register(potion);
-
-            // register the recipe
-            ItemStack out = PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), potion);
-            BrewingRecipeRegistry.addRecipe(new BrewingRecipe(p.getBase(), p.getReagant(), out));
-        }
-
-        Meson.log("Potion registration done");
-    }
-
-    @SubscribeEvent
-    public static void onCommonSetup(final FMLCommonSetupEvent event)
-    {
-        Meson.log("Common setup done");
-    }
+//    @SubscribeEvent
+//    public static void onRegisterBlocks(final RegistryEvent.Register<Block> event)
+//    {
+//        final IForgeRegistry<Block> registry = event.getRegistry();
+//
+//        // register blocks
+//        for (IMesonBlock block : RegistrationHandler.BLOCKS) {
+//            registry.register((Block)block);
+//        }
+//
+//        Meson.log("Block registration done");
+//    }
+//
+//    @SubscribeEvent
+//    public static void onRegisterItems(final RegistryEvent.Register<Item> event)
+//    {
+//        final IForgeRegistry<Item> registry = event.getRegistry();
+//
+//        // register items
+//        for (IMesonItem item : RegistrationHandler.ITEMS) {
+//            registry.register((Item)item);
+//        }
+//
+//        // register block items
+//        for (IMesonBlock block : RegistrationHandler.BLOCKS) {
+//            BlockItem blockItem = block.getBlockItem();
+//            registry.register(blockItem);
+//        }
+//
+//        Meson.log("Item registration done");
+//    }
+//
+//    @SubscribeEvent
+//    public static void onRegisterEffects(final RegistryEvent.Register<Effect> event)
+//    {
+//        final IForgeRegistry<Effect> registry = event.getRegistry();
+//
+//        // register effects
+//        for (IMesonEffect effect : RegistrationHandler.EFFECTS) {
+//            registry.register((Effect)effect);
+//        }
+//
+//        Meson.log("Effect registration done");
+//    }
+//
+//    @SubscribeEvent
+//    public static void onRegisterPotions(final RegistryEvent.Register<Potion> event)
+//    {
+//        final IForgeRegistry<Potion> registry = event.getRegistry();
+//
+//        // register potions
+//        for (IMesonPotion p : RegistrationHandler.POTIONS) {
+//            Potion potion = (Potion)p;
+//            registry.register(potion);
+//
+//            // register the recipe
+//            ((IMesonPotion)potion).registerRecipe();
+//        }
+//
+//        Meson.log("Potion registration done");
+//    }
 }
--- src/main/java/svenhjol/meson/handler/RegistrationHandler.java
+++ src/main/java/svenhjol/meson/handler/RegistrationHandler.java
@@ -1,5 +1,16 @@
 package svenhjol.meson.handler;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.Potion;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistry;
+import svenhjol.meson.Meson;
 import svenhjol.meson.iface.IMesonBlock;
 import svenhjol.meson.iface.IMesonEffect;
 import svenhjol.meson.iface.IMesonItem;
@@ -8,6 +19,9 @@ import svenhjol.meson.iface.IMesonPotion;
 import java.util.ArrayList;
 import java.util.List;
 
+import static net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.MOD;
+
+@Mod.EventBusSubscriber(bus = MOD)
 public class RegistrationHandler
 {
     public static List<IMesonBlock> BLOCKS = new ArrayList<>();
@@ -17,21 +31,87 @@ public class RegistrationHandler
 
     public static void addBlock(IMesonBlock block)
     {
+        ((Block)block).setRegistryName(new ResourceLocation(block.getModId(), block.getBaseName()));
         BLOCKS.add(block);
     }
 
     public static void addEffect(IMesonEffect effect)
     {
+        ((Effect)effect).setRegistryName(new ResourceLocation(effect.getModId(), effect.getBaseName()));
         EFFECTS.add(effect);
     }
 
     public static void addItem(IMesonItem item)
     {
+        ((Item)item).setRegistryName(new ResourceLocation(item.getModId(), item.getBaseName()));
         ITEMS.add(item);
     }
 
     public static void addPotion(IMesonPotion potion)
     {
+        ((Potion)potion).setRegistryName(new ResourceLocation(potion.getModId(), potion.getBaseName()));
         POTIONS.add(potion);
+    }
+
+    @SubscribeEvent
+    public static void onRegisterBlocks(final RegistryEvent.Register<Block> event)
+    {
+        final IForgeRegistry<Block> registry = event.getRegistry();
+
+        // register blocks
+        for (IMesonBlock block : RegistrationHandler.BLOCKS) {
+            registry.register((Block)block);
+        }
+
+        Meson.log("Block registration done");
+    }
+
+    @SubscribeEvent
+    public static void onRegisterItems(final RegistryEvent.Register<Item> event)
+    {
+        final IForgeRegistry<Item> registry = event.getRegistry();
+
+        // register items
+        for (IMesonItem item : RegistrationHandler.ITEMS) {
+            registry.register((Item)item);
+        }
+
+        // register block items
+        for (IMesonBlock block : RegistrationHandler.BLOCKS) {
+            BlockItem blockItem = block.getBlockItem();
+            registry.register(blockItem);
+        }
+
+        Meson.log("Item registration done");
+    }
+
+    @SubscribeEvent
+    public static void onRegisterEffects(final RegistryEvent.Register<Effect> event)
+    {
+        final IForgeRegistry<Effect> registry = event.getRegistry();
+
+        // register effects
+        for (IMesonEffect effect : RegistrationHandler.EFFECTS) {
+            registry.register((Effect)effect);
+        }
+
+        Meson.log("Effect registration done");
+    }
+
+    @SubscribeEvent
+    public static void onRegisterPotions(final RegistryEvent.Register<Potion> event)
+    {
+        final IForgeRegistry<Potion> registry = event.getRegistry();
+
+        // register potions
+        for (IMesonPotion p : RegistrationHandler.POTIONS) {
+            Potion potion = (Potion)p;
+            registry.register(potion);
+
+            // register the recipe
+            ((IMesonPotion)potion).registerRecipe();
+        }
+
+        Meson.log("Potion registration done");
     }
 }
