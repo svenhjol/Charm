c4def97348b9422c2dcccf970afac6924054b269
Sven <sven.hjol@protonmail.com>
2019-08-07 18:57:09 +0000

    ASM so potion stacks can at least be added to brewing stand outputs

--- src/main/resources/META-INF/charmcoremod.js
+++ src/main/resources/META-INF/charmcoremod.js
@@ -1,6 +1,7 @@
 function initializeCoreMod() {
 
     var ASM_HOOKS = "svenhjol/charm/base/CharmAsmHooks";
+    var Opcodes = Java.type('org.objectweb.asm.Opcodes');
     var InsnNode = Java.type('org.objectweb.asm.tree.InsnNode');
     var MethodInsnNode = Java.type('org.objectweb.asm.tree.MethodInsnNode');
     var JumpInsnNode = Java.type('org.objectweb.asm.tree.JumpInsnNode');
@@ -8,7 +9,38 @@ function initializeCoreMod() {
 
     return {
 
-        // add hook to PotionItem so we can disable the glint
+        /*
+         * Change the != to < in BrewingRecipeRegistry::isValidInput
+         * so that potion stacks can be added to brewing stand slots.
+         */
+        'BrewingRecipeRegistry': {
+            'target': {
+                'type': 'METHOD',
+                'class': 'net.minecraftforge.common.brewing.BrewingRecipeRegistry',
+                'methodName': 'isValidInput',
+                'methodDesc': '(Lnet/minecraft/item/ItemStack;)Z'
+            },
+            'transformer': function(method) {
+                var arrayLength = method.instructions.size();
+                for (var i = 0; i < arrayLength; ++i) {
+                    var instruction = method.instructions.get(i)
+                    var newInstructions = [];
+
+                    if (instruction.getOpcode() == Opcodes.IF_ICMPEQ) {
+                        var label = instruction.label;
+                        method.instructions.insertBefore(instruction, new JumpInsnNode(Opcodes.IF_ICMPGE, label));
+                        method.instructions.remove(instruction);
+
+                        print("Transformed BrewingRecipeRegistry");
+                        break;
+                    }
+                }
+            }
+        },
+
+        /*
+         * Add hook to PotionItem so glint can be disabled in config.
+         */
         'PotionItem': {
             'target': {
                 'type': 'METHOD',
@@ -17,7 +49,6 @@ function initializeCoreMod() {
                 'methodDesc': '(Lnet/minecraft/item/ItemStack;)Z'
             },
             'transformer': function(method) {
-                var Opcodes = Java.type('org.objectweb.asm.Opcodes');
                 var arrayLength = method.instructions.size();
                 for (var i = 0; i < arrayLength; ++i) {
                     var instruction = method.instructions.get(i);
