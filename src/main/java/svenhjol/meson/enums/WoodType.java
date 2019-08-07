147205b7f95b3a54aae9e0d660152ce5e893cd10
Sven <sven.hjol@protonmail.com>
2019-08-07 16:25:43 +0000

    Added woodtype enum

---
+++ src/main/java/svenhjol/meson/enums/WoodType.java
@@ -0,0 +1,13 @@
+package svenhjol.meson.enums;
+
+import svenhjol.meson.iface.IMesonEnum;
+
+public enum WoodType implements IMesonEnum
+{
+    OAK,
+    SPRUCE,
+    BIRCH,
+    JUNGLE,
+    ACACIA,
+    DARK_OAK
+}
