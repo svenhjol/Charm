e9212cb369f83c265f67a5a856497e8098617ab1
Sven <sven.hjol@protonmail.com>
2019-08-10 18:05:08 +0000

    Changed the horrible BrewingRecipeRegistry hack to a hook and fixed return bug

--- src/main/resources/META-INF/charmcoremod.js
+++ src/main/resources/META-INF/charmcoremod.js
@@ -3,6 +3,7 @@ function initializeCoreMod() {
     var ASM_HOOKS = "svenhjol/charm/base/CharmAsmHooks";
     var Opcodes = Java.type('org.objectweb.asm.Opcodes');
     var InsnNode = Java.type('org.objectweb.asm.tree.InsnNode');
+    var VarInsnNode = Java.type('org.objectweb.asm.tree.VarInsnNode');
     var MethodInsnNode = Java.type('org.objectweb.asm.tree.MethodInsnNode');
     var JumpInsnNode = Java.type('org.objectweb.asm.tree.JumpInsnNode');
     var LabelNode = Java.type('org.objectweb.asm.tree.LabelNode');
@@ -10,7 +11,7 @@ function initializeCoreMod() {
     return {
 
         /*
-         * Change the != to < in BrewingRecipeRegistry::isValidInput
+         * Add hook to BrewingRecipeRegistry::isValidInput
          * so that potion stacks can be added to brewing stand slots.
          */
         'BrewingRecipeRegistry': {
@@ -27,14 +28,24 @@ function initializeCoreMod() {
                     var newInstructions = [];
 
                     if (instruction.getOpcode() == Opcodes.IF_ICMPEQ) {
-                        var label = instruction.label;
-                        method.instructions.insertBefore(instruction, new JumpInsnNode(Opcodes.IF_ICMPGE, label));
-                        method.instructions.remove(instruction);
+                        var label = new LabelNode();
+                        newInstructions.push(new VarInsnNode(Opcodes.ALOAD, 0));
+                        newInstructions.push(new MethodInsnNode(Opcodes.INVOKESTATIC, ASM_HOOKS, "checkBrewingStandStack", "(Lnet/minecraft/item/ItemStack;)Z", false));
+                        newInstructions.push(new JumpInsnNode(Opcodes.IFEQ, label));
+                        newInstructions.push(new InsnNode(Opcodes.ICONST_1));
+                        newInstructions.push(new InsnNode(Opcodes.IRETURN));
+                        newInstructions.push(label);
+
+                        newInstructions.forEach(function(i) {
+                            method.instructions.insertBefore(instruction, i);
+                        });
 
                         print("Transformed BrewingRecipeRegistry");
                         break;
                     }
                 }
+
+                return method;
             }
         },
 
