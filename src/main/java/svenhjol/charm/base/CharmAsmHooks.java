c2728b535ace8a7295fc2ca26c2399fe8dffa284
Sven <sven.hjol@protonmail.com>
2019-08-10 18:30:40 +0000

    Probably over-simplistic hook for brewing stand output check

--- src/main/java/svenhjol/charm/base/CharmAsmHooks.java
+++ src/main/java/svenhjol/charm/base/CharmAsmHooks.java
@@ -1,12 +1,19 @@
 package svenhjol.charm.base;
 
+import net.minecraft.item.ItemStack;
 import svenhjol.charm.Charm;
 import svenhjol.charm.tweaks.feature.RemovePotionGlint;
+import svenhjol.charm.tweaks.feature.StackablePotions;
 
 public class CharmAsmHooks
 {
     public static boolean removePotionGlint()
     {
         return Charm.hasFeature(RemovePotionGlint.class);
+    }
+
+    public static boolean checkBrewingStandStack(ItemStack stack)
+    {
+        return Charm.hasFeature(StackablePotions.class);
     }
 }
