fa97f81022bc1f7a358854241f25fd27bb244021
Sven <sven.hjol@protonmail.com>
2019-08-12 20:15:11 +0000

    Added playSoundAtPos helper method

--- src/main/java/svenhjol/meson/helper/SoundHelper.java
+++ src/main/java/svenhjol/meson/helper/SoundHelper.java
@@ -4,12 +4,28 @@ import com.google.common.collect.Multimap;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.ISound;
 import net.minecraft.client.audio.SoundHandler;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class SoundHelper
 {
+    @OnlyIn(Dist.CLIENT)
+    public static void playSoundAtPos(BlockPos pos, SoundEvent sound, float volume, float pitch)
+    {
+        playSoundAtPos(pos, sound, SoundCategory.AMBIENT, volume, pitch);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void playSoundAtPos(BlockPos pos, SoundEvent sound, SoundCategory category, float volume, float pitch)
+    {
+        ClientWorld world = ClientHelper.getClientWorld();
+        world.playSound(pos.getX(), pos.getY(), pos.getZ(), sound, category, volume, pitch, true);
+    }
+
     public static SoundHandler getSoundHandler()
     {
         return Minecraft.getInstance().getSoundHandler();
