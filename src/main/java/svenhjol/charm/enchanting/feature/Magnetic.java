b96b6853f0073ad11d69c78d5a9ef38694cd145f
Sven <sven.hjol@protonmail.com>
2019-08-12 18:04:39 +0000

    Rewrite Magnetic logic

--- src/main/java/svenhjol/charm/enchanting/feature/Magnetic.java
+++ src/main/java/svenhjol/charm/enchanting/feature/Magnetic.java
@@ -1,13 +1,18 @@
 package svenhjol.charm.enchanting.feature;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.SoundEvents;
-import net.minecraft.server.management.PlayerInteractionManager;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -15,33 +20,35 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import svenhjol.charm.enchanting.enchantment.EnchantmentMagnetic;
 import svenhjol.charm.enchanting.message.MessageMagneticPickup;
 import svenhjol.meson.Feature;
-import svenhjol.meson.handler.ClientHandler;
+import svenhjol.meson.Meson;
 import svenhjol.meson.handler.NetworkHandler;
 import svenhjol.meson.helper.EnchantmentHelper;
 import svenhjol.meson.helper.PlayerHelper;
 import svenhjol.meson.helper.SoundHelper;
+import svenhjol.meson.helper.WorldHelper;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * @see net.minecraftforge.items.ItemHandlerHelper
+ */
 public class Magnetic extends Feature
 {
     public static EnchantmentMagnetic enchantment;
     public static int minEnchantability;
-    public static PlayerInteractionManager manager;
-    public static List<EntityItem> drops = new ArrayList<>();
+    public static Map<BlockPos, EntityPlayer> dropmap = new HashMap<>();
 
     @Override
     public String getDescription()
     {
-        return "Tools with the Magnetic enchantment automatically add dropped items to your inventory.";
+        return "Tools with the Magnetic enchantment automatically pick up drops.";
     }
 
     @Override
     public void configure()
     {
         super.configure();
-
         minEnchantability = 15;
     }
 
@@ -54,57 +61,80 @@ public class Magnetic extends Feature
     }
 
     @SubscribeEvent
-    public void onEntityCreated(EntityJoinWorldEvent event)
+    public void onBreak(BlockEvent.BreakEvent event)
     {
-        if (!event.getWorld().isRemote
-            && event.getEntity() instanceof EntityItem
-            && !event.isCanceled()
-            && manager != null
-        ) {
-            EntityItem item = (EntityItem)event.getEntity();
-            drops.add(item);
-            event.setCanceled(true);
+        if (EnchantmentHelper.hasEnchantment(Magnetic.enchantment, event.getPlayer().getHeldItemMainhand())) {
+            dropmap.put(event.getPos(), event.getPlayer());
+        } else {
+            dropmap.values().remove(event.getPlayer());
         }
+        Meson.log( dropmap.size() );
     }
 
-    public static void startCollectingDrops(PlayerInteractionManager manager)
+    @SubscribeEvent
+    public void onEntityCreate(EntityJoinWorldEvent event)
     {
-        if (EnchantmentHelper.hasEnchantment(enchantment, manager.player.getHeldItemMainhand())) {
-            Magnetic.manager = manager;
-        }
-    }
+        if (event.getEntity() instanceof EntityItem && !event.getWorld().isRemote) {
+            int range = 5;
+            BlockPos got = null;
 
-    public static void stopCollectingDrops()
-    {
-        if (manager != null && !drops.isEmpty()) {
-            ArrayList<EntityItem> copy = new ArrayList<>(drops);
-            drops.clear();
-
-            EntityPlayerMP player = manager.player;
-            for (EntityItem drop : copy) {
-                EntityItem fake = new EntityItem(player.world, player.posX, player.posY, player.posZ);
-                fake.setItem(drop.getItem());
-                if (!MinecraftForge.EVENT_BUS.post(new EntityItemPickupEvent(player, fake))) {
-                    boolean result = PlayerHelper.addOrDropStack(player, drop.getItem());
-                    if (result) {
-                        NetworkHandler.INSTANCE.sendTo(new MessageMagneticPickup(player.getPosition()), player);
-                    }
+            for (BlockPos pos : dropmap.keySet()) {
+                double dist = WorldHelper.getDistanceSq(pos, event.getEntity().getPosition());
+                if (dist <= range) {
+                    got = pos;
+                    break;
                 }
             }
 
-            manager = null;
+            if (got != null) {
+                EntityPlayer player = dropmap.get(got);
+                EntityItem fake = new EntityItem(player.world, player.posX, player.posY, player.posZ);
+                ItemStack item = ((EntityItem) event.getEntity()).getItem();
+                fake.setItem(item);
+                dropmap.remove(got);
+
+                if (!MinecraftForge.EVENT_BUS.post(new EntityItemPickupEvent(player, fake))) {
+                    if (PlayerHelper.addOrDropStack(player, item)) {
+                        NetworkHandler.INSTANCE.sendTo(new MessageMagneticPickup(player.getPosition()), (EntityPlayerMP)player);
+                    }
+                }
+
+                event.setCanceled(true);
+            }
         }
     }
 
-    @SideOnly(Side.CLIENT)
-    public static void effectPickup(BlockPos pos)
-    {
-        SoundHelper.playSoundAtPos(ClientHandler.getWorldClient(), pos, SoundEvents.ENTITY_ITEM_PICKUP, 1.0f, 1.0f);
-    }
+//    @SubscribeEvent
+//    public void onPlayerTick(PlayerTickEvent event)
+//    {
+//        if (event.phase == Phase.START
+//            && event.side.isServer()
+//            && EnchantmentHelper.hasEnchantment(enchantment, event.player.getHeldItemMainhand())
+//        ) {
+//            double x = event.player.posX;
+//            double y = event.player.posY;
+//            double z = event.player.posZ;
+//
+//            List<EntityItem> items = event.player.world.getEntitiesWithinAABB(EntityItem.class, new AxisAlignedBB(
+//                x - range, y - range, z - range, x + range, y + range, z + range));
+//
+//            for (EntityItem item : items) {
+//                if (item.getItem().isEmpty() || item.isDead) continue;
+//                item.setPosition(x, y, z);
+//            }
+//        }
+//    }
 
     @Override
     public boolean hasSubscriptions()
     {
         return true;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public static void effectPickup(BlockPos pos)
+    {
+        WorldClient world = Minecraft.getMinecraft().world;
+        SoundHelper.playSoundAtPos(world, pos, SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.BLOCKS,0.85F, ((world.rand.nextFloat() - world.rand.nextFloat()) * 0.7F + 1.0F) * 2.0F);
     }
 }
