dd081d4868c7f45be207ff1cb50a4c22dedac60a
Sven <sven.hjol@protonmail.com>
2019-08-13 13:06:53 +0000

    Added hasFeature back to main Charm class for convenience

--- src/main/java/svenhjol/charm/Charm.java
+++ src/main/java/svenhjol/charm/Charm.java
@@ -4,16 +4,24 @@ import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import svenhjol.charm.base.ClientProxy;
 import svenhjol.charm.base.CommonProxy;
+import svenhjol.meson.Feature;
+import svenhjol.meson.MesonLoader;
 import svenhjol.meson.iface.IMesonSidedProxy;
 
 @Mod(Charm.MOD_ID)
 public class Charm
 {
     public static final String MOD_ID = "charm";
-    public static IMesonSidedProxy proxy = DistExecutor.runForDist(() -> ClientProxy::new, () -> CommonProxy::new);
+    public static IMesonSidedProxy proxy = DistExecutor.runForDist(
+        () -> ClientProxy::new, () -> CommonProxy::new);
 
     public Charm()
     {
         proxy.init();
+    }
+
+    public static boolean hasFeature(Class<? extends Feature> feature)
+    {
+        return MesonLoader.hasFeature(feature); /* @todo should be able to check instance name? */
     }
 }
