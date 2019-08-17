bdd54c073260149db08d9f32a6b5feb344cfd95d
Sven <sven.hjol@protonmail.com>
2019-08-17 18:22:06 +0000

    Added helper method getBiomeAtPos because world.getBiome seems broken

--- src/main/java/svenhjol/meson/helper/WorldHelper.java
+++ src/main/java/svenhjol/meson/helper/WorldHelper.java
@@ -1,6 +1,8 @@
 package svenhjol.meson.helper;
 
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 
 public class WorldHelper
 {
@@ -11,5 +13,11 @@ public class WorldHelper
         double d2 = d0 - pos2.getX();
         double d3 = d1 - pos2.getZ();
         return d2 * d2 + d3 * d3;
+    }
+
+    public static Biome getBiomeAtPos(World world, BlockPos pos)
+    {
+        // world.getBiome() suffers from infinite badness when game loading
+        return world.getChunkProvider().getChunkGenerator().getBiomeProvider().getBiome(pos);
     }
 }
