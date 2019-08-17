81596d516101a9d0e343418299c0528bce890534
Sven <sven.hjol@protonmail.com>
2019-08-17 17:36:48 +0000

    Added jungle and swamp villages

--- src/main/java/svenhjol/charm/world/CharmWorld.java
+++ src/main/java/svenhjol/charm/world/CharmWorld.java
@@ -1,6 +1,7 @@
 package svenhjol.charm.world;
 
 import svenhjol.charm.world.feature.CompassBinding;
+import svenhjol.charm.world.feature.MoreVillageBiomes;
 import svenhjol.charm.world.feature.NetherGoldDeposits;
 import svenhjol.charm.world.feature.StructureMaps;
 import svenhjol.meson.Module;
@@ -10,6 +11,7 @@ public class CharmWorld extends Module
     public CharmWorld()
     {
         features.add(new CompassBinding());
+        features.add(new MoreVillageBiomes());
         features.add(new NetherGoldDeposits());
         features.add(new StructureMaps());
     }
---
+++ src/main/java/svenhjol/charm/world/feature/MoreVillageBiomes.java
@@ -0,0 +1,68 @@
+package svenhjol.charm.world.feature;
+
+import net.minecraft.entity.merchant.villager.VillagerData;
+import net.minecraft.entity.merchant.villager.VillagerEntity;
+import net.minecraft.entity.villager.IVillagerType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biomes;
+import net.minecraft.world.gen.feature.structure.VillageConfig;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import svenhjol.meson.Feature;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static net.minecraft.world.gen.feature.Feature.VILLAGE;
+
+public class MoreVillageBiomes extends Feature
+{
+    public static List<Biome> biomes = new ArrayList<>();
+
+    @Override
+    public void configure()
+    {
+        super.configure();
+
+        biomes.addAll(Arrays.asList(Biomes.JUNGLE, Biomes.SWAMP));
+    }
+
+    @Override
+    public void init()
+    {
+        super.init();
+
+        // there isn't dedicated structure pieces for jungles and swamps so just use plains
+        biomes.forEach(biome -> biome.addStructure(VILLAGE, new VillageConfig("village/plains/town_centers", 6)));
+    }
+
+    @SubscribeEvent
+    public void onSpawn(EntityJoinWorldEvent event)
+    {
+        if (!event.getWorld().isRemote
+            && event.getEntity() instanceof VillagerEntity
+            && event.getEntity().addedToChunk
+            && event.getEntity().ticksExisted == 0
+        ) {
+            VillagerEntity villager = (VillagerEntity)event.getEntity();
+            VillagerData data = villager.getVillagerData();
+
+            if (data.getType() == IVillagerType.PLAINS) {
+                BlockPos pos = event.getEntity().getPosition();
+                Biome biome = event.getWorld().getChunkProvider().getChunkGenerator().getBiomeProvider().getBiome(pos);
+
+                if (biomes.contains(biome)) {
+                    villager.setVillagerData(data.withType(IVillagerType.byBiome(biome)));
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean hasSubscriptions()
+    {
+        return true;
+    }
+}
