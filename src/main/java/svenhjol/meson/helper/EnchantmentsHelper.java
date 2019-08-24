bca33423479ef60d6e26657f9271a97255f661f5
Sven <sven.hjol@protonmail.com>
2019-08-24 15:59:50 +0000

    Added curse checking and enchantment removal to EnchantmentsHelper

--- src/main/java/svenhjol/meson/helper/EnchantmentsHelper.java
+++ src/main/java/svenhjol/meson/helper/EnchantmentsHelper.java
@@ -4,9 +4,35 @@ import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.EnchantedBookItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.ListNBT;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class EnchantmentsHelper
 {
+    public static List<Enchantment> getItemCurses(ItemStack item)
+    {
+        ListNBT tags = item.getEnchantmentTagList();
+        List<Enchantment> curses = new ArrayList<>();
+
+        if (!tags.isEmpty()) {
+            Map<Enchantment, Integer> enchants = EnchantmentHelper.getEnchantments(item);
+
+            for (Map.Entry<Enchantment, Integer> entry : enchants.entrySet()) {
+                Enchantment enchantment = entry.getKey();
+                if (enchantment == null) continue;
+                if (enchantment.isCurse()) {
+                    curses.add(enchantment);
+                }
+            }
+        }
+
+        return curses;
+    }
+
     public static boolean hasEnchantment(Enchantment enchantment, ItemStack stack)
     {
         if (stack.getItem() instanceof EnchantedBookItem) {
@@ -14,6 +40,24 @@ public class EnchantmentsHelper
             return false;
         } else {
             return EnchantmentHelper.getEnchantmentLevel(enchantment, stack) > 0;
+        }
+    }
+
+    public static void removeEnchantment(Enchantment enchantment, ItemStack stack)
+    {
+        if (hasEnchantment(enchantment, stack)) {
+            Map<Enchantment, Integer> enchants = EnchantmentHelper.getEnchantments(stack);
+            enchants.remove(enchantment);
+            EnchantmentHelper.setEnchantments(enchants, stack);
+        }
+    }
+
+    public static void removeRandomCurse(ItemStack stack)
+    {
+        List<Enchantment> curses = getItemCurses(stack);
+        if (curses.size() > 0) {
+            Enchantment curseToRemove = curses.get(new Random().nextInt(curses.size()));
+            removeEnchantment(curseToRemove, stack);
         }
     }
 }
