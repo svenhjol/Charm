a97ec3a739c6ba2d4bbbbc46b32cab92894d7000
Sven <sven.hjol@protonmail.com>
2019-08-24 15:59:30 +0000

    Added potion bottle helper methods

--- src/main/java/svenhjol/meson/helper/PotionHelper.java
+++ src/main/java/svenhjol/meson/helper/PotionHelper.java
@@ -4,6 +4,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionUtils;
+import net.minecraft.potion.Potions;
 
 public class PotionHelper
 {
@@ -13,4 +14,22 @@ public class PotionHelper
         PotionUtils.addPotionToItemStack(out, potion);
         return out;
     }
+
+    public static ItemStack getPotionBottle(int amount, Potion type)
+    {
+        ItemStack out = new ItemStack(Items.POTION, amount);
+        PotionUtils.addPotionToItemStack(out, type);
+        return out;
+    }
+
+    public static ItemStack getFilledWaterBottle()
+    {
+        return getFilledWaterBottle(1);
+    }
+
+    public static ItemStack getFilledWaterBottle(int amount)
+    {
+        return getPotionBottle(amount, Potions.WATER);
+    }
+
 }
