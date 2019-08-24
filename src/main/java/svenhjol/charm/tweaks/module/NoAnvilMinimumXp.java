6e0de56f83fdda8b3257644474079ad871f9caf8
Sven <sven.hjol@protonmail.com>
2019-08-24 14:21:27 +0000

    Added No Anvil Minimum XP

---
+++ src/main/java/svenhjol/charm/tweaks/module/NoAnvilMinimumXp.java
@@ -0,0 +1,11 @@
+package svenhjol.charm.tweaks.module;
+
+import svenhjol.charm.Charm;
+import svenhjol.charm.base.CharmCategories;
+import svenhjol.meson.MesonModule;
+import svenhjol.meson.iface.Module;
+
+@Module(mod = Charm.MOD_ID, category = CharmCategories.TWEAKS)
+public class NoAnvilMinimumXp extends MesonModule
+{
+}
