9910aece4b420c0dd03ae0426f3abfb6a919daff
Sven <sven.hjol@protonmail.com>
2019-08-24 14:22:12 +0000

    Crafting -> Building/Decoration Added Smithing category

--- src/main/java/svenhjol/charm/base/CharmCategories.java
+++ src/main/java/svenhjol/charm/base/CharmCategories.java
@@ -4,9 +4,10 @@ public class CharmCategories
 {
     public static final String AUTOMATION = "Automation";
     public static final String BREWING = "Brewing";
-    public static final String CRAFTING = "Crafting";
+    public static final String BUILDING = "Building";
     public static final String DECORATION = "Decoration";
     public static final String ENCHANTING = "Enchanting";
+    public static final String SMITHING = "Smithing";
     public static final String TWEAKS = "Tweaks";
     public static final String WORLD = "World";
 }
