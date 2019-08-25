0a0cb6e371223c4aa9e6cb4c201976193d1807b5
Sven <sven.hjol@protonmail.com>
2019-08-25 01:18:49 +0000

    Debug log, doesn't actually check for debug flag yet

--- src/main/java/svenhjol/meson/Meson.java
+++ src/main/java/svenhjol/meson/Meson.java
@@ -6,6 +6,13 @@ public class Meson
 {
     public static final String MOD_ID = "Meson";
 
+    public static void debug(Object... out)
+    {
+        for (Object obj : out) {
+            LogManager.getLogger(MOD_ID).debug(obj);
+        }
+    }
+
     public static void log(Object... out)
     {
         for (Object obj : out) {
