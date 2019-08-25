835c97fd1fe08aa05fb1e82f2aadfa1b783e3248
Sven <sven.hjol@protonmail.com>
2019-08-25 01:19:17 +0000

    Added "Remove nitwits" - so much easier in 1.14!

---
+++ src/main/java/svenhjol/charm/tweaks/module/RemoveNitwits.java
@@ -0,0 +1,33 @@
+package svenhjol.charm.tweaks.module;
+
+import net.minecraft.entity.merchant.villager.VillagerData;
+import net.minecraft.entity.merchant.villager.VillagerEntity;
+import net.minecraft.entity.merchant.villager.VillagerProfession;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import svenhjol.charm.Charm;
+import svenhjol.charm.base.CharmCategories;
+import svenhjol.meson.Meson;
+import svenhjol.meson.MesonModule;
+import svenhjol.meson.iface.Module;
+
+@Module(mod = Charm.MOD_ID, category = CharmCategories.TWEAKS, hasSubscriptions = true)
+public class RemoveNitwits extends MesonModule
+{
+    @SubscribeEvent
+    public void onVillagerSpawn(EntityJoinWorldEvent event)
+    {
+        if (!event.isCanceled()
+            && !event.getWorld().isRemote
+            && event.getEntity() instanceof VillagerEntity
+        ) {
+            VillagerEntity villager = (VillagerEntity)event.getEntity();
+            VillagerData data = villager.getVillagerData();
+
+            if (data.getProfession() == VillagerProfession.NITWIT) {
+                villager.setVillagerData(data.withProfession(VillagerProfession.NONE));
+                Meson.debug("Changed nitwit's profession to NONE");
+            }
+        }
+    }
+}
