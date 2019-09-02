ff1d51d456ccbc711c242b7685647f734ac754b7
Sven <sven.hjol@protonmail.com>
2019-09-02 12:30:44 +0000

    Removed registerTrades() method from module in favor of events

--- src/main/java/svenhjol/meson/MesonModule.java
+++ src/main/java/svenhjol/meson/MesonModule.java
@@ -1,16 +1,11 @@
 package svenhjol.meson;
 
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
-import net.minecraft.entity.merchant.villager.VillagerProfession;
-import net.minecraft.entity.merchant.villager.VillagerTrades.ITrade;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.config.ModConfig.ModConfigEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
-
-import java.util.List;
 
 public abstract class MesonModule
 {
@@ -56,10 +51,5 @@ public abstract class MesonModule
     public void loadComplete(FMLLoadCompleteEvent event)
     {
         // do final things
-    }
-
-    public void registerTrades(Int2ObjectMap<List<ITrade>> trades, VillagerProfession profession)
-    {
-        // register villager trades based on profession
     }
 }
