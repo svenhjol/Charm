fc63da0a4cd86dcfa1b40083250d02bbb79d818e
Sven <sven.hjol@protonmail.com>
2019-09-06 12:14:53 +0000

    Records stopping music is now client side only

--- src/main/java/svenhjol/charm/tweaks/module/RecordsStopBackgroundMusic.java
+++ src/main/java/svenhjol/charm/tweaks/module/RecordsStopBackgroundMusic.java
@@ -20,13 +20,14 @@ import svenhjol.meson.MesonModule;
 import svenhjol.meson.helper.SoundHelper;
 import svenhjol.meson.iface.Module;
 
-@Module(mod = Charm.MOD_ID, category = CharmCategories.TWEAKS, hasSubscriptions = true,
+@Module(mod = Charm.MOD_ID, category = CharmCategories.TWEAKS, hasSubscriptions = true, server = false,
     description = "Stops the background music when playing a music disc in a jukebox.")
 public class RecordsStopBackgroundMusic extends MesonModule
 {
     private int ticks; // delay
     private ISound music = null; // music to stop
 
+    @OnlyIn(Dist.CLIENT)
     @SubscribeEvent
     public void onBlockInteract(RightClickBlock event)
     {
