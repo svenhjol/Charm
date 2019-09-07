e4f3d5041197c251e6a5c1466422ac922858e9ba
Sven <sven.hjol@protonmail.com>
2019-09-07 01:19:26 +0000

    Simpler potion recipe registration for coffee and decay

--- src/main/java/svenhjol/charm/brewing/potion/CoffeePotion.java
+++ src/main/java/svenhjol/charm/brewing/potion/CoffeePotion.java
@@ -1,9 +1,7 @@
 package svenhjol.charm.brewing.potion;
 
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.PotionUtils;
 import net.minecraft.potion.Potions;
 import svenhjol.charm.brewing.module.Coffee;
 import svenhjol.meson.MesonModule;
@@ -15,7 +13,6 @@ public class CoffeePotion extends MesonPotion
     {
         super(module, "coffee_potion", new EffectInstance(Coffee.effect, Coffee.duration * 20));
 
-        setPotionBase(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.WATER));
-        setPotionReagant(new ItemStack(Items.COCOA_BEANS));
+        registerRecipe(Potions.WATER, Items.COCOA_BEANS);
     }
 }
--- src/main/java/svenhjol/charm/brewing/potion/DecayPotion.java
+++ src/main/java/svenhjol/charm/brewing/potion/DecayPotion.java
@@ -1,9 +1,7 @@
 package svenhjol.charm.brewing.potion;
 
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.PotionUtils;
 import net.minecraft.potion.Potions;
 import svenhjol.charm.brewing.module.Decay;
 import svenhjol.meson.MesonModule;
@@ -15,7 +13,6 @@ public class DecayPotion extends MesonPotion
     {
         super(module, "decay_potion", new EffectInstance(Decay.effect, Decay.duration * 20));
 
-        setPotionBase(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.AWKWARD));
-        setPotionReagant(new ItemStack(Items.WITHER_ROSE));
+        registerRecipe(Potions.AWKWARD, Items.WITHER_ROSE);
     }
 }
