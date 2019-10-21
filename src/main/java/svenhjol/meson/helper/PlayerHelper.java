5f210e18f8d7b9944e823f3d8ddaf1985519a2a5
Sven <sven.hjol@protonmail.com>
2019-10-21 14:26:39 +0100

    Sorting out teleport methods

--- src/main/java/svenhjol/meson/helper/PlayerHelper.java
+++ src/main/java/svenhjol/meson/helper/PlayerHelper.java
@@ -1,14 +1,19 @@
 package svenhjol.meson.helper;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.server.TicketType;
+
+import java.util.function.Consumer;
 
 @SuppressWarnings("unused")
 public class PlayerHelper
@@ -52,21 +57,64 @@ public class PlayerHelper
      * Basic way to teleport a player to co-ordinate in a dimension.
      * If the player is not in the specified dimension they will be transferred first.
      */
-    public static void teleportPlayer(PlayerEntity player, BlockPos pos, int dim)
+    public static void teleport(PlayerEntity player, BlockPos pos, int dim)
+    {
+        teleport(player, pos, dim, p -> {});
+    }
+
+    public static void teleport(PlayerEntity player, BlockPos pos, int dim, Consumer<BlockPos> onTeleport)
     {
         World world = player.world;
+        if (world.isRemote) return;
 
-        if (player.dimension.getId() != dim) {
-            if (!world.isRemote && !player.isPassenger() && !player.isBeingRidden() && player.isNonBoss()) {
-                DimensionType dimension = DimensionType.getById(dim);
-                if (dimension != null) {
-                    player.changeDimension(dimension);
-                }
-            }
-        }
+        changeDimension(player, dim);
 
         ((ServerPlayerEntity)player).teleport((ServerWorld)world, pos.getX(), pos.getY(), pos.getZ(), player.rotationYaw, player.rotationPitch);
         BlockPos updateDest = world.getHeight(Heightmap.Type.MOTION_BLOCKING, pos);
         player.setPositionAndUpdate(updateDest.getX(), updateDest.getY() + 1, updateDest.getZ()); // TODO check landing block
+
+        onTeleport.accept(player.getPosition());
+    }
+
+    public static void teleportSurface(PlayerEntity player, BlockPos pos, int dim)
+    {
+        teleportSurface(player, pos, dim, p -> {});
+    }
+
+    public static void teleportSurface(PlayerEntity player, BlockPos pos, int dim, Consumer<BlockPos> onTeleport)
+    {
+        World world = player.world;
+        if (world.isRemote) return;
+
+        // TODO check this actually does anything - it's supposed to preload the chunk
+        ChunkPos chunkpos = new ChunkPos(new BlockPos(pos.getX(), pos.getY(), pos.getZ()));
+        ((ServerWorld)world).getChunkProvider().func_217228_a(TicketType.POST_TELEPORT, chunkpos, 1, player.getEntityId());
+
+        Minecraft.getInstance().deferTask(() -> {
+            teleport(player, pos, dim, (p) -> {
+                for (int y = world.getHeight(); y > 0; y--) {
+                    BlockPos pp = new BlockPos(p.getX(), y, p.getZ());
+                    if (world.getBlockState(pp).isAir()
+                        && !world.getBlockState(pp.down()).isAir()
+                    ) {
+                        player.setPositionAndUpdate(pp.getX(), y, pp.getZ());
+                        onTeleport.accept(player.getPosition());
+                        break;
+                    }
+                }
+            });
+        });
+    }
+
+    public static void changeDimension(PlayerEntity player, int dim)
+    {
+        if (player.world.isRemote || player.dimension.getId() == dim) return;
+
+        if (!player.isPassenger() && !player.isBeingRidden() && player.isNonBoss()) {
+            DimensionType dimension = DimensionType.getById(dim);
+            if (dimension != null) {
+                player.changeDimension(dimension);
+            }
+        }
     }
 }
