9d77483fbb6e9fb89a867eeed67d29714a92d960
Sven <sven.hjol@protonmail.com>
2019-11-07 17:32:51 +0000

    Moonstone block description

--- src/main/java/svenhjol/charm/building/module/BlockOfMoonstone.java
+++ src/main/java/svenhjol/charm/building/module/BlockOfMoonstone.java
@@ -10,7 +10,9 @@ import svenhjol.meson.iface.Module;
 import java.util.ArrayList;
 import java.util.List;
 
-@Module(mod = Charm.MOD_ID, category = CharmCategories.BUILDING)
+@Module(mod = Charm.MOD_ID, category = CharmCategories.BUILDING,
+    description = "A decorative block made from 4 moonstones of the same color.\n" +
+        "Moonstone blocks change their light levels according to the moon phase.")
 public class BlockOfMoonstone extends MesonModule
 {
     public static List<MoonstoneBlock> blocks = new ArrayList<>();
