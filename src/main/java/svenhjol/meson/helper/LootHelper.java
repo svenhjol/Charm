3a470f343a43489ed7ac58aa8be97e6d3a8a9c77
Sven <sven.hjol@protonmail.com>
2019-12-20 23:23:13 +0000

    Method to fetch all vanilla and custom chests

--- src/main/java/svenhjol/meson/helper/LootHelper.java
+++ src/main/java/svenhjol/meson/helper/LootHelper.java
@@ -1,14 +1,20 @@
 package svenhjol.meson.helper;
 
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.storage.loot.LootEntry;
 import net.minecraft.world.storage.loot.LootPool;
 import net.minecraft.world.storage.loot.LootTable;
+import net.minecraft.world.storage.loot.LootTables;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LootHelper
 {
+    public static List<ResourceLocation> customTables = new ArrayList<>();
+
     public static void addTableEntry(LootTable table, LootEntry entry)
     {
         List<LootPool> pools = ObfuscationReflectionHelper.getPrivateValue(LootTable.class, table, "field_186466_c");
@@ -18,5 +24,19 @@ public class LootHelper
         List<LootEntry> entries = ObfuscationReflectionHelper.getPrivateValue(LootPool.class, pool, "field_186453_a");
         if (entries == null) return;
         entries.add(entry);
+    }
+
+    public static List<ResourceLocation> getLootTables()
+    {
+        List<ResourceLocation> tables = new ArrayList<>();
+
+        List<ResourceLocation> vanillaChests = LootTables.func_215796_a().stream()
+            .filter(r -> r.getPath().contains("chest"))
+            .collect(Collectors.toList());
+
+        tables.addAll(vanillaChests);
+        tables.addAll(customTables);
+
+        return tables;
     }
 }
