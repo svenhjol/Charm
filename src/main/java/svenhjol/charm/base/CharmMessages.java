4896bed1e6e1a419d34baf8e0a48b3b56dc82917
Sven <sven.hjol@protonmail.com>
2020-04-03 23:02:05 +0000

    Delete rune portal

--- src/main/java/svenhjol/charm/base/CharmMessages.java
+++ src/main/java/svenhjol/charm/base/CharmMessages.java
@@ -2,7 +2,6 @@ package svenhjol.charm.base;
 
 import svenhjol.charm.brewing.message.ClientCakeAction;
 import svenhjol.charm.tools.message.ClientGlowingAction;
-import svenhjol.charm.world.message.ClientRunePortalAction;
 import svenhjol.meson.MesonInstance;
 import svenhjol.meson.handler.PacketHandler;
 
@@ -12,6 +11,5 @@ public class CharmMessages {
 
         handler.register(ClientCakeAction.class, ClientCakeAction::encode, ClientCakeAction::decode, ClientCakeAction.Handler::handle);
         handler.register(ClientGlowingAction.class, ClientGlowingAction::encode, ClientGlowingAction::decode, ClientGlowingAction.Handler::handle);
-        handler.register(ClientRunePortalAction.class, ClientRunePortalAction::encode, ClientRunePortalAction::decode, ClientRunePortalAction.Handler::handle);
     }
 }
--- src/main/java/svenhjol/charm/base/compat/IQuarkCompat.java
+++ src/main/java/svenhjol/charm/base/compat/IQuarkCompat.java
@@ -1,11 +1,9 @@
 package svenhjol.charm.base.compat;
 
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.event.AnvilUpdateEvent;
 import svenhjol.meson.enums.ColorVariant;
 
 public interface IQuarkCompat {
-    void onTallowAnvilUpdate(AnvilUpdateEvent event);
     boolean hasColorRuneModule();
     boolean isRune(ItemStack stack);
     ColorVariant getRuneColor(ItemStack stack);
