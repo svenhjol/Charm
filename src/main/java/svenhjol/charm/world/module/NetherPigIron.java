b12469dbfd3106befee8b0e84e28136cf0c1671e
Sven <sven.hjol@protonmail.com>
2020-04-03 23:02:28 +0000

    Update feature code for 1.15

--- src/main/java/svenhjol/charm/world/module/NetherPigIron.java
+++ src/main/java/svenhjol/charm/world/module/NetherPigIron.java
@@ -2,7 +2,9 @@ package svenhjol.charm.world.module;
 
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.placement.ConfiguredPlacement;
 import net.minecraft.world.gen.placement.CountRangeConfig;
 import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -14,8 +16,6 @@ import svenhjol.charm.world.item.PigIronNuggetItem;
 import svenhjol.meson.MesonModule;
 import svenhjol.meson.iface.Config;
 import svenhjol.meson.iface.Module;
-
-import static net.minecraft.world.gen.feature.Feature.ORE;
 
 @Module(mod = Charm.MOD_ID, category = CharmCategories.WORLD,
     description = "Pig iron ore can be broken for pig iron nuggets or smelted into regular iron.")
@@ -29,27 +29,27 @@ public class NetherPigIron extends MesonModule {
     @Config(name = "Cluster count")
     public static int clusterCount = 12;
 
+    public static ConfiguredPlacement<CountRangeConfig> placement = null;
+
+    public static OreFeatureConfig config = null;
+
     @Override
     public void init() {
         block = new PigIronOreBlock(this);
         item = new PigIronNuggetItem(this);
+        placement = Placement.COUNT_RANGE.configure(new CountRangeConfig(clusterCount, 10, 20, 128));
+        config = new OreFeatureConfig(
+            OreFeatureConfig.FillerBlockType.NETHERRACK,
+            block.getDefaultState(),
+            veinSize
+        );
     }
 
     @Override
     public void onCommonSetup(FMLCommonSetupEvent event) {
         for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
             if (biome.getCategory() != Biome.Category.NETHER) continue;
-
-            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
-                Biome.createDecoratedFeature(ORE,
-                    new OreFeatureConfig(
-                        OreFeatureConfig.FillerBlockType.NETHERRACK,
-                        block.getDefaultState(),
-                        veinSize
-                    ),
-                    Placement.COUNT_RANGE,
-                    new CountRangeConfig(clusterCount, 10, 0, 128))
-            );
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, Feature.ORE.withConfiguration(config).withPlacement(placement));
         }
     }
 }
