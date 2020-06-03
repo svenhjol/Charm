70152747c808f1c26322d99426e6b3972bb00342
Sven <sven.hjol@protonmail.com>
2020-06-03 23:12:39 +0000

    Helper method to capitalize a string of words

--- src/main/java/svenhjol/meson/helper/StringHelper.java
+++ src/main/java/svenhjol/meson/helper/StringHelper.java
@@ -38,4 +38,19 @@ public class StringHelper {
     public static TextFormatting getTextFormattingByDyeDamage(int dyeDamage) {
         return TextFormatting.fromColorIndex(dyeTextMap.get(dyeDamage));
     }
+
+    // see https://stackoverflow.com/a/1892812
+    public static String capitalizeString(String string) {
+        char[] chars = string.toLowerCase().toCharArray();
+        boolean found = false;
+        for (int i = 0; i < chars.length; i++) {
+            if (!found && Character.isLetter(chars[i])) {
+                chars[i] = Character.toUpperCase(chars[i]);
+                found = true;
+            } else if (Character.isWhitespace(chars[i]) || chars[i]=='.' || chars[i]=='\'') { // You can add other chars here
+                found = false;
+            }
+        }
+        return String.valueOf(chars);
+    }
 }
