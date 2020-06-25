ce3ec9e5aa89e7d36ca15a2c20968e49ebe15ac1
Sven <sven.hjol@protonmail.com>
2020-06-25 23:04:02 +0000

    Register rainbow lantern block

--- src/main/java/svenhjol/charm/decoration/block/ColoredLanternBlock.java
+++ src/main/java/svenhjol/charm/decoration/block/ColoredLanternBlock.java
@@ -3,11 +3,11 @@ package svenhjol.charm.decoration.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.item.DyeColor;
 import svenhjol.meson.MesonModule;
+import svenhjol.meson.enums.ColorVariant;
 
 public class ColoredLanternBlock extends BaseLanternBlock {
-    public ColoredLanternBlock(MesonModule module, DyeColor color) {
+    public ColoredLanternBlock(MesonModule module, ColorVariant color) {
         super(module, color + "_lantern", Block.Properties
             .create(Material.IRON)
             .hardnessAndResistance(3.5F)
--- src/main/java/svenhjol/charm/decoration/module/ColoredLanterns.java
+++ src/main/java/svenhjol/charm/decoration/module/ColoredLanterns.java
@@ -2,13 +2,13 @@ package svenhjol.charm.decoration.module;
 
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
-import net.minecraft.item.DyeColor;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import svenhjol.charm.Charm;
 import svenhjol.charm.base.CharmCategories;
 import svenhjol.charm.decoration.block.BaseLanternBlock;
 import svenhjol.charm.decoration.block.ColoredLanternBlock;
 import svenhjol.meson.MesonModule;
+import svenhjol.meson.enums.ColorVariant;
 import svenhjol.meson.iface.Module;
 
 import java.util.HashMap;
@@ -17,11 +17,11 @@ import java.util.Map;
 @Module(mod = Charm.MOD_ID, category = CharmCategories.DECORATION,
     description = "Gold lanterns can be combined with dye to make colored lanterns.")
 public class ColoredLanterns extends MesonModule {
-    public static Map<DyeColor, BaseLanternBlock> lanterns = new HashMap<>();
+    public static Map<ColorVariant, BaseLanternBlock> lanterns = new HashMap<>();
 
     @Override
     public void init() {
-        for (DyeColor color : DyeColor.values()) {
+        for (ColorVariant color : ColorVariant.values()) {
             lanterns.put(color, new ColoredLanternBlock(this, color));
         }
     }
