e52efb6ad9cd9c5e39bafdf85de8a0bdf7988696
Sven Hjól <sven.hjol@protonmail.com>
2020-08-17 23:56:02 +0000

    Added Feather Falling Crops

---
+++ src/main/java/svenhjol/charm/mixin/FarmlandBlockMixin.java
@@ -0,0 +1,22 @@
+package svenhjol.charm.mixin;
+
+import net.minecraft.block.FarmlandBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import svenhjol.charm.module.FeatherFallingCrops;
+
+@Mixin(FarmlandBlock.class)
+public class FarmlandBlockMixin {
+    @Inject(method = "onLandedUpon", at = @At("HEAD"), cancellable = true)
+    private void hookOnLandedUpon(World world, BlockPos pos, Entity entity, float distance, CallbackInfo ci) {
+        boolean result = FeatherFallingCrops.landedOnFarmlandBlock(entity);
+
+        if (result)
+            ci.cancel();
+    }
+}
