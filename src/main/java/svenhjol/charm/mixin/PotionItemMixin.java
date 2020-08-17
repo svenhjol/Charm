14aed362b6ee6ccfb24fffb42ab9e7e4a76b9d98
Sven Hjól <sven.hjol@protonmail.com>
2020-08-17 23:55:53 +0000

    Added Remove Potion Glint

---
+++ src/main/java/svenhjol/charm/mixin/PotionItemMixin.java
@@ -0,0 +1,22 @@
+package svenhjol.charm.mixin;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.PotionItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import svenhjol.charm.module.RemovePotionGlint;
+
+@Mixin(PotionItem.class)
+public class PotionItemMixin {
+    @Inject(
+        method = "hasGlint",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    public void hasEffectHook(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        if (RemovePotionGlint.shouldRemoveGlint())
+            cir.setReturnValue(false);
+    }
+}
---
+++ src/main/java/svenhjol/charm/module/RemovePotionGlint.java
@@ -0,0 +1,18 @@
+package svenhjol.charm.module;
+
+import svenhjol.meson.MesonModule;
+import svenhjol.meson.iface.Module;
+
+@Module(description = "Removes the potion enchantment glint so you can see what the potion color is.")
+public class RemovePotionGlint extends MesonModule {
+    private static boolean isEnabled;
+
+    @Override
+    public void init() {
+        isEnabled = this.enabled;
+    }
+
+    public static boolean shouldRemoveGlint() {
+        return isEnabled;
+    }
+}
