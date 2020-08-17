6534ae16ad6e2af1b165f7bde4479c978bef422c
Sven Hjól <sven.hjol@protonmail.com>
2020-08-17 23:03:15 +0000

    Add core module for enabling debug mode

---
+++ src/main/java/svenhjol/charm/module/Core.java
@@ -0,0 +1,11 @@
+package svenhjol.charm.module;
+
+import svenhjol.meson.MesonModule;
+import svenhjol.meson.iface.Config;
+import svenhjol.meson.iface.Module;
+
+@Module(alwaysEnabled = true, description = "Core configuration values.")
+public class Core extends MesonModule {
+    @Config(name = "Debug mode", description = "If true, routes additional debug messages into the standard game log.")
+    public static boolean debug = false;
+}
