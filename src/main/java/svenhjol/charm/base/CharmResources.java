607dd045c6b7eee4d8e4701765393cb83ef786d6
Sven Hjól <sven.hjol@protonmail.com>
2020-08-20 02:04:42 +0000

    Port CharmResources and inventory buttons

---
+++ src/main/java/svenhjol/charm/base/CharmResources.java
@@ -0,0 +1,8 @@
+package svenhjol.charm.base;
+
+import net.minecraft.util.Identifier;
+import svenhjol.charm.Charm;
+
+public class CharmResources {
+    public static final Identifier INVENTORY_BUTTONS = new Identifier(Charm.MOD_ID, "textures/gui/inventory_buttons.png");
+}
