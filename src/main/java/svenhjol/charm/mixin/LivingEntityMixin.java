4c486770848fdafaa9584e95c977efeb6961fc29
Sven Hjól <sven.hjol@protonmail.com>
2020-08-23 03:20:45 +0000

    Mixin to living entity to call drop event

--- src/main/java/svenhjol/charm/mixin/LivingEntityMixin.java
+++ src/main/java/svenhjol/charm/mixin/LivingEntityMixin.java
@@ -1,11 +1,16 @@
 package svenhjol.charm.mixin;
 
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import svenhjol.charm.event.EntityDropsCallback;
 import svenhjol.charm.module.UseTotemFromInventory;
 
 @Mixin(LivingEntity.class)
@@ -19,5 +24,13 @@ public class LivingEntityMixin {
     )
     private ItemStack hookTryUseTotem(LivingEntity livingEntity, Hand hand) {
         return UseTotemFromInventory.tryFromInventory(livingEntity, hand);
+    }
+
+    @Inject(method = "drop", at = @At("TAIL"))
+    private void hookDrop(DamageSource source, CallbackInfo ci) {
+        LivingEntity entity = (LivingEntity)(Object)this;
+        int lootingLevel = EnchantmentHelper.getLooting(entity);
+
+        EntityDropsCallback.EVENT.invoker().interact(entity, source, lootingLevel);
     }
 }
