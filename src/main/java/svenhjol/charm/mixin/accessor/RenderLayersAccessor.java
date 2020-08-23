9c30e6c06bcc333204b6b17a8588e0d6d56a2b5a
Sven Hjól <sven.hjol@protonmail.com>
2020-08-23 19:24:02 +0000

    RenderLayersAccessor to set block render types

---
+++ src/main/java/svenhjol/charm/mixin/accessor/RenderLayersAccessor.java
@@ -0,0 +1,17 @@
+package svenhjol.charm.mixin.accessor;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.RenderLayers;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@Mixin(RenderLayers.class)
+public interface RenderLayersAccessor {
+    @Accessor("BLOCKS")
+    static Map<Block, RenderLayer> getBlocks() {
+        throw new IllegalStateException();
+    }
+}
