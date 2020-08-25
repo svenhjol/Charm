c9a3011148fc2762dabc3085806b00ca4b303b66
Sven Hjól <sven.hjol@protonmail.com>
2020-08-25 01:32:25 +0000

    Simplify event return vals

--- src/main/java/svenhjol/charm/event/RenderGuiCallback.java
+++ src/main/java/svenhjol/charm/event/RenderGuiCallback.java
@@ -4,18 +4,13 @@ import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.ActionResult;
 
 public interface RenderGuiCallback {
     Event<RenderGuiCallback> EVENT = EventFactory.createArrayBacked(RenderGuiCallback.class, (listeners) -> (client, matrices, mouseX, mouseY, delta) -> {
         for (RenderGuiCallback listener : listeners) {
-            ActionResult result = listener.interact(client, matrices, mouseX, mouseY, delta);
-            if (result != ActionResult.PASS)
-                return result;
+            listener.interact(client, matrices, mouseX, mouseY, delta);
         }
-
-        return ActionResult.PASS;
     });
 
-    ActionResult interact(MinecraftClient client, MatrixStack matrices, int mouseX, int mouseY, float delta);
+    void interact(MinecraftClient client, MatrixStack matrices, int mouseX, int mouseY, float delta);
 }
--- src/main/java/svenhjol/charm/event/SetupGuiCallback.java
+++ src/main/java/svenhjol/charm/event/SetupGuiCallback.java
@@ -4,7 +4,6 @@ import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.widget.AbstractButtonWidget;
-import net.minecraft.util.ActionResult;
 
 import java.util.List;
 import java.util.function.Consumer;
@@ -12,13 +11,9 @@ import java.util.function.Consumer;
 public interface SetupGuiCallback {
     Event<SetupGuiCallback> EVENT = EventFactory.createArrayBacked(SetupGuiCallback.class, (listeners) -> (client, width, height, buttons, addButton) -> {
         for (SetupGuiCallback listener : listeners) {
-            ActionResult result = listener.interact(client, width, height, buttons, addButton);
-            if (result != ActionResult.PASS)
-                return result;
+            listener.interact(client, width, height, buttons, addButton);
         }
-
-        return ActionResult.PASS;
     });
 
-    ActionResult interact(MinecraftClient client, int width, int height, List<AbstractButtonWidget> buttons, Consumer<AbstractButtonWidget> addButton);
+    void interact(MinecraftClient client, int width, int height, List<AbstractButtonWidget> buttons, Consumer<AbstractButtonWidget> addButton);
 }
