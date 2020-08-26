6683288412fb327e82c6a610e861c212f54ea8f1
Sven Hjól <sven.hjol@protonmail.com>
2020-08-26 02:11:54 +0000

    Hooks to load common and server setup callbacks

---
+++ src/main/java/svenhjol/charm/mixin/MinecraftClientMixin.java
@@ -0,0 +1,24 @@
+package svenhjol.charm.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.resource.ResourcePackManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import svenhjol.charm.event.CommonSetupCallback;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+    @Redirect(
+        method = "<init>",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/resource/ResourcePackManager;scanPacks()V"
+        )
+    )
+    private void hookAfterScanPacks(ResourcePackManager resourcePackManager) {
+        resourcePackManager.scanPacks();
+        CommonSetupCallback.EVENT.invoker().interact();
+    }
+}
+
