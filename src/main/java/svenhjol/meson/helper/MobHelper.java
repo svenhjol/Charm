0a1345bd118d8825310cff7506b991572617eec9
Sven Hjól <sven.hjol@protonmail.com>
2020-08-27 03:03:50 +0000

    Helper method to spawn entities. Use this instead of vanilla for now

--- src/main/java/svenhjol/meson/helper/MobHelper.java
+++ src/main/java/svenhjol/meson/helper/MobHelper.java
@@ -1,8 +1,13 @@
 package svenhjol.meson.helper;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.goal.GoalSelector;
 import net.minecraft.entity.ai.goal.PrioritizedGoal;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import svenhjol.charm.mixin.accessor.GoalSelectorAccessor;
 import svenhjol.charm.mixin.accessor.MobEntityAccessor;
 
@@ -15,5 +20,9 @@ public class MobHelper {
 
     public static GoalSelector getGoalSelector(MobEntity mob) {
         return ((MobEntityAccessor)mob).getGoalSelector();
+    }
+
+    public static <T extends Entity> T spawn(EntityType<T> type, ServerWorld world, BlockPos pos, SpawnReason reason) {
+        return type.create(world, null, null, null, pos, reason, false, false);
     }
 }
