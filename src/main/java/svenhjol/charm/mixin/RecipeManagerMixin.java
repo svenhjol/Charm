4fc32f59a0af50ccd0e35e110c92b7c7c91edf6d
Sven Hjól <sven.hjol@protonmail.com>
2020-08-27 23:37:42 +0000

    Recipe handler to filter recipes with disabled modules

---
+++ src/main/java/svenhjol/charm/mixin/RecipeManagerMixin.java
@@ -0,0 +1,25 @@
+package svenhjol.charm.mixin;
+
+import com.google.gson.JsonElement;
+import net.minecraft.recipe.RecipeManager;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.profiler.Profiler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import svenhjol.meson.handler.RecipeHandler;
+
+import java.util.Map;
+
+@Mixin(RecipeManager.class)
+public class RecipeManagerMixin {
+    @Inject(
+        method = "apply",
+        at = @At("HEAD")
+    )
+    private void hookApply(Map<Identifier, JsonElement> map, ResourceManager resourceManager, Profiler profiler, CallbackInfo ci) {
+        RecipeHandler.filter(map);
+    }
+}
---
+++ src/main/java/svenhjol/meson/handler/RecipeHandler.java
@@ -0,0 +1,30 @@
+package svenhjol.meson.handler;
+
+import com.google.gson.JsonElement;
+import net.minecraft.util.Identifier;
+import svenhjol.meson.Meson;
+import svenhjol.meson.MesonModule;
+import svenhjol.meson.helper.StringHelper;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class RecipeHandler {
+    public static void filter(Map<Identifier, JsonElement> recipes) {
+        for (String modId : Meson.loadedModules.keySet()) {
+            Map<String, MesonModule> modMap = Meson.loadedModules.get(modId);
+            List<Identifier> modRecipes = recipes.keySet().stream().filter(r -> r.getNamespace().equals(modId)).collect(Collectors.toList());
+
+            modRecipes.forEach(recipeId -> {
+                String path = recipeId.getPath();
+                if (!path.contains("/"))
+                    return;
+
+                String moduleId = StringHelper.snakeToUpperCamel(path.split("/")[0]);
+                if (modMap.containsKey(moduleId) && !modMap.get(moduleId).enabled)
+                    recipes.remove(recipeId);
+            });
+        }
+    }
+}
