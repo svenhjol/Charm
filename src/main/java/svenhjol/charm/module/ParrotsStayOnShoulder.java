e1e7c2688e2fe07744e5901c1da66754cf45c296
Sven Hjól <sven.hjol@protonmail.com>
2020-08-28 03:39:05 +0000

    Optimization to parrot check

--- src/main/java/svenhjol/charm/module/ParrotsStayOnShoulder.java
+++ src/main/java/svenhjol/charm/module/ParrotsStayOnShoulder.java
@@ -6,22 +6,24 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
 import svenhjol.charm.event.PlayerTickCallback;
 import svenhjol.charm.mixin.accessor.PlayerEntityAccessor;
-import svenhjol.meson.Meson;
 import svenhjol.meson.MesonModule;
 import svenhjol.meson.iface.Module;
 
 @Module(description = "Parrots stay on your shoulder when jumping and falling. Crouch to make them dismount.")
 public class ParrotsStayOnShoulder extends MesonModule {
+    private static boolean isEnabled = false;
+
     @Override
     public void init() {
         if (!enabled)
             return;
 
         PlayerTickCallback.EVENT.register(this::dismountParrot);
+        isEnabled = true;
     }
 
     public static boolean shouldParrotStayMounted(World world, long shoulderTime) {
-        return shoulderTime + 20L < world.getTime() && Meson.enabled("charm:parrots_stay_on_shoulder");
+        return shoulderTime + 20L < world.getTime() && isEnabled;
     }
 
     public void dismountParrot(PlayerEntity player) {
