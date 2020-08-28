97e44745bbdcdcff1bece7f1938693fd68ff99d0
Sven Hjól <sven.hjol@protonmail.com>
2020-08-28 04:01:28 +0000

    Textures defined in resources file

--- src/main/java/svenhjol/charm/base/CharmResources.java
+++ src/main/java/svenhjol/charm/base/CharmResources.java
@@ -5,4 +5,5 @@ import svenhjol.charm.Charm;
 
 public class CharmResources {
     public static final Identifier INVENTORY_BUTTONS = new Identifier(Charm.MOD_ID, "textures/gui/inventory_buttons.png");
+    public static final Identifier GUI_9_TEXTURE = new Identifier(Charm.MOD_ID, "textures/gui/generic_9.png");
 }
--- src/main/java/svenhjol/charm/gui/CrateScreen.java
+++ src/main/java/svenhjol/charm/gui/CrateScreen.java
@@ -6,12 +6,9 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
-import svenhjol.charm.Charm;
+import svenhjol.charm.base.CharmResources;
 
 public class CrateScreen extends HandledScreen<ScreenHandler> {
-    private static final Identifier GUI_TEXTURE = new Identifier(Charm.MOD_ID, "textures/gui/generic_9.png");
-
     public CrateScreen(ScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
         this.passEvents = true;
@@ -37,7 +34,7 @@ public class CrateScreen extends HandledScreen<ScreenHandler> {
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
 
         if (client != null) {
-            client.getTextureManager().bindTexture(GUI_TEXTURE);
+            client.getTextureManager().bindTexture(CharmResources.GUI_9_TEXTURE);
 
             int x = (width - backgroundWidth) / 2;
             int y = (height - backgroundHeight) / 2;
