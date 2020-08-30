af23bc52afcfee1380cf8bc6400d75671e2766ce
Sven Hjól <sven.hjol@protonmail.com>
2020-08-30 23:39:03 +0000

    Add 18-slot gui texture

--- src/main/java/svenhjol/charm/base/CharmResources.java
+++ src/main/java/svenhjol/charm/base/CharmResources.java
@@ -6,4 +6,5 @@ import svenhjol.charm.Charm;
 public class CharmResources {
     public static final Identifier INVENTORY_BUTTONS = new Identifier(Charm.MOD_ID, "textures/gui/inventory_buttons.png");
     public static final Identifier GUI_9_TEXTURE = new Identifier(Charm.MOD_ID, "textures/gui/generic_9.png");
+    public static final Identifier GUI_18_TEXTURE = new Identifier(Charm.MOD_ID, "textures/gui/generic_18.png");
 }
