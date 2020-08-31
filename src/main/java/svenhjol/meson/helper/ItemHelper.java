57686441340c96e3ae18290976a9354459771b84
Sven Hjól <sven.hjol@protonmail.com>
2020-08-31 22:00:10 +0000

    Mixin allows any item life to be extended

--- src/main/java/svenhjol/meson/helper/ItemHelper.java
+++ src/main/java/svenhjol/meson/helper/ItemHelper.java
@@ -1,11 +1,17 @@
 package svenhjol.meson.helper;
 
 import net.minecraft.block.Block;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class ItemHelper {
+    public static Map<Item, Integer> ITEM_LIFETIME = new HashMap<>();
+
     public static Class<? extends Block> getBlockClass(ItemStack stack) {
         return Block.getBlockFromItem(stack.getItem()).getClass();
     }
@@ -16,5 +22,15 @@ public class ItemHelper {
             amount++;
 
         return amount;
+    }
+
+    public static boolean shouldItemDespawn(ItemEntity itemEntity) {
+        Item item = itemEntity.getStack().getItem();
+        if (ItemHelper.ITEM_LIFETIME.containsKey(item)) {
+            if (itemEntity.getAge() < ItemHelper.ITEM_LIFETIME.get(item))
+                return false;
+        }
+
+        return true;
     }
 }
