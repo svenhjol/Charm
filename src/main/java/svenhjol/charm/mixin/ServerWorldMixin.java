290b718eaaecaf777c7fd36bd4d6e00780846235
Sven Hjól <sven.hjol@protonmail.com>
2020-09-01 00:54:43 +0000

    ServerWorld mixin to fire event when player joins

--- src/main/java/svenhjol/charm/mixin/ServerWorldMixin.java
+++ src/main/java/svenhjol/charm/mixin/ServerWorldMixin.java
@@ -1,10 +1,12 @@
 package svenhjol.charm.mixin;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import svenhjol.charm.event.AddEntityCallback;
 
@@ -30,5 +32,21 @@ public class ServerWorldMixin {
     )
     private void hookLoadEntity(Entity entity, CallbackInfoReturnable<Boolean> cir) {
         AddEntityCallback.EVENT.invoker().interact(entity);
+    }
+
+    /**
+     * Calls our AddEntityCallback event when a player
+     * joins the game on the server side.
+     *
+     * Inspired by Forge's EntityJoinWorldEvent which
+     * provides a hook at the same point in the code,
+     * except allows it to be cancellable.
+     */
+    @Inject(
+        method = "addPlayer",
+        at = @At("HEAD")
+    )
+    private void hookAddPlayer(ServerPlayerEntity player, CallbackInfo ci) {
+        AddEntityCallback.EVENT.invoker().interact(player);
     }
 }
