72b8a92a0c395951651b295c1fcd40c52362e020
Sven Hjól <sven.hjol@protonmail.com>
2020-09-03 20:53:07 +0000

    Lumberjacks/Woodcutter blocks WIP

--- src/main/java/svenhjol/charm/Charm.java
+++ src/main/java/svenhjol/charm/Charm.java
@@ -55,6 +55,7 @@ public class Charm extends MesonMod {
             Innkeepers.class,
             InventoryTidying.class,
             LanternImprovements.class,
+            Lumberjacks.class,
             MineshaftImprovements.class,
             MoreVillageBiomes.class,
             MoreVillagerTrades.class,
