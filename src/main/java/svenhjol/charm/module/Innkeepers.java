cc87393c9e1d1e658842997a93d85980d7b85d6d
Sven Hjól <sven.hjol@protonmail.com>
2020-09-04 15:54:08 +0000

    Sugar

--- src/main/java/svenhjol/charm/module/Innkeepers.java
+++ src/main/java/svenhjol/charm/module/Innkeepers.java
@@ -1,18 +1,15 @@
 package svenhjol.charm.module;
 
 import net.minecraft.client.render.RenderLayer;
-import net.minecraft.structure.pool.StructurePool;
-import net.minecraft.structure.processor.StructureProcessorList;
-import net.minecraft.structure.processor.StructureProcessorLists;
 import net.minecraft.util.Identifier;
 import svenhjol.charm.block.BrewBottleBlock;
 import svenhjol.charm.event.StructureSetupCallback;
+import svenhjol.charm.event.StructureSetupCallback.VillageType;
 import svenhjol.charm.mixin.accessor.RenderLayersAccessor;
 import svenhjol.meson.MesonModule;
 import svenhjol.meson.iface.Module;
 
-import java.util.HashMap;
-import java.util.Map;
+import static svenhjol.charm.event.StructureSetupCallback.addVillageHouse;
 
 @Module(description = "")
 public class Innkeepers extends MesonModule {
@@ -24,25 +21,12 @@ public class Innkeepers extends MesonModule {
 
         // register innkeeper structures
         StructureSetupCallback.EVENT.register(() -> {
-            StructureProcessorList processor = StructureProcessorLists.MOSSIFY_10_PERCENT;
-            StructurePool.Projection projection = StructurePool.Projection.RIGID;
 
-            Map<Identifier, Integer> plainsBuildings = new HashMap<>();
-            Map<Identifier, Integer> savannaBuildings = new HashMap<>();
-            Map<Identifier, Integer> taigaBuildings = new HashMap<>();
+            // @Coranthes: register houses here, one per line
+            addVillageHouse(VillageType.TAIGA, new Identifier("charm:village/taiga/houses/taiga_innkeeper_1"), 10);
+//            addVillageHouse(VillageType.PLAINS, new Identifier("charm:village/plains/houses/plains_innkeeper_1"), 10);
+//            addVillageHouse(VillageType.SAVANNA, new Identifier("charm:village/savanna/houses/savanna_innkeeper_1"), 10);
 
-            Identifier plainsHouses = new Identifier("village/plains/houses");
-            Identifier savannaHouses = new Identifier("village/savanna/houses");
-            Identifier taigaHouses = new Identifier("village/taiga/houses");
-
-            // @Coranthes: register your buildings here, 1 per line.
-            // plainsBuildings.put(new Identifier("charm:village/plains/houses/plains_beekeeper_1"), 10);
-            // savannaBuildings.put(new Identifier("charm:village/savanna/houses/savanna_beekeeper_1"), 10);
-            // taigaBuildings.put(new Identifier("charm:village/plains/houses/plains_beekeeper_1"), 10);
-
-            plainsBuildings.forEach((building, count) -> StructureSetupCallback.addStructurePoolElement(plainsHouses, building, processor, projection, count));
-            savannaBuildings.forEach((building, count) -> StructureSetupCallback.addStructurePoolElement(savannaHouses, building, processor, projection, count));
-            taigaBuildings.forEach((building, count) -> StructureSetupCallback.addStructurePoolElement(taigaHouses, building, processor, projection, count));
         });
     }
 
