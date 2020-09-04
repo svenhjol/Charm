c3191f6c93047f3e4ab820abf4547308446e8c59
Sven Hjól <sven.hjol@protonmail.com>
2020-09-04 15:41:09 +0000

    Register innkeeper structures stub

--- src/main/java/svenhjol/charm/module/Innkeepers.java
+++ src/main/java/svenhjol/charm/module/Innkeepers.java
@@ -1,10 +1,18 @@
 package svenhjol.charm.module;
 
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.structure.pool.StructurePool;
+import net.minecraft.structure.processor.StructureProcessorList;
+import net.minecraft.structure.processor.StructureProcessorLists;
+import net.minecraft.util.Identifier;
 import svenhjol.charm.block.BrewBottleBlock;
+import svenhjol.charm.event.StructureSetupCallback;
 import svenhjol.charm.mixin.accessor.RenderLayersAccessor;
 import svenhjol.meson.MesonModule;
 import svenhjol.meson.iface.Module;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Module(description = "")
 public class Innkeepers extends MesonModule {
@@ -13,6 +21,28 @@ public class Innkeepers extends MesonModule {
     @Override
     public void init() {
         BREW_BOTTLE = new BrewBottleBlock(this);
+
+        // register beekeeper structures
+        StructureSetupCallback.EVENT.register(() -> {
+            StructureProcessorList processor = StructureProcessorLists.MOSSIFY_10_PERCENT;
+            StructurePool.Projection projection = StructurePool.Projection.RIGID;
+
+            Map<Identifier, Integer> plainsBuildings = new HashMap<>();
+            Map<Identifier, Integer> savannaBuildings = new HashMap<>();
+            Map<Identifier, Integer> taigaBuildings = new HashMap<>();
+
+            Identifier plainsHouses = new Identifier("village/plains/houses");
+            Identifier savannaHouses = new Identifier("village/savanna/houses");
+            Identifier taigaHouses = new Identifier("village/taiga/houses");
+
+            // @Coranthes: register your buildings here, 1 per line.
+            // plainsBuildings.put(new Identifier("charm:village/plains/houses/plains_beekeeper_1"), 10);
+            // savannaBuildings.put(new Identifier("charm:village/savanna/houses/savanna_beekeeper_1"), 10);
+
+            plainsBuildings.forEach((building, count) -> StructureSetupCallback.addStructurePoolElement(plainsHouses, building, processor, projection, count));
+            savannaBuildings.forEach((building, count) -> StructureSetupCallback.addStructurePoolElement(savannaHouses, building, processor, projection, count));
+            taigaBuildings.forEach((building, count) -> StructureSetupCallback.addStructurePoolElement(taigaHouses, building, processor, projection, count));
+        });
     }
 
     @Override
