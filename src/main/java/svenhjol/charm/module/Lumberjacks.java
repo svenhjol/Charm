1cf73276b8a426fced4b13e9d97d544d9fc19cdc
Sven Hjól <sven.hjol@protonmail.com>
2020-09-05 01:07:19 +0000

    Lumberjack villager registration

--- src/main/java/svenhjol/charm/module/Lumberjacks.java
+++ src/main/java/svenhjol/charm/module/Lumberjacks.java
@@ -2,11 +2,19 @@ package svenhjol.charm.module;
 
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.recipe.RecipeType;
 import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradeOffers;
+import net.minecraft.village.VillagerProfession;
+import net.minecraft.world.poi.PointOfInterestType;
 import svenhjol.charm.Charm;
 import svenhjol.charm.block.WoodcutterBlock;
 import svenhjol.charm.gui.WoodcutterScreen;
@@ -14,29 +22,54 @@ import svenhjol.charm.mixin.accessor.RenderLayersAccessor;
 import svenhjol.charm.recipe.WoodcuttingRecipe;
 import svenhjol.charm.screenhandler.WoodcutterScreenHandler;
 import svenhjol.meson.MesonModule;
+import svenhjol.meson.helper.VillagerHelper;
 import svenhjol.meson.iface.Module;
 
-@Module(description = "")
+import javax.annotation.Nullable;
+import java.util.Random;
+
+@Module(description = "Lumberjacks are villagers that trade wooden items. Their job site is the woodcutter.")
 public class Lumberjacks extends MesonModule {
     public static Identifier RECIPE_ID = new Identifier("woodcutting");
-    public static Identifier SCREEN_HANDLER_ID = new Identifier(Charm.MOD_ID, "woodcutter");
-    public static WoodcutterBlock WOODCUTTER_BLOCK;
+    public static Identifier BLOCK_ID = new Identifier(Charm.MOD_ID, "woodcutter");
+    public static Identifier VILLAGER_ID = new Identifier(Charm.MOD_ID, "lumberjack");
+
+    public static VillagerProfession LUMBERJACK;
+    public static WoodcutterBlock WOODCUTTER;
+    public static PointOfInterestType POIT;
     public static ScreenHandlerType<WoodcutterScreenHandler> SCREEN_HANDLER;
     public static RecipeType<WoodcuttingRecipe> RECIPE_TYPE;
     public static RecipeSerializer<WoodcuttingRecipe> RECIPE_SERIALIZER;
 
     @Override
     public void init() {
-        WOODCUTTER_BLOCK = new WoodcutterBlock(this);
+        WOODCUTTER = new WoodcutterBlock(this);
+
         RECIPE_TYPE = RecipeType.register(RECIPE_ID.toString());
         RECIPE_SERIALIZER = RecipeSerializer.register(RECIPE_ID.toString(), new WoodcuttingRecipe.Serializer(WoodcuttingRecipe::new));
-        SCREEN_HANDLER = Registry.register(Registry.SCREEN_HANDLER, SCREEN_HANDLER_ID, new ScreenHandlerType<>(WoodcutterScreenHandler::new));
-//        ScreenHandlerRegistry.registerSimple(SCREEN_HANDLER_ID, WoodcutterScreenHandler::new);
+
+        SCREEN_HANDLER = Registry.register(Registry.SCREEN_HANDLER, BLOCK_ID, new ScreenHandlerType<>(WoodcutterScreenHandler::new));
+
+        // TODO dedicated sounds for woodcutter and jobsite
+        POIT = VillagerHelper.addPointOfInterestType(BLOCK_ID, WOODCUTTER, 1);
+        LUMBERJACK = VillagerHelper.addProfession(VILLAGER_ID, POIT, SoundEvents.ENTITY_VILLAGER_WORK_MASON);
+        VillagerHelper.addTrade(LUMBERJACK, 1, new EmeraldsForWoodenPlanks());
     }
 
     @Override
     public void initClient() {
-        RenderLayersAccessor.getBlocks().put(WOODCUTTER_BLOCK, RenderLayer.getCutout());
+        RenderLayersAccessor.getBlocks().put(WOODCUTTER, RenderLayer.getCutout());
         ScreenRegistry.register(SCREEN_HANDLER, WoodcutterScreen::new);
+    }
+
+    public static class EmeraldsForWoodenPlanks implements TradeOffers.Factory {
+        @Nullable
+        @Override
+        public TradeOffer create(Entity entity, Random random) {
+            int count = random.nextInt(2) + 1;
+            ItemStack in1 = new ItemStack(Items.OAK_PLANKS, count * 4);
+            ItemStack out = new ItemStack(Items.EMERALD, count);
+            return new TradeOffer(in1, out, 10, 5, 0.2F);
+        }
     }
 }
