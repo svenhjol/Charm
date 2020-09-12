79cfd4695b1bb550400b8e03c648c2746503d9a0
Sven Hjól <sven.hjol@protonmail.com>
2020-09-12 01:27:24 +0000

    Hook into MinecraftServer for worldload

---
+++ src/main/java/svenhjol/meson/mixin/MinecraftServerMixin.java
@@ -0,0 +1,23 @@
+package svenhjol.meson.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import svenhjol.meson.event.LoadWorldCallback;
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin {
+    @Inject(
+        method = "runServer",
+        at = @At(
+            value = "INVOKE_ASSIGN",
+            target = "Lnet/minecraft/util/Util;getMeasuringTimeMs()J",
+            ordinal = 0
+        )
+    )
+    private void hookRunServer(CallbackInfo ci) {
+        LoadWorldCallback.EVENT.invoker().interact((MinecraftServer)(Object)this);
+    }
+}
