b192a5764fbe0a31430b8bf339d56887eebd72e0
Sven Hjól <sven.hjol@protonmail.com>
2020-09-13 14:24:59 +0000

    SingleItemTypeTrade in VillagerHelper

--- src/main/java/svenhjol/meson/helper/VillagerHelper.java
+++ src/main/java/svenhjol/meson/helper/VillagerHelper.java
@@ -6,22 +6,30 @@ import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import net.fabricmc.fabric.mixin.object.builder.PointOfInterestTypeAccessor;
 import net.fabricmc.fabric.mixin.object.builder.VillagerProfessionAccessor;
 import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.village.TradeOffers;
+import net.minecraft.village.TradeOffer;
 import net.minecraft.village.VillagerProfession;
 import net.minecraft.world.poi.PointOfInterestType;
 
+import javax.annotation.Nullable;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+
+import static net.minecraft.village.TradeOffers.*;
 
 public class VillagerHelper {
     public static VillagerProfession addProfession(Identifier id, PointOfInterestType poit, SoundEvent worksound) {
         VillagerProfession profession = VillagerProfessionAccessor.create(id.toString(), poit, ImmutableSet.of(), ImmutableSet.of(), worksound);
         VillagerProfession registeredProfession = Registry.register(Registry.VILLAGER_PROFESSION, id, profession);
-        TradeOffers.PROFESSION_TO_LEVELED_TRADE.put(profession, new Int2ObjectOpenHashMap<>());
+        PROFESSION_TO_LEVELED_TRADE.put(profession, new Int2ObjectOpenHashMap<>());
         return registeredProfession;
     }
 
@@ -31,9 +39,9 @@ public class VillagerHelper {
         return PointOfInterestTypeAccessor.callSetup(poit);
     }
 
-    public static void addTrade(VillagerProfession profession, int level, TradeOffers.Factory trade) {
-        Int2ObjectMap<TradeOffers.Factory[]> fixedTrades = TradeOffers.PROFESSION_TO_LEVELED_TRADE.get(profession);
-        Int2ObjectMap<List<TradeOffers.Factory>> mutableTrades = new Int2ObjectOpenHashMap<>();
+    public static void addTrade(VillagerProfession profession, int level, Factory trade) {
+        Int2ObjectMap<Factory[]> fixedTrades = PROFESSION_TO_LEVELED_TRADE.get(profession);
+        Int2ObjectMap<List<Factory>> mutableTrades = new Int2ObjectOpenHashMap<>();
 
         for (int i = 1; i <= 5; i++) {
             mutableTrades.put(i, DefaultedList.of());
@@ -45,22 +53,40 @@ public class VillagerHelper {
 
         mutableTrades.get(level).add(trade);
 
-        Int2ObjectMap<TradeOffers.Factory[]> mappedTrades = new Int2ObjectOpenHashMap<>();
-        mutableTrades.int2ObjectEntrySet().forEach(e -> mappedTrades.put(e.getIntKey(), e.getValue().toArray(new TradeOffers.Factory[0])));
-        TradeOffers.PROFESSION_TO_LEVELED_TRADE.put(profession, mappedTrades);
+        Int2ObjectMap<Factory[]> mappedTrades = new Int2ObjectOpenHashMap<>();
+        mutableTrades.int2ObjectEntrySet().forEach(e -> mappedTrades.put(e.getIntKey(), e.getValue().toArray(new Factory[0])));
+        PROFESSION_TO_LEVELED_TRADE.put(profession, mappedTrades);
     }
 
-    public static void addWanderingTrade(TradeOffers.Factory trade, boolean isRare) {
+    public static void addWanderingTrade(Factory trade, boolean isRare) {
         if (isRare) {
-            List<TradeOffers.Factory> rareTrades = DefaultedList.of();
-            rareTrades.addAll(Arrays.asList(TradeOffers.WANDERING_TRADER_TRADES.get(2)));
+            List<Factory> rareTrades = DefaultedList.of();
+            rareTrades.addAll(Arrays.asList(WANDERING_TRADER_TRADES.get(2)));
             rareTrades.add(trade);
-            TradeOffers.WANDERING_TRADER_TRADES.put(2, rareTrades.toArray(new TradeOffers.Factory[0]));
+            WANDERING_TRADER_TRADES.put(2, rareTrades.toArray(new Factory[0]));
         } else {
-            List<TradeOffers.Factory> normalTrades = DefaultedList.of();
-            normalTrades.addAll(Arrays.asList(TradeOffers.WANDERING_TRADER_TRADES.get(1)));
+            List<Factory> normalTrades = DefaultedList.of();
+            normalTrades.addAll(Arrays.asList(WANDERING_TRADER_TRADES.get(1)));
             normalTrades.add(trade);
-            TradeOffers.WANDERING_TRADER_TRADES.put(1, normalTrades.toArray(new TradeOffers.Factory[0]));
+            WANDERING_TRADER_TRADES.put(1, normalTrades.toArray(new Factory[0]));
+        }
+    }
+
+    public static abstract class SingleItemTypeTrade implements Factory {
+        protected Item in = Items.AIR;
+        protected Item out = Items.EMERALD;
+        protected int inCount = 1;
+        protected int outCount = 1;
+        protected int maxUses = 20;
+        protected int experience = 2;
+        protected float multiplier = 0.05F;
+
+        @Nullable
+        @Override
+        public TradeOffer create(Entity entity, Random random) {
+            ItemStack in1 = new ItemStack(in, inCount);
+            ItemStack out1 = new ItemStack(out, outCount);
+            return new TradeOffer(in1, out1, maxUses, experience, multiplier);
         }
     }
 }
