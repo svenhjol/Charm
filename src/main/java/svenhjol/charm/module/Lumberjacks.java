2a7badc8c76e1709bee7b5bd4af68faf25e9690a
Sven Hjól <sven.hjol@protonmail.com>
2020-09-17 20:38:41 +0000

    Added lumberjack trades

--- src/main/java/svenhjol/charm/module/Lumberjacks.java
+++ src/main/java/svenhjol/charm/module/Lumberjacks.java
@@ -1,25 +1,19 @@
 package svenhjol.charm.module;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
-import net.minecraft.village.TradeOffer;
-import net.minecraft.village.TradeOffers;
 import net.minecraft.village.VillagerProfession;
 import net.minecraft.world.poi.PointOfInterestType;
 import svenhjol.charm.Charm;
+import svenhjol.charm.village.LumberjackTradeOffers.*;
 import svenhjol.meson.MesonModule;
 import svenhjol.meson.event.StructureSetupCallback;
 import svenhjol.meson.event.StructureSetupCallback.VillageType;
 import svenhjol.meson.helper.VillagerHelper;
 import svenhjol.meson.iface.Module;
 
-import javax.annotation.Nullable;
-import java.util.Random;
-
 import static svenhjol.meson.event.StructureSetupCallback.addVillageHouse;
+import static svenhjol.meson.helper.VillagerHelper.addTrade;
 
 @Module(description = "Lumberjacks are villagers that trade wooden items. Their job site is the woodcutter.")
 public class Lumberjacks extends MesonModule {
@@ -32,31 +26,34 @@ public class Lumberjacks extends MesonModule {
         // TODO dedicated sounds for woodcutter and jobsite
         POIT = VillagerHelper.addPointOfInterestType(Woodcutters.BLOCK_ID, Woodcutters.WOODCUTTER, 1);
         LUMBERJACK = VillagerHelper.addProfession(VILLAGER_ID, POIT, SoundEvents.ENTITY_VILLAGER_WORK_MASON);
-        VillagerHelper.addTrade(LUMBERJACK, 1, new EmeraldsForWoodenPlanks());
 
+        // register lumberjack trades
+        addTrade(LUMBERJACK, 1, new EmeraldsForOverworldLogs());
+        addTrade(LUMBERJACK, 1, new CommonSaplingsForEmeralds());
+        addTrade(LUMBERJACK, 1, new LaddersForEmeralds());
+        addTrade(LUMBERJACK, 2, new EmeraldsForBones());
+        addTrade(LUMBERJACK, 2, new BedForEmeralds());
+        addTrade(LUMBERJACK, 2, new FencesForEmeralds());
+        addTrade(LUMBERJACK, 3, new BarkForLogs());
+        addTrade(LUMBERJACK, 3, new EmeraldsForStems());
+        addTrade(LUMBERJACK, 3, new DoorsForEmeralds());
+        addTrade(LUMBERJACK, 3, new UncommonSaplingsForEmeralds());
+        addTrade(LUMBERJACK, 4, new CrateForEmeralds());
+        addTrade(LUMBERJACK, 4, new PlanksForLogs());
+        addTrade(LUMBERJACK, 5, new BookcaseForEmeralds());
+        addTrade(LUMBERJACK, 5, new WorkstationForEmeralds());
 
         // register lumberjack structures
-        // keep an eye on lumberjack spawn weight Coranthes
         StructureSetupCallback.EVENT.register(() -> {
             addVillageHouse(VillageType.DESERT, new Identifier("charm:village/desert/houses/desert_lumberjack_1"), 10);
-            addVillageHouse(VillageType.SAVANNA, new Identifier("charm:village/savanna/houses/savanna_lumberjack_1"), 10);
+            addVillageHouse(VillageType.DESERT, new Identifier("charm:village/desert/houses/desert_lumberjack_2"), 10);
+            addVillageHouse(VillageType.PLAINS, new Identifier("charm:village/plains/houses/plains_beejack_1"), 10);
             addVillageHouse(VillageType.PLAINS, new Identifier("charm:village/plains/houses/plains_lumberjack_1"), 5);
             addVillageHouse(VillageType.PLAINS, new Identifier("charm:village/plains/houses/plains_lumberjack_2"), 10);
-            addVillageHouse(VillageType.PLAINS, new Identifier("charm:village/plains/houses/plains_beejack_1"), 10);
-            addVillageHouse(VillageType.TAIGA, new Identifier("charm:village/taiga/houses/taiga_lumberjack_1"), 5);
-            addVillageHouse(VillageType.DESERT, new Identifier("charm:village/desert/houses/desert_lumberjack_2"), 10);
+            addVillageHouse(VillageType.SAVANNA, new Identifier("charm:village/savanna/houses/savanna_lumberjack_1"), 10);
             addVillageHouse(VillageType.SNOWY, new Identifier("charm:village/snowy/houses/snowy_lumberbee_1"), 10);
+            addVillageHouse(VillageType.TAIGA, new Identifier("charm:village/taiga/houses/taiga_lumberjack_1"), 5);
         });
     }
 
-    public static class EmeraldsForWoodenPlanks implements TradeOffers.Factory {
-        @Nullable
-        @Override
-        public TradeOffer create(Entity entity, Random random) {
-            int count = random.nextInt(2) + 1;
-            ItemStack in1 = new ItemStack(Items.OAK_PLANKS, count * 6);
-            ItemStack out = new ItemStack(Items.EMERALD, count);
-            return new TradeOffer(in1, out, 10, 1, 0.2F);
-        }
-    }
 }
