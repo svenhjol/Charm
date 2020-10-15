3beff57ddd8338e0464003b7850b68ee4400771c
Sven Hjól <sven.hjol@protonmail.com>
2020-10-15 01:26:02 +0000

    Add coral squid spawn egg

--- src/main/java/svenhjol/charm/module/CoralSquids.java
+++ src/main/java/svenhjol/charm/module/CoralSquids.java
@@ -3,6 +3,9 @@ package svenhjol.charm.module;
 import net.fabricmc.fabric.mixin.object.builder.DefaultAttributeRegistryAccessor;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SpawnEggItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
@@ -23,7 +26,10 @@ import java.util.List;
 @Module(description = "Coral Squids spawn around coral in warm oceans.")
 public class CoralSquids extends MesonModule {
     public static Identifier ID = new Identifier(Charm.MOD_ID, "coral_squid");
+    public static Identifier EGG_ID = new Identifier(Charm.MOD_ID, "coral_squid_spawn_egg");
+
     public static EntityType<CoralSquidEntity> CORAL_SQUID;
+    public static Item SPAWN_EGG;
 
     public CoralSquidsClient client;
 
@@ -36,6 +42,9 @@ public class CoralSquids extends MesonModule {
             .setDimensions(0.4F, 0.4F)
             .maxTrackingRange(8)
             .build(ID.getPath());
+
+        SPAWN_EGG = new SpawnEggItem(CORAL_SQUID, 0xFF00FF, 0x0000FF, (new Item.Settings()).group(ItemGroup.MISC));
+        Registry.register(Registry.ITEM, EGG_ID, SPAWN_EGG);
 
         Registry.register(Registry.ENTITY_TYPE, ID, CORAL_SQUID);
         DefaultAttributeRegistryAccessor.getRegistry()
