bb99e88de4d9b12195470130411b790b87f314bf
Sven Hjól <sven.hjol@protonmail.com>
2020-10-19 21:02:45 +0000

    More helper

--- src/main/java/svenhjol/charm/base/helper/DimensionHelper.java
+++ src/main/java/svenhjol/charm/base/helper/DimensionHelper.java
@@ -4,6 +4,8 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
 
+import javax.annotation.Nullable;
+
 public class DimensionHelper {
     public static boolean isOverworld(World world) {
         return world.getRegistryKey() == World.OVERWORLD;
@@ -17,12 +19,25 @@ public class DimensionHelper {
         return world.getRegistryKey() == World.END;
     }
 
-    public static boolean isDimension(World world, Identifier dim) {
-        return getDimension(world).equals(dim);
+    public static boolean isDimension(World world, Identifier dimension) {
+        return getDimension(world).equals(dimension);
     }
 
     public static Identifier getDimension(World world) {
         RegistryKey<World> key = world.getRegistryKey();
         return key.getValue();
+    }
+
+    @Nullable
+    public static RegistryKey<World> getDimension(Identifier dim) {
+        if (World.OVERWORLD.getValue().equals(dim)) {
+            return World.OVERWORLD;
+        } else if (World.NETHER.getValue().equals(dim)) {
+            return World.NETHER;
+        } else if (World.END.getValue().equals(dim)) {
+            return World.END;
+        }
+
+        return null;
     }
 }
