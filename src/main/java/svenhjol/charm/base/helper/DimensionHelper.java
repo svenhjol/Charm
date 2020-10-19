a651c7b7275c8e63f326adc2af550e611ccc24cc
Sven Hjól <sven.hjol@protonmail.com>
2020-10-19 20:40:17 +0000

    Helper methods for dimension types

--- src/main/java/svenhjol/charm/base/helper/DimensionHelper.java
+++ src/main/java/svenhjol/charm/base/helper/DimensionHelper.java
@@ -5,6 +5,18 @@ import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
 
 public class DimensionHelper {
+    public static boolean isOverworld(World world) {
+        return world.getRegistryKey() == World.OVERWORLD;
+    }
+
+    public static boolean isNether(World world) {
+        return world.getRegistryKey() == World.NETHER;
+    }
+
+    public static boolean isEnd(World world) {
+        return world.getRegistryKey() == World.END;
+    }
+
     public static boolean isDimension(World world, Identifier dim) {
         return getDimension(world).equals(dim);
     }
