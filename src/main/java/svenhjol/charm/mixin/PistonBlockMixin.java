b67992a4b06c80a65857b57e3ba75182764dccd1
Sven Hjól <sven.hjol@protonmail.com>
2020-10-22 21:38:05 +0000

    Created "immovable by pistons" tag to handle this stuff in future

---
+++ src/main/java/svenhjol/charm/mixin/PistonBlockMixin.java
@@ -0,0 +1,25 @@
+package svenhjol.charm.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.PistonBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import svenhjol.charm.base.CharmTags;
+
+@Mixin(PistonBlock.class)
+public class PistonBlockMixin {
+    @Inject(
+        method = "isMovable",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private static void hookIsMovable(BlockState blockState, World world, BlockPos blockPos, Direction direction, boolean canBreak, Direction pistonDir, CallbackInfoReturnable<Boolean> cir) {
+        if (blockState.getBlock().isIn(CharmTags.IMMOVABLE_BY_PISTONS))
+            cir.setReturnValue(false);
+    }
+}
