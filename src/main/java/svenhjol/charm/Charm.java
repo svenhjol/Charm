39919e0533e1ee75debb7a94b42b1baa83561571
Sven Hjól <sven.hjol@protonmail.com>
2020-10-28 17:36:24 +0000

    colored enchants

--- src/main/java/svenhjol/charm/Charm.java
+++ src/main/java/svenhjol/charm/Charm.java
@@ -38,6 +38,7 @@ public class Charm implements ModInitializer {
             Crates.class,
             DecreaseRepairCost.class,
             DirtToPath.class,
+            EnchantColors.class,
             EndermitePowder.class,
             EntitySpawner.class,
             ExtendNetherite.class,
---
+++ src/main/java/svenhjol/charm/module/EnchantColors.java
@@ -0,0 +1,45 @@
+package svenhjol.charm.module;
+
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.DyeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.screen.AnvilScreenHandler;
+import net.minecraft.util.ActionResult;
+import org.apache.logging.log4j.util.TriConsumer;
+import svenhjol.charm.Charm;
+import svenhjol.charm.base.CharmModule;
+import svenhjol.charm.base.iface.Module;
+import svenhjol.charm.event.UpdateAnvilCallback;
+import svenhjol.charm.handler.ColoredGlintHandler;
+
+@Module(mod = Charm.MOD_ID)
+public class EnchantColors extends CharmModule {
+    @Override
+    public void init() {
+        UpdateAnvilCallback.EVENT.register(this::tryColor);
+    }
+
+    private ActionResult tryColor(AnvilScreenHandler handler, ItemStack left, ItemStack right, Inventory output, String name, int baseCost, TriConsumer<ItemStack, Integer, Integer> apply) {
+        ItemStack out;
+
+        if (left.isEmpty() || right.isEmpty())
+            return ActionResult.PASS;
+
+        if (!(right.getItem() instanceof DyeItem))
+            return ActionResult.PASS;
+
+        ListTag enchantments = left.getEnchantments();
+        if (enchantments.isEmpty())
+            return ActionResult.PASS;
+
+        DyeItem dye = (DyeItem)right.getItem();
+        String colorName = dye.getColor().asString();
+
+        out = left.copy();
+        out.getOrCreateTag().putString(ColoredGlintHandler.GLINT_TAG, colorName);
+        apply.accept(out, 0, 1);
+
+        return ActionResult.SUCCESS;
+    }
+}
