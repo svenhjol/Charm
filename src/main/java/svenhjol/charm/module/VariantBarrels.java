0c2e9f95acf5ee4c3b0e74e75807d97a53421a2c
Sven Hjól <sven.hjol@protonmail.com>
2020-10-28 01:45:24 +0000

    Fix missing block entity

--- src/main/java/svenhjol/charm/module/VariantBarrels.java
+++ src/main/java/svenhjol/charm/module/VariantBarrels.java
@@ -27,6 +27,6 @@ public class VariantBarrels extends CharmModule {
             BARREL_BLOCKS.put(type, new VariantBarrelBlock(this, type));
         }
 
-        RegistryHandler.blockEntity(ID, BarrelBlockEntity::new);
+        BLOCK_ENTITY = RegistryHandler.blockEntity(ID, BarrelBlockEntity::new);
     }
 }
