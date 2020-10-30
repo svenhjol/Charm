39b0a4f97afad84407336db1e64bdc397bd26592
Sven Hjól <sven.hjol@protonmail.com>
2020-10-30 23:11:18 +0000

    Armor, elytra, shield and trident glint fixes

--- src/main/java/svenhjol/charm/mixin/ArmorFeatureRendererMixin.java
+++ src/main/java/svenhjol/charm/mixin/ArmorFeatureRendererMixin.java
@@ -1,5 +1,7 @@
 package svenhjol.charm.mixin;
 
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.ArmorFeatureRenderer;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
@@ -10,22 +12,43 @@ import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import svenhjol.charm.base.handler.ModuleHandler;
+import svenhjol.charm.handler.ColoredGlintHandler;
 import svenhjol.charm.module.ArmorInvisibility;
 
 @Mixin(ArmorFeatureRenderer.class)
 public class ArmorFeatureRendererMixin<T extends LivingEntity, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>> {
+    private ItemStack itemStackToRender;
+
     @Inject(
         method = "renderArmor",
         at = @At("HEAD"),
         cancellable = true
     )
-    private void hookRenderArmorPart(MatrixStack matrices, VertexConsumerProvider vertexConsumers, T livingEntity, EquipmentSlot equipmentSlot, int i, A bipedEntityModel, CallbackInfo ci) {
+    private void hookRenderArmor(MatrixStack matrices, VertexConsumerProvider vertexConsumers, T livingEntity, EquipmentSlot equipmentSlot, int i, A bipedEntityModel, CallbackInfo ci) {
         if (ModuleHandler.enabled("charm:armor_invisibility")) {
             ItemStack stack = livingEntity.getEquippedStack(equipmentSlot);
             if (ArmorInvisibility.shouldArmorBeInvisible(livingEntity, stack))
                 ci.cancel();
         }
+
+        // take a reference to the item being rendered, this is needed for the glint consumer
+        this.itemStackToRender = livingEntity.getEquippedStack(equipmentSlot);
+    }
+
+    /**
+     * Call Charm's glint handler instead.
+     */
+    @Redirect(
+        method = "renderArmorParts",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/render/item/ItemRenderer;getArmorGlintConsumer(Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/render/RenderLayer;ZZ)Lnet/minecraft/client/render/VertexConsumer;"
+        )
+    )
+    private VertexConsumer hookRenderArmorParts(VertexConsumerProvider provider, RenderLayer layer, boolean solid, boolean glint) {
+        return ColoredGlintHandler.getArmorGlintConsumer(provider, layer, solid, glint, this.itemStackToRender);
     }
 }
--- src/main/java/svenhjol/charm/mixin/BuiltinModelItemRendererMixin.java
+++ src/main/java/svenhjol/charm/mixin/BuiltinModelItemRendererMixin.java
@@ -1,6 +1,8 @@
 package svenhjol.charm.mixin;
 
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.render.item.BuiltinModelItemRenderer;
@@ -12,11 +14,15 @@ import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import svenhjol.charm.event.BlockItemRenderCallback;
+import svenhjol.charm.handler.ColoredGlintHandler;
 
 @Mixin(BuiltinModelItemRenderer.class)
 public class BuiltinModelItemRendererMixin {
+    private ItemStack itemStackToRender;
+
     /**
      * Allows modules to define their own blockItem entity renderers
      * to show in the player's inventory.
@@ -27,6 +33,8 @@ public class BuiltinModelItemRendererMixin {
         cancellable = true
     )
     private void hookRender(ItemStack stack, ModelTransformation.Mode mode, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, int j, CallbackInfo ci) {
+        this.itemStackToRender = stack;
+
         Item item = stack.getItem();
         if (item instanceof BlockItem) {
             BlockEntity blockEntity = BlockItemRenderCallback.EVENT.invoker().interact(((BlockItem) item).getBlock());
@@ -36,5 +44,19 @@ public class BuiltinModelItemRendererMixin {
                 ci.cancel();
             }
         }
+    }
+
+    /**
+     * Redirect shield and trident glint consumer calls to Charm's ColoredGlintHandler
+     */
+    @Redirect(
+        method = "render",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/render/item/ItemRenderer;getDirectItemGlintConsumer(Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/render/RenderLayer;ZZ)Lnet/minecraft/client/render/VertexConsumer;"
+        )
+    )
+    private VertexConsumer hookRenderGetDirectGlintConsumer(VertexConsumerProvider provider, RenderLayer layer, boolean solid, boolean glint) {
+        return ColoredGlintHandler.getDirectItemGlintConsumer(provider, layer, solid, glint, this.itemStackToRender);
     }
 }
