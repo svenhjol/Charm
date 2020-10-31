cd350c1039cf341a19e6dab77305c4ae92f11026
Sven Hjól <sven.hjol@protonmail.com>
2020-10-31 02:55:55 +0000

    Event to catch player drops on death

--- src/main/java/svenhjol/charm/mixin/PlayerEntityMixin.java
+++ src/main/java/svenhjol/charm/mixin/PlayerEntityMixin.java
@@ -3,18 +3,24 @@ package svenhjol.charm.mixin;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.ActionResult;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import svenhjol.charm.event.PlayerDropInventoryCallback;
 import svenhjol.charm.event.PlayerTickCallback;
 import svenhjol.charm.module.ParrotsStayOnShoulder;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends Entity {
     @Shadow private long shoulderEntityAddedTime;
+
+    @Shadow @Final public PlayerInventory inventory;
 
     public PlayerEntityMixin(EntityType<?> type, World world) {
         super(type, world);
@@ -33,5 +39,19 @@ public abstract class PlayerEntityMixin extends Entity {
     @Inject(method = "tick", at = @At("TAIL"))
     private void hookTick(CallbackInfo ci) {
         PlayerTickCallback.EVENT.invoker().interact((PlayerEntity)(Object)this);
+    }
+
+    @Inject(
+        method = "dropInventory",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/entity/player/PlayerInventory;dropAll()V"
+        ),
+        cancellable = true
+    )
+    private void hookDropInventory(CallbackInfo ci) {
+        ActionResult result = PlayerDropInventoryCallback.EVENT.invoker().interact((PlayerEntity) (Object) this, this.inventory);
+        if (result == ActionResult.SUCCESS)
+            ci.cancel();
     }
 }
