56a604616a9145124a425f02224c3492100a15bd
Sven Hjól <sven.hjol@protonmail.com>
2020-11-05 19:53:21 +0000

    Stupid

--- src/main/java/svenhjol/charm/mixin/accessor/HoeItemAccessor.java
+++ src/main/java/svenhjol/charm/mixin/accessor/HoeItemAccessor.java
@@ -1,13 +1,13 @@
 package svenhjol.charm.mixin.accessor;
 
 import net.minecraft.block.Block;
-import net.minecraft.item.ShovelItem;
+import net.minecraft.item.HoeItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.util.Set;
 
-@Mixin(ShovelItem.class)
+@Mixin(HoeItem.class)
 public interface HoeItemAccessor {
     @Accessor("EFFECTIVE_BLOCKS")
     static Set<Block> getEffectiveBlocks() {
