cabd19ed7bd7fb8fefa32b339a3904670b05fee3
Sven Hjól <sven.hjol@protonmail.com>
2020-11-05 23:54:16 +0000

    Neater handlers

--- src/main/java/svenhjol/charm/client/BatBucketClient.java
+++ src/main/java/svenhjol/charm/client/BatBucketClient.java
@@ -2,9 +2,12 @@ package svenhjol.charm.client;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
+import net.fabricmc.fabric.api.network.PacketContext;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.math.Box;
 import svenhjol.charm.module.BatBuckets;
 import svenhjol.charm.base.CharmModule;
@@ -23,30 +26,34 @@ public class BatBucketClient {
         this.module = module;
 
         // register client messages
-        ClientSidePacketRegistry.INSTANCE.register(BatBuckets.MSG_CLIENT_SET_GLOWING, (context, data) -> {
-            double range = data.readDouble();
-            int ticks = data.readInt() * 20; // ticks is sent as number of seconds, multiply by 20 for ticks
-            context.getTaskQueue().execute(() -> {
-                this.range = range;
-                this.ticks = ticks;
-            });
-        });
+        ClientSidePacketRegistry.INSTANCE.register(BatBuckets.MSG_CLIENT_SET_GLOWING, this::handleMessageClientSetGlowing);
 
         // register client tick handler
-        ClientTickEvents.START_CLIENT_TICK.register(minecraft -> {
-            if (minecraft.player != null
-                && ticks > 0
-                && range > 0
-            ) {
-                if (ticks % 10 == 0 || entities.isEmpty()) {
-                    setGlowing(false);
-                    setNearbyEntities(minecraft.player);
-                    setGlowing(true);
-                }
+        ClientTickEvents.START_CLIENT_TICK.register(this::handleClientTick);
+    }
 
-                if (--ticks <= 0)
-                    setGlowing(false);
+    private void handleClientTick(MinecraftClient minecraft) {
+        if (minecraft.player != null
+            && ticks > 0
+            && range > 0
+        ) {
+            if (ticks % 10 == 0 || entities.isEmpty()) {
+                setGlowing(false);
+                setNearbyEntities(minecraft.player);
+                setGlowing(true);
             }
+
+            if (--ticks <= 0)
+                setGlowing(false);
+        }
+    }
+
+    private void handleMessageClientSetGlowing(PacketContext context, PacketByteBuf data) {
+        double range = data.readDouble();
+        int ticks = data.readInt() * 20; // ticks is sent as number of seconds, multiply by 20 for ticks
+        context.getTaskQueue().execute(() -> {
+            this.range = range;
+            this.ticks = ticks;
         });
     }
 
