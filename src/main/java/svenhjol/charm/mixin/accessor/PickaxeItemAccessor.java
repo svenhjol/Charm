c53294f021c82aa79bd3bceee8c6b43b473bc67b
Sven Hjól <sven.hjol@protonmail.com>
2020-11-05 19:57:27 +0000

    More stupid

--- src/main/java/svenhjol/charm/mixin/accessor/PickaxeItemAccessor.java
+++ src/main/java/svenhjol/charm/mixin/accessor/PickaxeItemAccessor.java
@@ -1,13 +1,13 @@
 package svenhjol.charm.mixin.accessor;
 
 import net.minecraft.block.Block;
-import net.minecraft.item.ShovelItem;
+import net.minecraft.item.PickaxeItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.util.Set;
 
-@Mixin(ShovelItem.class)
+@Mixin(PickaxeItem.class)
 public interface PickaxeItemAccessor {
     @Accessor("EFFECTIVE_BLOCKS")
     static Set<Block> getEffectiveBlocks() {
