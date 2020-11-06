2011b24d1481c2b9654a8c988b57dc430fd2afb5
Sven Hjól <sven.hjol@protonmail.com>
2020-11-06 15:38:36 +0000

    Abstract player entity

--- src/main/java/svenhjol/charm/module/AutomaticRecipeUnlock.java
+++ src/main/java/svenhjol/charm/module/AutomaticRecipeUnlock.java
@@ -1,18 +1,18 @@
 package svenhjol.charm.module;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeManager;
-import net.minecraft.server.network.ServerPlayerEntity;
 import svenhjol.charm.Charm;
-import svenhjol.charm.base.handler.ModuleHandler;
 import svenhjol.charm.base.CharmModule;
+import svenhjol.charm.base.handler.ModuleHandler;
 import svenhjol.charm.base.iface.Module;
 
 import java.util.Collection;
 
 @Module(mod = Charm.MOD_ID, description = "Unlocks all vanilla recipes.")
 public class AutomaticRecipeUnlock extends CharmModule {
-    public static void unlockRecipes(ServerPlayerEntity player) {
+    public static void unlockRecipes(PlayerEntity player) {
         if (!ModuleHandler.enabled("charm:automatic_recipe_unlock"))
             return;
 
