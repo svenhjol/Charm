4c60b0fc35611a0329553f112d375c7efe16c0cc
Sven Hjól <sven.hjol@protonmail.com>
2020-11-10 17:53:58 +0000

    FabricBlockSettings instead of shitmix

--- src/main/java/svenhjol/charm/block/RedstoneSandBlock.java
+++ src/main/java/svenhjol/charm/block/RedstoneSandBlock.java
@@ -1,27 +1,25 @@
 package svenhjol.charm.block;
 
-import net.minecraft.block.Block;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ShovelItem;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
 import svenhjol.charm.base.CharmModule;
 import svenhjol.charm.base.block.CharmFallingBlock;
-import svenhjol.charm.base.helper.BlockHelper;
 
 public class RedstoneSandBlock extends CharmFallingBlock {
     public RedstoneSandBlock(CharmModule module) {
-        super(module, "redstone_sand", Block.Settings
+        super(module, "redstone_sand", FabricBlockSettings
             .of(Material.AGGREGATE)
             .sounds(BlockSoundGroup.SAND)
+            .breakByTool(FabricToolTags.SHOVELS)
             .strength(0.5F)
         );
-
-        BlockHelper.setEffectiveTool(this, ShovelItem.class);
     }
 
     @Override
