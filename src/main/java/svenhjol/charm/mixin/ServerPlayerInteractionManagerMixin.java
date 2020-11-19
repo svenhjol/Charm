396176503c712edc6570470167cf448dab456996
Sven Hjól <sven.hjol@protonmail.com>
2020-11-19 19:32:11 +0000

    Restore because Fabric's doesn't target drops

--- src/main/java/svenhjol/charm/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/svenhjol/charm/mixin/ServerPlayerInteractionManagerMixin.java
@@ -1,5 +1,7 @@
 package svenhjol.charm.mixin;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.server.world.ServerWorld;
@@ -11,7 +13,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import svenhjol.charm.event.PlayerBreakBlockCallback;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import svenhjol.charm.event.PlayerBreakBlockAfterBreak;
 
 @Mixin(ServerPlayerInteractionManager.class)
 public class ServerPlayerInteractionManagerMixin {
@@ -23,11 +26,15 @@ public class ServerPlayerInteractionManagerMixin {
 
     @Inject(
         method = "tryBreakBlock",
-        at = @At("HEAD"),
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/block/Block;afterBreak(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/item/ItemStack;)V"
+        ),
+        locals = LocalCapture.CAPTURE_FAILHARD,
         cancellable = true
     )
-    private void hookTryBreakBlock(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        ActionResult result = PlayerBreakBlockCallback.EVENT.invoker().interact(world, gameMode, player, pos);
+    private void hookTryBreakBlockAfterBreak(BlockPos pos, CallbackInfoReturnable<Boolean> cir, BlockState state, BlockEntity blockEntity) {
+        ActionResult result = PlayerBreakBlockAfterBreak.EVENT.invoker().interact(world, player, pos, state, blockEntity);
         if (result != ActionResult.PASS)
             cir.setReturnValue(false);
     }
