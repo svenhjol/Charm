8636b5f5aaec4e2d9801d48cf94ef9e6b208cd04
Sven Hjól <sven.hjol@protonmail.com>
2020-11-21 23:32:59 +0000

    REI container should not reference a client-only plugin

--- src/main/java/svenhjol/charm/integration/CharmReiContainerPlugin.java
+++ src/main/java/svenhjol/charm/integration/CharmReiContainerPlugin.java
@@ -2,11 +2,13 @@ package svenhjol.charm.integration;
 
 import me.shedaniel.rei.plugin.containers.CraftingContainerInfoWrapper;
 import me.shedaniel.rei.server.ContainerInfoHandler;
+import net.minecraft.util.Identifier;
+import svenhjol.charm.Charm;
 import svenhjol.charm.screenhandler.KilnScreenHandler;
 
 public class CharmReiContainerPlugin implements Runnable {
     @Override
     public void run() {
-        ContainerInfoHandler.registerContainerInfo(CharmReiPlugin.FIRING, CraftingContainerInfoWrapper.create(KilnScreenHandler.class));
+        ContainerInfoHandler.registerContainerInfo(new Identifier(Charm.MOD_ID, "plugins/firing"), CraftingContainerInfoWrapper.create(KilnScreenHandler.class));
     }
 }
