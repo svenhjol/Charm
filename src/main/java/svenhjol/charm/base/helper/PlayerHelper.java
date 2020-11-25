fa6ecd0efd5969fb4f7f3ddb3977fffbe36c359b
Sven Hjól <sven.hjol@protonmail.com>
2020-11-25 20:00:45 +0000

    Port Strange's teleport helper method

--- src/main/java/svenhjol/charm/base/helper/PlayerHelper.java
+++ src/main/java/svenhjol/charm/base/helper/PlayerHelper.java
@@ -4,6 +4,10 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class PlayerHelper {
     /**
@@ -19,6 +23,14 @@ public class PlayerHelper {
             return false;
         }
         return true;
+    }
+
+    public static void teleport(World world, BlockPos pos, PlayerEntity player) {
+        if (!world.isClient) {
+            player.requestTeleport(pos.getX() + 0.5D, pos.getY() + 0.25D, pos.getZ() + 0.5D);
+            world.sendEntityStatus(player, (byte) 46);
+            world.playSound(null, pos, SoundEvents.BLOCK_PORTAL_TRAVEL, SoundCategory.PLAYERS, 0.5F, 1.25F);
+        }
     }
 
     public static void openInventory() {
