a9bd39232c42519f8befd337c8265858c60a750f
Sven Hjól <sven.hjol@protonmail.com>
2020-11-27 17:27:58 +0000

    Now with new and improved teleport

--- src/main/java/svenhjol/charm/base/helper/PlayerHelper.java
+++ src/main/java/svenhjol/charm/base/helper/PlayerHelper.java
@@ -4,10 +4,18 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ChunkTicketType;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
+
+import java.util.EnumSet;
+import java.util.Set;
 
 public class PlayerHelper {
     /**
@@ -27,9 +35,30 @@ public class PlayerHelper {
 
     public static void teleport(World world, BlockPos pos, PlayerEntity player) {
         if (!world.isClient) {
-            player.requestTeleport(pos.getX() + 0.5D, pos.getY() + 0.25D, pos.getZ() + 0.5D);
-            world.sendEntityStatus(player, (byte) 46);
-            world.playSound(null, pos, SoundEvents.BLOCK_PORTAL_TRAVEL, SoundCategory.PLAYERS, 0.5F, 1.25F);
+            ServerWorld serverWorld = (ServerWorld) world;
+
+            double x = pos.getX() + 0.5D;
+            double y = pos.getY() + 0.25D;
+            double z = pos.getZ() + 0.5D;
+            float yaw = player.yaw;
+            float pitch = player.pitch;
+            Set<PlayerPositionLookS2CPacket.Flag> flags = EnumSet.noneOf(PlayerPositionLookS2CPacket.Flag.class);
+
+            ChunkPos chunkPos = new ChunkPos(new BlockPos(x, y, z));
+            serverWorld.getChunkManager().addTicket(ChunkTicketType.POST_TELEPORT, chunkPos, 1, player.getEntityId());
+            player.stopRiding();
+
+            if (player.isSleeping())
+                player.wakeUp(true, true);
+
+            if (world == player.world) {
+                ((ServerPlayerEntity)player).networkHandler.teleportRequest(x, y, z, yaw, pitch, flags);
+            } else {
+                ((ServerPlayerEntity)player).teleport(serverWorld, x, y, z, yaw, pitch);
+            }
+//            player.requestTeleport(pos.getX() + 0.5D, pos.getY() + 0.25D, pos.getZ() + 0.5D);
+//            world.sendEntityStatus(player, (byte) 46);
+//            world.playSound(null, pos, SoundEvents.BLOCK_PORTAL_TRAVEL, SoundCategory.PLAYERS, 0.5F, 1.25F);
         }
     }
 
