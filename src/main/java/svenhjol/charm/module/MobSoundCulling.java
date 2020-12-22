b434db6d5a717da3afd2a65c9cdcc280e76fd5db
Sven Hjól <sven.hjol@protonmail.com>
2020-12-22 01:55:59 +0000

    Fall through to vanilla value

--- src/main/java/svenhjol/charm/module/MobSoundCulling.java
+++ src/main/java/svenhjol/charm/module/MobSoundCulling.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 @Module(mod = Charm.MOD_ID, description = "Configure how often mob sounds are played.")
 public class MobSoundCulling extends CharmModule {
-    public static final int VANILLA_DELAY = 120;
 
     @Config(name = "Custom mob types", description = "List of mobs to apply the custom sound delay to.")
     public static List<String> customMobs = Arrays.asList(
@@ -23,7 +22,7 @@ public class MobSoundCulling extends CharmModule {
         "minecraft:sheep"
     );
 
-    @Config(name = "Custom mob sound delay", description = "This delay applies to all mobs in the custom mob types list. Vanilla delay is 80 or 120 depending on the mob.")
+    @Config(name = "Custom mob sound delay", description = "This delay applies to all mobs in the custom mob types list. As a guide, sheep and cows are 120 in vanilla.")
     public static int customDelay = 800;
 
     public static int getMinAmbientSoundDelay(MobEntity entity) {
@@ -32,7 +31,7 @@ public class MobSoundCulling extends CharmModule {
         if (ModuleHandler.enabled(MobSoundCulling.class) && customMobs.contains(id)) {
             return customDelay;
         } else {
-            return VANILLA_DELAY;
+            return -1;
         }
     }
 }
