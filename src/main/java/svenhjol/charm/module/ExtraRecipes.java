c5c8b6737d9d9af5fd7cda4c06e7de3c2968633c
Coranthes <josa23oreo@gmail.com>
2020-12-22 15:02:29 -0500

    More recipes, of course

--- src/main/java/svenhjol/charm/module/ExtraRecipes.java
+++ src/main/java/svenhjol/charm/module/ExtraRecipes.java
@@ -9,18 +9,33 @@ import svenhjol.charm.base.iface.Module;
 import java.util.ArrayList;
 import java.util.List;
 
-@Module(mod = Charm.MOD_ID, description = "Adds a recipe for Gilded Blackstone.")
+@Module(mod = Charm.MOD_ID, description = "Adds custom recipes.")
 public class ExtraRecipes extends CharmModule {
     @Config(name = "Gilded Blackstone", description = "If true, adds a recipe for Gilded Blackstone using gold nuggets and blackstone.")
     public static boolean useGildedBlackstone = true;
 
+    @Config(name = "Trident", description = "If true, adds a recipe for the Trident using prismarine shards and crystals.")
+    public static boolean useTrident = true;
+
+    @Config(name = "Cyan Dye", description = "If true, adds a recipe for Cyan Dye using warped roots.")
+    public static boolean useCyanDye = true;
+
+    @Config(name = "Green Dye", description = "If true, adds a recipe for Green Dye using vines.")
+    public static boolean useGreenDye = true;
     @Override
+
+
     public List<Identifier> getRecipesToRemove() {
         List<Identifier> removedRecipes = new ArrayList<>();
-
         if (!useGildedBlackstone)
             removedRecipes.add(new Identifier(Charm.MOD_ID, "extra_recipes/gilded_blackstone"));
-
+        if (!useTrident)
+            removedRecipes.add(new Identifier(Charm.MOD_ID, "extra_recipes/trident"));
+        if (!useCyanDye)
+            removedRecipes.add(new Identifier(Charm.MOD_ID, "extra_recipes/cyan_dye"));
+        if (!useGreenDye)
+            removedRecipes.add(new Identifier(Charm.MOD_ID, "extra_recipes/green_dye"));
         return removedRecipes;
     }
 }
+
