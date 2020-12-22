035f3a18c5d66fdf259768bd9a7b1e75ba20098f
Coranthes <josa23oreo@gmail.com>
2020-12-21 20:24:57 -0500

    And configurable!

--- src/main/java/svenhjol/charm/module/CraftableGildedBlackstone.java
+++ src/main/java/svenhjol/charm/module/CraftableGildedBlackstone.java
@@ -6,10 +6,6 @@ import svenhjol.charm.base.iface.Module;
 
 @Module(mod = Charm.MOD_ID, description = "Adds a recipe for Gilded Blackstone.")
 public class CraftableGildedBlackstone extends CharmModule {
-    public static CraftableGildedBlackstone GILDED_BLACKSTONE;
 
-    @Override
-    public void register() {
-        GILDED_BLACKSTONE = new CraftableGildedBlackstone();
-    }
+
 }
