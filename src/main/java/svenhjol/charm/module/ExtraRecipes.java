65f3934702b360472f074ed1e0dc4aa511c976cb
Coranthes <josa23oreo@gmail.com>
2020-12-26 12:38:21 -0500

    added a proper green dye recipe

--- src/main/java/svenhjol/charm/module/ExtraRecipes.java
+++ src/main/java/svenhjol/charm/module/ExtraRecipes.java
@@ -20,7 +20,7 @@ public class ExtraRecipes extends CharmModule {
     @Config(name = "Cyan Dye", description = "If true, adds a recipe for Cyan Dye using warped roots.")
     public static boolean useCyanDye = true;
 
-    @Config(name = "Green Dye", description = "If true, adds a recipe for Green Dye using vines.")
+    @Config(name = "Green Dye", description = "If true, adds a recipe for Green Dye using yellow and blue dyes.")
     public static boolean useGreenDye = true;
 
     @Config(name = "Soul Torch", description = "If true, adds a recipe for Soul Torches using soul sand/soul soil and sticks.")
