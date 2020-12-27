a04112162c586f49b0b965b1041479e6635b62b9
Sven Hjól <sven.hjol@protonmail.com>
2020-12-27 00:23:02 +0000

    More copypasta to try and fix enchanting power

--- src/main/java/svenhjol/charm/mixin/EnchantmentScreenHandlerMixin.java
+++ src/main/java/svenhjol/charm/mixin/EnchantmentScreenHandlerMixin.java
@@ -4,25 +4,22 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.screen.EnchantmentScreenHandler;
-import net.minecraft.screen.Property;
-import net.minecraft.screen.ScreenHandler;
-import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.screen.*;
 import net.minecraft.util.registry.Registry;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import svenhjol.charm.base.helper.EnchantmentsHelper;
 
 import java.util.List;
 import java.util.Random;
 
 @Mixin(EnchantmentScreenHandler.class)
-public abstract class EnchantmentScreenHandlerMixin {
+public abstract class EnchantmentScreenHandlerMixin extends ScreenHandler {
     @Shadow @Final private ScreenHandlerContext context;
 
     @Shadow @Final private Random random;
@@ -35,7 +32,13 @@ public abstract class EnchantmentScreenHandlerMixin {
 
     @Shadow @Final public int[] enchantmentLevel;
 
+    protected EnchantmentScreenHandlerMixin(@Nullable ScreenHandlerType<?> type, int syncId) {
+        super(type, syncId);
+    }
+
     @Shadow protected abstract List<EnchantmentLevelEntry> generateEnchantments(ItemStack stack, int slot, int level);
+
+    @Shadow @Final private Inventory inventory;
 
     /**
      * Add a hook that reimplements the check for surrounding blocks that provide enchanting bonus.
@@ -43,79 +46,83 @@ public abstract class EnchantmentScreenHandlerMixin {
      */
     @Inject(
         method = "onContentChanged",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/screen/ScreenHandlerContext;run(Ljava/util/function/BiConsumer;)V",
-            shift = At.Shift.BEFORE
-        ),
-        locals = LocalCapture.CAPTURE_FAILHARD,
+        at = @At("HEAD"),
         cancellable = true
     )
-    private void hookOnContentChanged(Inventory inventory, CallbackInfo ci, ItemStack itemStack) {
-        if (EnchantmentsHelper.ENCHANTING_BLOCKS.size() > 0) {
-            /** Copypasta from {@link EnchantmentScreenHandler#onContentChanged(Inventory) */
-            context.run((world, blockPos) -> {
-                int i = 0;
+    private void hookOnContentChanged(Inventory inventory, CallbackInfo ci) {
+        /** Copypasta from {@link EnchantmentScreenHandler#onContentChanged(Inventory) */
+        if (inventory == this.inventory) {
+            ItemStack itemStack = inventory.getStack(0);
+            if (!itemStack.isEmpty() && itemStack.isEnchantable()) {
+                this.context.run((world, blockPos) -> {
+                    int i = 0;
 
-                int j;
-                for (j = -1; j <= 1; ++j) {
-                    for (int k = -1; k <= 1; ++k) {
-                        if ((j != 0 || k != 0) && world.isAir(blockPos.add(k, 0, j)) && world.isAir(blockPos.add(k, 1, j))) {
-                            if (EnchantmentsHelper.canBlockPowerEnchantingTable(world.getBlockState(blockPos.add(k * 2, 0, j * 2)))) {
-                                ++i;
-                            }
-
-                            if (EnchantmentsHelper.canBlockPowerEnchantingTable(world.getBlockState(blockPos.add(k * 2, 1, j * 2)))) {
-                                ++i;
-                            }
-
-                            if (k != 0 && j != 0) {
-                                if (EnchantmentsHelper.canBlockPowerEnchantingTable(world.getBlockState(blockPos.add(k * 2, 0, j)))) {
+                    int j;
+                    for(j = -1; j <= 1; ++j) {
+                        for(int k = -1; k <= 1; ++k) {
+                            if ((j != 0 || k != 0) && world.isAir(blockPos.add(k, 0, j)) && world.isAir(blockPos.add(k, 1, j))) {
+                                if (EnchantmentsHelper.canBlockPowerEnchantingTable(world.getBlockState(blockPos.add(k * 2, 0, j * 2)))) {
                                     ++i;
                                 }
 
-                                if (EnchantmentsHelper.canBlockPowerEnchantingTable(world.getBlockState(blockPos.add(k * 2, 1, j)))) {
+                                if (EnchantmentsHelper.canBlockPowerEnchantingTable(world.getBlockState(blockPos.add(k * 2, 1, j * 2)))) {
                                     ++i;
                                 }
 
-                                if (EnchantmentsHelper.canBlockPowerEnchantingTable(world.getBlockState(blockPos.add(k, 0, j * 2)))) {
-                                    ++i;
-                                }
+                                if (k != 0 && j != 0) {
+                                    if (EnchantmentsHelper.canBlockPowerEnchantingTable(world.getBlockState(blockPos.add(k * 2, 0, j)))) {
+                                        ++i;
+                                    }
 
-                                if (EnchantmentsHelper.canBlockPowerEnchantingTable(world.getBlockState(blockPos.add(k, 1, j * 2)))) {
-                                    ++i;
+                                    if (EnchantmentsHelper.canBlockPowerEnchantingTable(world.getBlockState(blockPos.add(k * 2, 1, j)))) {
+                                        ++i;
+                                    }
+
+                                    if (EnchantmentsHelper.canBlockPowerEnchantingTable(world.getBlockState(blockPos.add(k, 0, j * 2)))) {
+                                        ++i;
+                                    }
+
+                                    if (EnchantmentsHelper.canBlockPowerEnchantingTable(world.getBlockState(blockPos.add(k, 1, j * 2)))) {
+                                        ++i;
+                                    }
                                 }
                             }
                         }
                     }
-                }
 
-                this.random.setSeed((long) this.seed.get());
+                    this.random.setSeed((long)this.seed.get());
 
-                for (j = 0; j < 3; ++j) {
-                    this.enchantmentPower[j] = EnchantmentHelper.calculateRequiredExperienceLevel(this.random, j, i, itemStack);
-                    this.enchantmentId[j] = -1;
-                    this.enchantmentLevel[j] = -1;
-                    if (this.enchantmentPower[j] < j + 1) {
-                        this.enchantmentPower[j] = 0;
-                    }
-                }
-
-                for (j = 0; j < 3; ++j) {
-                    if (this.enchantmentPower[j] > 0) {
-                        List<EnchantmentLevelEntry> list = this.generateEnchantments(itemStack, j, this.enchantmentPower[j]);
-                        if (list != null && !list.isEmpty()) {
-                            EnchantmentLevelEntry enchantmentLevelEntry = (EnchantmentLevelEntry) list.get(this.random.nextInt(list.size()));
-                            this.enchantmentId[j] = Registry.ENCHANTMENT.getRawId(enchantmentLevelEntry.enchantment);
-                            this.enchantmentLevel[j] = enchantmentLevelEntry.level;
+                    for(j = 0; j < 3; ++j) {
+                        this.enchantmentPower[j] = EnchantmentHelper.calculateRequiredExperienceLevel(this.random, j, i, itemStack);
+                        this.enchantmentId[j] = -1;
+                        this.enchantmentLevel[j] = -1;
+                        if (this.enchantmentPower[j] < j + 1) {
+                            this.enchantmentPower[j] = 0;
                         }
                     }
-                }
 
-                ((ScreenHandler) (Object) this).sendContentUpdates();
-                if (i > 0)
-                    ci.cancel();
-            });
+                    for(j = 0; j < 3; ++j) {
+                        if (this.enchantmentPower[j] > 0) {
+                            List<EnchantmentLevelEntry> list = this.generateEnchantments(itemStack, j, this.enchantmentPower[j]);
+                            if (list != null && !list.isEmpty()) {
+                                EnchantmentLevelEntry enchantmentLevelEntry = (EnchantmentLevelEntry)list.get(this.random.nextInt(list.size()));
+                                this.enchantmentId[j] = Registry.ENCHANTMENT.getRawId(enchantmentLevelEntry.enchantment);
+                                this.enchantmentLevel[j] = enchantmentLevelEntry.level;
+                            }
+                        }
+                    }
+
+                    this.sendContentUpdates();
+                });
+            } else {
+                for(int i = 0; i < 3; ++i) {
+                    this.enchantmentPower[i] = 0;
+                    this.enchantmentId[i] = -1;
+                    this.enchantmentLevel[i] = -1;
+                }
+            }
+
+            ci.cancel();
         }
     }
 }
