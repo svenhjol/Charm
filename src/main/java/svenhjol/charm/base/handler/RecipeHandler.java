3c85afa79f2cb025c139ebb45deb71ea21824b18
Sven Hjól <sven.hjol@protonmail.com>
2021-01-17 01:43:42 +0000

    Add extra filter step to recipes

--- src/main/java/svenhjol/charm/base/handler/RecipeHandler.java
+++ src/main/java/svenhjol/charm/base/handler/RecipeHandler.java
@@ -2,44 +2,74 @@ package svenhjol.charm.base.handler;
 
 import com.google.gson.JsonElement;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.JsonHelper;
 import svenhjol.charm.base.CharmModule;
+import svenhjol.charm.base.helper.ModHelper;
 import svenhjol.charm.base.helper.StringHelper;
 
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class RecipeHandler {
-    public static void filter(Map<Identifier, JsonElement> recipes) {
+    private static final Map<String, CharmModule> loadedModules = ModuleHandler.getLoadedModules();
+
+    public static void prepareCharmModulesFilter(Map<Identifier, JsonElement> recipes) {
         Map<String, CharmModule> loadedModules = ModuleHandler.getLoadedModules();
 
         // remove recipes specified by enabled modules
         loadedModules.values().stream().filter(m -> m.enabled && !m.getRecipesToRemove().isEmpty()).forEach(m -> {
             m.getRecipesToRemove().forEach(recipes::remove);
         });
-
-        // fetch all modded recipes
-        List<Identifier> moddedRecipes = recipes.keySet().stream().filter(r -> !r.getNamespace().equals("minecraft")).collect(Collectors.toList());
-
-        moddedRecipes.forEach(recipeId -> {
-            String path = recipeId.getPath();
-            if (!path.contains("/"))
-                return;
-
-            String moduleId = StringHelper.snakeToUpperCamel(path.split("/")[0]);
-
-            // remove recipes for disabled modules
-            if (loadedModules.containsKey(moduleId) && !loadedModules.get(moduleId).enabled)
-                recipes.remove(recipeId);
-        });
     }
 
-    public static Iterator<Map.Entry<Identifier, JsonElement>> sortedRecipes(Map<Identifier, JsonElement> recipes) {
+    public static Iterator<Map.Entry<Identifier, JsonElement>> sortAndFilterRecipes(Map<Identifier, JsonElement> recipes) {
+        return sortRecipes(recipes)
+            .filter(RecipeHandler::filterUnloadedRecipeTypes)
+            .filter(RecipeHandler::prepareCharmModulesFilter)
+            .iterator();
+    }
+
+    /**
+     * When crafting recipes are loaded in game, we want modded recipes to take precedence over vanilla.
+     * Sort recipes so that all modded recipes are iterated before vanilla recipes.
+     *
+     * @param recipes
+     * @return
+     */
+    private static Stream<Map.Entry<Identifier, JsonElement>> sortRecipes(Map<Identifier, JsonElement> recipes) {
         return Stream.concat(
             recipes.entrySet().stream().filter(r -> !r.getKey().getNamespace().equals("minecraft")),
             recipes.entrySet().stream().filter(r -> r.getKey().getNamespace().equals("minecraft"))
-        ).iterator();
+        );
+    }
+
+    /**
+     * Filter out recipes from Charm modules that have been disabled in config.
+     *
+     * @param recipe
+     * @return
+     */
+    private static boolean prepareCharmModulesFilter(Map.Entry<Identifier, JsonElement> recipe) {
+        Identifier id = recipe.getKey();
+        if (id.getNamespace().equals("minecraft"))
+            return true;
+
+        String path = id.getPath();
+        String moduleId = StringHelper.snakeToUpperCamel(path.split("/")[0]);
+
+        // remove recipes for disabled charm modules
+        return !loadedModules.containsKey(moduleId) || loadedModules.get(moduleId).enabled;
+    }
+
+    /**
+     * Filter out recipes where the recipe type refers to a module that isn't loaded.
+     *
+     * @param recipe
+     * @return
+     */
+    private static boolean filterUnloadedRecipeTypes(Map.Entry<Identifier, JsonElement> recipe) {
+        Identifier type = new Identifier(JsonHelper.getString(recipe.getValue().getAsJsonObject(), "type"));
+        return type.getNamespace().equals("minecraft") || ModHelper.isLoaded(type.getNamespace());
     }
 }
