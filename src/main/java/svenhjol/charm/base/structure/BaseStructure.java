a584d4ca7ff7b6be0395258f4f91b0bf2c1dee4b
Sven Hjól <sven.hjol@protonmail.com>
2021-01-25 19:48:08 +0000

    Can specify structure processor list for each pool

--- src/main/java/svenhjol/charm/base/structure/BaseStructure.java
+++ src/main/java/svenhjol/charm/base/structure/BaseStructure.java
@@ -5,6 +5,7 @@ import com.mojang.datafixers.util.Pair;
 import net.minecraft.structure.pool.StructurePool;
 import net.minecraft.structure.pool.StructurePoolElement;
 import net.minecraft.structure.pool.StructurePools;
+import net.minecraft.structure.processor.StructureProcessorList;
 import net.minecraft.structure.processor.StructureProcessorLists;
 import net.minecraft.util.Identifier;
 
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
+@SuppressWarnings("SameParameterValue")
 public abstract class BaseStructure {
     private final String modId;
     private final String mainFolder;
@@ -30,14 +32,18 @@ public abstract class BaseStructure {
     }
 
     protected void addStart(String pieceName, int weight) {
-        starts.add(Pair.of(StructurePoolElement.method_30435(getPiecePath(pieceName), StructureProcessorLists.EMPTY), weight));
+        addStart(pieceName, weight, StructureProcessorLists.EMPTY);
     }
 
-    protected void registerPool(String poolName, Map<String, Integer> elements, StructurePool.Projection projection) {
+    protected void addStart(String pieceName, int weight, StructureProcessorList processors) {
+        starts.add(Pair.of(StructurePoolElement.method_30435(getPiecePath(pieceName), processors), weight));
+    }
+
+    protected void registerPool(String poolName, Map<String, Integer> elements, StructurePool.Projection projection, StructureProcessorList processors) {
         final List<Pair<Function<StructurePool.Projection, ? extends StructurePoolElement>, Integer>> pieces = new ArrayList<>();
 
         elements.forEach((piece, weight) ->
-            pieces.add(Pair.of(StructurePoolElement.method_30435(getPiecePath(piece), StructureProcessorLists.EMPTY), weight)));
+            pieces.add(Pair.of(StructurePoolElement.method_30435(getPiecePath(piece), processors), weight)));
 
         StructurePools.register(new StructurePool(
             getPoolPath(poolName),
@@ -48,7 +54,7 @@ public abstract class BaseStructure {
     }
 
     protected void registerPool(String poolName, Map<String, Integer> elements) {
-        registerPool(poolName, elements, StructurePool.Projection.RIGID);
+        registerPool(poolName, elements, StructurePool.Projection.RIGID, StructureProcessorLists.EMPTY);
     }
 
     protected String getPiecePath(String piece) {
