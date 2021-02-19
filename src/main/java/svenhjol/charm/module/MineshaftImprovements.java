5ce0e14ec91dc2a65e277c88457c71b123208e5d
Sven Hjól <sven.hjol@protonmail.com>
2021-02-19 18:13:23 +0000

    Mineshafts generate different kinds of minecart. Tweaked values

--- src/main/java/svenhjol/charm/module/MineshaftImprovements.java
+++ src/main/java/svenhjol/charm/module/MineshaftImprovements.java
@@ -1,12 +1,10 @@
 package svenhjol.charm.module;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.LanternBlock;
-import net.minecraft.block.SlabBlock;
-import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.*;
+import net.minecraft.block.enums.RailShape;
+import net.minecraft.entity.vehicle.*;
 import net.minecraft.loot.LootTables;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.structure.MineshaftGenerator.MineshaftCorridor;
 import net.minecraft.structure.MineshaftGenerator.MineshaftRoom;
 import net.minecraft.structure.StructurePiece;
@@ -14,14 +12,13 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.StructureAccessor;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.MineshaftFeature;
 import svenhjol.charm.Charm;
-import svenhjol.charm.base.handler.ModuleHandler;
-import svenhjol.charm.blockentity.CrateBlockEntity;
 import svenhjol.charm.base.CharmModule;
 import svenhjol.charm.base.iface.Config;
 import svenhjol.charm.base.iface.Module;
@@ -35,25 +32,22 @@ import java.util.Random;
 
 @Module(mod = Charm.MOD_ID, description = "Adds decoration and more ores to mineshafts.")
 public class MineshaftImprovements extends CharmModule {
-    public static List<BlockState> commonFloorBlocks = new ArrayList<>();
-    public static List<BlockState> commonCeilingBlocks = new ArrayList<>();
-    public static List<BlockState> rareFloorBlocks = new ArrayList<>();
-    public static List<BlockState> rareCeilingBlocks = new ArrayList<>();
+    public static List<BlockState> floorBlocks = new ArrayList<>();
+    public static List<BlockState> ceilingBlocks = new ArrayList<>();
     public static List<BlockState> pileBlocks = new ArrayList<>();
     public static List<BlockState> roomBlocks = new ArrayList<>();
     public static List<BlockState> roomDecoration = new ArrayList<>();
-    public static List<Identifier> crateLootTables = new ArrayList<>();
+    public static List<Identifier> minecartLootTables = new ArrayList<>();
 
-    public static float floorBlockChance = 0.04F;
-    public static float ceilingBlockChance = 0.02F;
-    public static float rareBlockChance = 0.08F;
-    public static float roomBlockChance = 0.3F;
-    public static float blockPileChance = 0.1F;
-    public static float crateChance = 0.1F;
+    public static float floorBlockChance = 0.003F;
+    public static float ceilingBlockChance = 0.01F;
+    public static float roomBlockChance = 0.25F;
+    public static float blockPileChance = 0.2F;
+    public static float minecartChance = 0.15F;
 
     private static boolean isEnabled = false;
 
-    @Config(name = "Corridor blocks", description = "If true, stone, ore, lanterns and TNT will spawn inside mineshaft corridors.")
+    @Config(name = "Corridor blocks", description = "If true, lanterns and TNT will spawn inside mineshaft corridors.")
     public static boolean generateCorridorBlocks = true;
 
     @Config(name = "Corridor block piles", description = "If true, occasionally there will be piles of ore in mineshaft corridors.")
@@ -62,21 +56,17 @@ public class MineshaftImprovements extends CharmModule {
     @Config(name = "Room blocks", description = "If true, precious ores will spawn in the central mineshaft room.")
     public static boolean generateRoomBlocks = true;
 
-    @Config(name = "Add crates", description = "If true, loot crates will be added to mineshaft corridors.")
-    public static boolean generateCrates = true;
+    @Config(name = "Add minecarts", description = "If true, minecarts of different kinds will be added to mineshaft corridors.")
+    public static boolean generateMinecarts = true;
 
     @Override
     public void init() {
         isEnabled = true;
 
-        commonFloorBlocks.addAll(Arrays.asList(
-            Blocks.IRON_ORE.getDefaultState(),
-            Blocks.COAL_ORE.getDefaultState(),
-            Blocks.GOLD_ORE.getDefaultState()
-        ));
-
-        rareFloorBlocks.addAll(Arrays.asList(
+        floorBlocks.addAll(Arrays.asList(
             Blocks.TNT.getDefaultState(),
+            Blocks.IRON_ORE.getDefaultState(),
+            Blocks.GOLD_ORE.getDefaultState(),
             Blocks.LANTERN.getDefaultState()
         ));
 
@@ -85,24 +75,20 @@ public class MineshaftImprovements extends CharmModule {
             Blocks.COAL_ORE.getDefaultState(),
             Blocks.REDSTONE_ORE.getDefaultState(),
             Blocks.GOLD_ORE.getDefaultState(),
+            Blocks.LAPIS_ORE.getDefaultState(),
             Blocks.COBBLESTONE.getDefaultState(),
             Blocks.COARSE_DIRT.getDefaultState(),
             Blocks.GRAVEL.getDefaultState()
         ));
 
-        commonCeilingBlocks.addAll(Arrays.asList(
-            Blocks.COBWEB.getDefaultState(),
+        ceilingBlocks.addAll(Arrays.asList(
             Blocks.LANTERN.getDefaultState().with(LanternBlock.HANGING, true),
-            Blocks.IRON_ORE.getDefaultState(),
-            Blocks.COAL_ORE.getDefaultState()
-        ));
-
-        rareCeilingBlocks.addAll(Arrays.asList(
-            Blocks.GOLD_ORE.getDefaultState()
+            Blocks.CHAIN.getDefaultState().with(ChainBlock.AXIS, Direction.Axis.Y)
         ));
 
         roomBlocks.addAll(Arrays.asList(
             Blocks.DIAMOND_ORE.getDefaultState(),
+            Blocks.EMERALD_ORE.getDefaultState(),
             Blocks.GOLD_ORE.getDefaultState(),
             Blocks.LAPIS_ORE.getDefaultState()
         ));
@@ -113,9 +99,10 @@ public class MineshaftImprovements extends CharmModule {
             Blocks.DIRT.getDefaultState()
         ));
 
-        crateLootTables.addAll(Arrays.asList(
-            LootTables.ABANDONED_MINESHAFT_CHEST,
+        minecartLootTables.addAll(Arrays.asList(
             LootTables.SIMPLE_DUNGEON_CHEST,
+            LootTables.ABANDONED_MINESHAFT_CHEST,
+            LootTables.VILLAGE_TEMPLE_CHEST,
             LootTables.VILLAGE_CARTOGRAPHER_CHEST,
             LootTables.VILLAGE_MASON_CHEST,
             LootTables.VILLAGE_TOOLSMITH_CHEST,
@@ -127,6 +114,7 @@ public class MineshaftImprovements extends CharmModule {
         if (!isEnabled)
             return;
 
+        // don't add any decoration to mesa mineshafts
         if (((MineshaftGeneratorAccessor)piece).getMineshaftType() == MineshaftFeature.Type.MESA)
             return;
 
@@ -170,7 +158,7 @@ public class MineshaftImprovements extends CharmModule {
                     for (int ix = 0; ix <= 2; ix++) {
                         for (int iz = -1; iz <= 1; iz++) {
                             boolean valid = validFloorBlock(piece, world, ix, iy, iz, box);
-                            if (valid && rand.nextFloat() < 0.75F)
+                            if (valid && rand.nextFloat() < 0.7F)
                                 ((StructurePieceAccessor)piece).callAddBlock(world, rand.nextFloat() < 0.5 ? block1 : block2, ix, iy, iz, box);
                         }
                     }
@@ -178,27 +166,38 @@ public class MineshaftImprovements extends CharmModule {
             }
         }
 
-        if (generateCrates && ModuleHandler.enabled("charm:crates") && rand.nextFloat() < crateChance) {
-            if (rand.nextFloat() < 0.9F) {
-                int r = rand.nextInt(3) + 12;
-                int y = ((StructurePieceAccessor)piece).callApplyYTransform(0);
-                int x = ((StructurePieceAccessor)piece).callApplyXTransform(1, r);
-                int z = ((StructurePieceAccessor)piece).callApplyZTransform(1, r);
+        if (generateMinecarts && rand.nextFloat() < minecartChance) {
+            int y = ((StructurePieceAccessor)piece).callApplyYTransform(0);
+            int x = ((StructurePieceAccessor)piece).callApplyXTransform(1, 0);
+            int z = ((StructurePieceAccessor)piece).callApplyZTransform(1, 0);
 
-                BlockPos blockpos = new BlockPos(x, y, z);
+            BlockPos cartPos = new BlockPos(x, y, z);
+            Identifier loot = minecartLootTables.get(rand.nextInt(minecartLootTables.size()));
 
-                if (box.contains(blockpos)) {
-                    BlockState state = Crates.getRandomCrateBlock(rand).getDefaultState();
-                    Identifier loot = crateLootTables.get(rand.nextInt(crateLootTables.size()));
+            if (box.contains(cartPos) && world.getBlockState(cartPos).isAir() && !world.getBlockState(cartPos.down()).isAir()) {
+                BlockState blockState = Blocks.RAIL.getDefaultState().with(RailBlock.SHAPE, rand.nextBoolean() ? RailShape.NORTH_SOUTH : RailShape.EAST_WEST);
+                ((StructurePieceAccessor)piece).callAddBlock(world, blockState, x, y, z, box);
 
-                    world.setBlockState(blockpos, state, 2);
+                AbstractMinecartEntity minecartEntity;
+                ServerWorld serverWorld = world.toServerWorld();
+                double cartX = (double) cartPos.getX() + 0.5D;
+                double cartY = (double) cartPos.getY() + 0.5D;
+                double cartZ = (double) cartPos.getZ() + 0.5D;
 
-                    BlockEntity blockEntity = world.getBlockEntity(blockpos);
-                    if (blockEntity instanceof CrateBlockEntity) {
-                        ((CrateBlockEntity) blockEntity).setLootTable(loot, rand.nextLong());
-                        blockEntity.writeNbt(new CompoundTag());
-                    }
+                if (rand.nextFloat() < 0.4F) {
+                    minecartEntity = new ChestMinecartEntity(serverWorld, cartX, cartY, cartZ);
+                    ((ChestMinecartEntity)minecartEntity).setLootTable(loot, rand.nextLong());
+                } else if (rand.nextFloat() < 0.4F) {
+                    minecartEntity = new TntMinecartEntity(serverWorld, cartX, cartY, cartZ);
+                } else if (rand.nextFloat() < 0.4F) {
+                    minecartEntity = new HopperMinecartEntity(serverWorld, cartX, cartY, cartZ);
+                } else if (rand.nextFloat() < 0.4F) {
+                    minecartEntity = new FurnaceMinecartEntity(serverWorld, cartX, cartY, cartZ);
+                } else {
+                    minecartEntity = new MinecartEntity(serverWorld, cartX, cartY, cartZ);
                 }
+
+                world.spawnEntity(minecartEntity);
             }
         }
     }
@@ -266,15 +265,11 @@ public class MineshaftImprovements extends CharmModule {
     }
 
     private static BlockState getFloorBlock(Random rand) {
-        return rand.nextFloat() < rareBlockChance
-            ? getRandomBlockFromList(rareFloorBlocks, rand)
-            : getRandomBlockFromList(commonFloorBlocks, rand);
+        return getRandomBlockFromList(floorBlocks, rand);
     }
 
     private static BlockState getCeilingBlock(Random rand) {
-        return rand.nextFloat() < rareBlockChance
-            ? getRandomBlockFromList(rareCeilingBlocks, rand)
-            : getRandomBlockFromList(commonCeilingBlocks, rand);
+        return getRandomBlockFromList(ceilingBlocks, rand);
     }
 
     private static BlockState getRandomBlockFromList(List<BlockState> blocks, Random rand) {
