1f2943bb5ba3013500f1c29d9cea79df11552284
f43nd1r <account+github@faendir.com>
2021-02-25 00:31:27 +0100

    port "register processor"

--- src/main/java/svenhjol/charm/base/CharmStructures.java
+++ src/main/java/svenhjol/charm/base/CharmStructures.java
@@ -1,18 +1,28 @@
 package svenhjol.charm.base;
 
 import com.google.common.collect.ImmutableList;
+import com.mojang.serialization.Codec;
 import net.minecraft.block.Blocks;
 import net.minecraft.structure.processor.BlockIgnoreStructureProcessor;
+import net.minecraft.structure.processor.StructureProcessorType;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import svenhjol.charm.Charm;
 import svenhjol.charm.base.handler.DecorationHandler;
 import svenhjol.charm.base.structure.DataBlockProcessor;
 
 import java.util.Arrays;
 
 public class CharmStructures {
+    private static final DataBlockProcessor DATA_BLOCK_PROCESSOR = new DataBlockProcessor();
+    private static final Codec<DataBlockProcessor> CODEC = Codec.unit(DATA_BLOCK_PROCESSOR);
+    public static final StructureProcessorType<DataBlockProcessor> DATA_BLOCK_PROCESSOR_TYPE = () -> CODEC;
+
     public static void init() {
         DecorationHandler.SINGLE_POOL_ELEMENT_PROCESSORS.addAll(Arrays.asList(
-            new BlockIgnoreStructureProcessor(ImmutableList.of(Blocks.GRAY_STAINED_GLASS)),
-            new DataBlockProcessor()
+                new BlockIgnoreStructureProcessor(ImmutableList.of(Blocks.GRAY_STAINED_GLASS)),
+                DATA_BLOCK_PROCESSOR
         ));
+        Registry.register(Registry.STRUCTURE_PROCESSOR, new Identifier(Charm.MOD_ID, "data_block_processor"), DATA_BLOCK_PROCESSOR_TYPE);
     }
 }
