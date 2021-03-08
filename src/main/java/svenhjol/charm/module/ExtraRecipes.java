4f6a4a5f7b4d79cd7a854458ff2e926ff4804920
Sven Hjól <sven.hjol@protonmail.com>
2021-03-08 01:09:21 +0000

    Grinding horse armor added to extra recipes

--- src/main/java/svenhjol/charm/module/ExtraRecipes.java
+++ src/main/java/svenhjol/charm/module/ExtraRecipes.java
@@ -1,11 +1,18 @@
 package svenhjol.charm.module;
 
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import svenhjol.charm.Charm;
 import svenhjol.charm.base.CharmModule;
+import svenhjol.charm.base.handler.ModuleHandler;
 import svenhjol.charm.base.iface.Config;
 import svenhjol.charm.base.iface.Module;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -37,6 +44,19 @@ public class ExtraRecipes extends CharmModule {
     @Config(name = "Bundle from leather", description = "If true, adds a recipe for crafting bundles from leather.")
     public static boolean useBundle = true;
 
+    @Config(name = "Grindable horse armor", description = "If true, adds recipes for grinding horse armor.")
+    public static boolean useHorseArmor = true;
+
+    public static final Map<Item, Item> horseArmorRecipes = new HashMap<>();
+
+    @Override
+    public void init() {
+        horseArmorRecipes.put(Items.LEATHER_HORSE_ARMOR, Items.LEATHER);
+        horseArmorRecipes.put(Items.IRON_HORSE_ARMOR, Items.IRON_INGOT);
+        horseArmorRecipes.put(Items.GOLDEN_HORSE_ARMOR, Items.GOLD_INGOT);
+        horseArmorRecipes.put(Items.DIAMOND_HORSE_ARMOR, Items.DIAMOND);
+    }
+
     @Override
     public List<Identifier> getRecipesToRemove() {
         List<Identifier> removedRecipes = new ArrayList<>();
@@ -58,6 +78,25 @@ public class ExtraRecipes extends CharmModule {
         });
 
         return removedRecipes;
+    }
+
+    public static boolean tryUpdateGrindstoneOutput(Inventory inputs, Inventory output, @Nullable PlayerEntity player) {
+        if (!isExtraRecipesEnabled())
+            return false;
+
+        Item slot0 = inputs.getStack(0).getItem();
+        Item slot1 = inputs.getStack(1).getItem();
+
+        if (horseArmorRecipes.containsKey(slot0)) {
+            output.setStack(0, new ItemStack(horseArmorRecipes.get(slot0)));
+        } else if (horseArmorRecipes.containsKey(slot1)) {
+            output.setStack(0, new ItemStack(horseArmorRecipes.get(slot1)));
+        }
+        return true;
+    }
+
+    private static boolean isExtraRecipesEnabled() {
+        return ModuleHandler.enabled(ExtraRecipes.class);
     }
 }
 
