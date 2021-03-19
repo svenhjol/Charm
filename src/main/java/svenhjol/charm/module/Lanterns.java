789fc89c128c368de2caadb44a2d20487fcb7dee
Coranthes <josa23oreo@gmail.com>
2021-03-18 23:08:17 -0400

    Lantern modules merged

--- src/main/java/svenhjol/charm/module/Lanterns.java
+++ src/main/java/svenhjol/charm/module/Lanterns.java
@@ -4,15 +4,21 @@ import svenhjol.charm.Charm;
 import svenhjol.charm.base.CharmModule;
 import svenhjol.charm.base.iface.Module;
 import svenhjol.charm.block.CopperLanternBlock;
-import svenhjol.charm.client.CopperLanternsClient;
+import svenhjol.charm.block.GoldLanternBlock;
+import svenhjol.charm.client.LanternsClient;
 
-@Module(mod = Charm.MOD_ID, client = CopperLanternsClient.class, description = "Copper version of the vanilla lanterns.")
-public class CopperLanterns extends CharmModule {
+@Module(mod = Charm.MOD_ID, client = LanternsClient.class, description = "Gold version of the vanilla lanterns.")
+public class Lanterns extends CharmModule {
+    public static GoldLanternBlock GOLD_LANTERN;
+    public static GoldLanternBlock GOLD_SOUL_LANTERN;
     public static CopperLanternBlock COPPER_LANTERN;
     public static CopperLanternBlock COPPER_SOUL_LANTERN;
 
+
     @Override
     public void register() {
+        GOLD_LANTERN = new GoldLanternBlock(this, "gold_lantern");
+        GOLD_SOUL_LANTERN = new GoldLanternBlock(this, "gold_soul_lantern");
         COPPER_LANTERN = new CopperLanternBlock(this, "copper_lantern");
         COPPER_SOUL_LANTERN = new CopperLanternBlock(this, "copper_soul_lantern");
     }
