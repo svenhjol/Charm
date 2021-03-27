6f2f01391b2fc1cfbea63606b730102f74142b08
Sven Hjól <sven.hjol@protonmail.com>
2021-03-27 20:49:20 +0000

    Jigsaw variable height feature

---
+++ src/main/java/svenhjol/charm/base/structure/JigsawVariableHeightFeature.java
@@ -0,0 +1,55 @@
+package svenhjol.charm.base.structure;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.structure.MarginedStructureStart;
+import net.minecraft.structure.PoolStructurePiece;
+import net.minecraft.structure.StructureManager;
+import net.minecraft.structure.pool.StructurePoolBasedGenerator;
+import net.minecraft.structure.pool.StructurePools;
+import net.minecraft.util.math.BlockBox;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.world.HeightLimitView;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.feature.StructureFeature;
+import net.minecraft.world.gen.feature.StructurePoolFeatureConfig;
+
+public class JigsawVariableHeightFeature extends StructureFeature<StructurePoolFeatureConfig> {
+    private final int structureStartY;
+    private final int variation;
+    private final boolean surface;
+
+    public JigsawVariableHeightFeature(Codec<StructurePoolFeatureConfig> codec, int structureStartY, int variation, boolean surface) {
+        super(codec);
+        this.structureStartY = structureStartY;
+        this.variation = variation;
+        this.surface = surface;
+    }
+
+    public StructureFeature.StructureStartFactory<StructurePoolFeatureConfig> getStructureStartFactory() {
+        return (feature, chunkPos, blockBox, i, l) -> new Start(this, chunkPos, blockBox, i, l);
+    }
+
+    public static class Start extends MarginedStructureStart<StructurePoolFeatureConfig> {
+        private final JigsawVariableHeightFeature jigsawFeature;
+
+        public Start(JigsawVariableHeightFeature feature, ChunkPos chunkPos, BlockBox blockBox, int i, long l) {
+            super(feature, chunkPos, blockBox, i, l);
+            this.jigsawFeature = feature;
+        }
+
+        public void init(DynamicRegistryManager dynamicRegistryManager, ChunkGenerator chunkGenerator, StructureManager structureManager, ChunkPos chunkPos, Biome biome, StructurePoolFeatureConfig structurePoolFeatureConfig, HeightLimitView heightLimitView) {
+
+            int v = jigsawFeature.variation;
+            int y = jigsawFeature.structureStartY;
+            y = y - v + random.next(v * 2);
+
+            BlockPos blockPos = new BlockPos(chunkPos.getStartX(), y, chunkPos.getStartZ());
+            StructurePools.initDefaultPools();
+            StructurePoolBasedGenerator.method_30419(dynamicRegistryManager, structurePoolFeatureConfig, PoolStructurePiece::new, chunkGenerator, structureManager, blockPos, this.children, this.random, false, this.jigsawFeature.surface, heightLimitView);
+            this.setBoundingBoxFromChildren();
+        }
+    }
+}
