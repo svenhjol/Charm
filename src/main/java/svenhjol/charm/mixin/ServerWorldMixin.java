4fb9c64c6f84f36988526ab3691899baa6623918
Sven Hjól <sven.hjol@protonmail.com>
2021-04-08 22:48:43 +0000

    Hook to load things after serverworld has finished init

--- src/main/java/svenhjol/charm/mixin/ServerWorldMixin.java
+++ src/main/java/svenhjol/charm/mixin/ServerWorldMixin.java
@@ -1,10 +1,19 @@
 package svenhjol.charm.mixin;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.WorldGenerationProgressListener;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.World;
 import net.minecraft.world.chunk.WorldChunk;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.gen.Spawner;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.level.ServerWorldProperties;
+import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,10 +21,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import svenhjol.charm.event.AddEntityCallback;
+import svenhjol.charm.event.ServerWorldInitCallback;
 import svenhjol.charm.module.SnowStorms;
+
+import java.util.List;
+import java.util.concurrent.Executor;
 
 @Mixin(ServerWorld.class)
 public class ServerWorldMixin {
+    @Inject(
+        method = "<init>",
+        at = @At("TAIL")
+    )
+    private void hookInit(MinecraftServer server, Executor workerExecutor, LevelStorage.Session session, ServerWorldProperties properties, RegistryKey<World> worldKey, DimensionType dimensionType, WorldGenerationProgressListener worldGenerationProgressListener, ChunkGenerator chunkGenerator, boolean debugWorld, long seed, List<Spawner> spawners, boolean shouldTickTime, CallbackInfo ci) {
+        ServerWorldInitCallback.EVENT.invoker().interact((ServerWorld)(Object)this);
+    }
+
     @Inject(
         method = "addEntity",
         at = @At(
