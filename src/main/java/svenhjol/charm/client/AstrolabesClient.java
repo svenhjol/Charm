239a1ce5db4aeb4d91bfa8ea4bdc5347aaa632cd
Sven Hjól <sven.hjol@protonmail.com>
2021-04-12 00:45:42 +0000

    Finish astrolabe logic

--- src/main/java/svenhjol/charm/client/AstrolabesClient.java
+++ src/main/java/svenhjol/charm/client/AstrolabesClient.java
@@ -13,6 +13,7 @@ import svenhjol.charm.base.CharmModule;
 import svenhjol.charm.item.AstrolabeItem;
 import svenhjol.charm.module.Astrolabes;
 
+import javax.annotation.Nullable;
 import java.util.function.Predicate;
 
 public class AstrolabesClient extends CharmClientModule {
@@ -28,19 +29,22 @@ public class AstrolabesClient extends CharmClientModule {
     }
 
     private float handleAlignX(ItemStack stack, ClientWorld world, LivingEntity entity, int seed) {
-        return isAligned(stack, world, pos -> pos.getX() == entity.getBlockX()) ? 1 : 0;
+        return isAligned(stack, world, entity, pos
+            -> pos.getZ() == entity.getBlockZ()) ? 1 : 0;
     }
 
     private float handleAlignY(ItemStack stack, ClientWorld world, LivingEntity entity, int seed) {
-        return isAligned(stack, world, pos -> pos.getY() == entity.getBlockY()) ? 1 : 0;
+        return isAligned(stack, world, entity, pos
+            -> pos.getY() == entity.getBlockY()) ? 1 : 0;
     }
 
     private float handleAlignZ(ItemStack stack, ClientWorld world, LivingEntity entity, int seed) {
-        return isAligned(stack, world, pos -> pos.getZ() == entity.getBlockZ()) ? 1 : 0;
+        return isAligned(stack, world, entity, pos ->
+            pos.getX() == entity.getBlockX()) ? 1 : 0;
     }
 
-    private boolean isAligned(ItemStack stack, ClientWorld world, Predicate<BlockPos.Mutable> testPosition) {
-        if (world == null)
+    private boolean isAligned(ItemStack stack, @Nullable ClientWorld world, @Nullable LivingEntity entity, Predicate<BlockPos.Mutable> testPosition) {
+        if (entity == null || entity.world == null)
             return false;
 
         Identifier dimension = AstrolabeItem.getDimension(stack);
@@ -53,7 +57,7 @@ public class AstrolabesClient extends CharmClientModule {
 
         if (dimension != null) {
             // if the astrolabe was set in the nether and the user is not in the nether, multiply X and Z
-            if (dimension.equals(World.NETHER.getValue()) && world.getRegistryKey() != World.NETHER) {
+            if (dimension.equals(World.NETHER.getValue()) && entity.world.getRegistryKey() != World.NETHER) {
                 int x = position.getX();
                 int y = position.getY();
                 int z = position.getZ();
@@ -62,7 +66,7 @@ public class AstrolabesClient extends CharmClientModule {
             }
 
             // if the astrolabe was set outside the nether and the user is in the nether, divide X and Z
-            if (!dimension.equals(World.NETHER.getValue()) && world.getRegistryKey() == World.NETHER) {
+            if (!dimension.equals(World.NETHER.getValue()) && entity.world.getRegistryKey() == World.NETHER) {
                 int x = position.getX();
                 int y = position.getY();
                 int z = position.getZ();
--- src/main/java/svenhjol/charm/item/AstrolabeItem.java
+++ src/main/java/svenhjol/charm/item/AstrolabeItem.java
@@ -1,11 +1,16 @@
 package svenhjol.charm.item;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.DyeColor;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.TypedActionResult;
@@ -40,6 +45,11 @@ public class AstrolabeItem extends CharmItem {
             return TypedActionResult.success(held);
         }
 
+        BlockPos position = getPosition(held);
+        if (world.isClient && position != null) {
+            createAxisEffect((ClientWorld)world, position, user);
+        }
+
         return TypedActionResult.pass(held);
     }
 
@@ -67,5 +77,54 @@ public class AstrolabeItem extends CharmItem {
 
     public static void setPosition(ItemStack astrolabe, BlockPos position) {
         astrolabe.getOrCreateTag().putLong(POSITION_NBT, position.asLong());
+    }
+
+    @Environment(EnvType.CLIENT)
+    private void createAxisEffect(ClientWorld world, BlockPos pos, PlayerEntity player) {
+        int dist = 32;
+
+        boolean alignedX = false;
+        boolean alignedY = false;
+        boolean alignedZ = false;
+
+        double px = Math.abs(pos.getX() - player.getX());
+        double py = Math.abs(pos.getY() - player.getY());
+        double pz = Math.abs(pos.getZ() - player.getZ());
+
+        if (py <= dist) {
+            if (pz <= dist) {
+                alignedX = true;
+                for (int x = -dist; x < dist; x++) {
+                    this.createAxisParticles(world, new BlockPos(player.getX() + x, pos.getY(), pos.getZ()), DyeColor.RED);
+                }
+            }
+
+            if (px <= dist) {
+                alignedZ = true;
+                for (int z = -dist; z < dist; z++) {
+                    this.createAxisParticles(world, new BlockPos(pos.getX(), pos.getY(), player.getZ() + z), DyeColor.BLUE);
+                }
+            }
+        }
+
+        if (px <= dist && pz <= dist) {
+            alignedY = true;
+            for (int y = -dist; y < dist; y++) {
+                this.createAxisParticles(world, new BlockPos(pos.getX(), player.getY() + y, pos.getZ()), DyeColor.PURPLE);
+            }
+        }
+    }
+
+    private void createAxisParticles(ClientWorld world, BlockPos pos, DyeColor color) {
+        float[] col = color.getColorComponents();
+        for (int i = 0; i < 3; i++) {
+            double d = (double) pos.getX() + 0.5D + (world.random.nextDouble() * 0.4D) - 0.2D;
+            double e = (double) pos.getY() + 0.25D;
+            double f = (double) pos.getZ() + 0.5D + (world.random.nextDouble() * 0.4D) - 0.2D;
+
+            world.addImportantParticle(ParticleTypes.ENTITY_EFFECT, d, e, f, col[0], col[1], col[2]);
+        }
+
+        world.addParticle(ParticleTypes.SMOKE, pos.getX() + 0.5D, pos.getY() + 0.5D, pos.getZ() + 0.5D, 0, 0, 0);
     }
 }
