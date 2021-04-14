db3efdc2cbf6efa5fd35ca053972e48e7300bbfa
Sven Hjól <sven.hjol@protonmail.com>
2021-04-14 23:28:36 +0000

    Potion recipe now uses amethyst shard

--- src/main/java/svenhjol/charm/potion/SpelunkingPotion.java
+++ src/main/java/svenhjol/charm/potion/SpelunkingPotion.java
@@ -10,6 +10,6 @@ import svenhjol.charm.module.PotionOfSpelunking;
 public class SpelunkingPotion extends CharmPotion {
     public SpelunkingPotion(CharmModule module) {
         super(module, "spelunking", new StatusEffectInstance(PotionOfSpelunking.SPELUNKING_EFFECT, PotionOfSpelunking.duration * 20));
-        registerRecipe(Potions.AWKWARD, Items.ENDER_EYE);
+        registerRecipe(Potions.AWKWARD, Items.AMETHYST_SHARD);
     }
 }
