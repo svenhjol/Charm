a60cc4f0e4fdd15419f3eb1403301c29864ef046
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-04-18 18:44:15 -0400

    changed it to only replace source blocks

--- src/main/java/svenhjol/charm/module/Bumblezone.java
+++ src/main/java/svenhjol/charm/module/Bumblezone.java
@@ -35,7 +35,7 @@ public class Bumblezone extends CharmModule {
             BlockState neighborBlock = world.getBlockState(neighborPos);
 
             // Found watery block to replace, store the position of the water
-            if (!neighborBlock.getBlock().is(bumblezoneFluid) && neighborBlock.getMaterial() == Material.WATER) {
+            if (!neighborBlock.getBlock().is(bumblezoneFluid) && neighborBlock.getMaterial() == Material.WATER && neighborBlock.getFluidState().isStill()) {
                 waterPos.add(neighborPos.toImmutable());
                 recursiveReplaceWater(world, neighborPos, depth + 1, maxDepth, waterPos);
             }
