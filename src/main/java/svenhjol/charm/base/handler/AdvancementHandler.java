e032ac4cb4627c01a07d9a48fdb058ceeaa21f4d
Sven Hjól <sven.hjol@protonmail.com>
2021-05-03 00:22:17 +0000

    Method to get all players in range for advancement

--- src/main/java/svenhjol/charm/base/handler/AdvancementHandler.java
+++ src/main/java/svenhjol/charm/base/handler/AdvancementHandler.java
@@ -1,6 +1,10 @@
 package svenhjol.charm.base.handler;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
 import svenhjol.charm.module.Core;
 
 import java.util.ArrayList;
@@ -16,5 +20,9 @@ public class AdvancementHandler {
             if (!m.enabled || !Core.advancements)
                 advancementsToRemove.addAll(m.advancements());
         }));
+    }
+
+    public static List<PlayerEntity> getPlayersInRange(World world, BlockPos pos) {
+        return world.getNonSpectatingEntities(PlayerEntity.class, new Box(pos).expand(8.0D));
     }
 }
