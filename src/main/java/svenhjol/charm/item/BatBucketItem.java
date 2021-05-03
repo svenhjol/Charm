c1c0b8f87ce21529af5a0d51f05d710531520abf
Sven Hjól <sven.hjol@protonmail.com>
2021-05-03 18:01:10 +0000

    No longer needs block right-click to release bat

--- src/main/java/svenhjol/charm/item/BatBucketItem.java
+++ src/main/java/svenhjol/charm/item/BatBucketItem.java
@@ -1,31 +1,39 @@
 package svenhjol.charm.item;
 
-import io.netty.buffer.Unpooled;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.passive.BatEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.*;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import svenhjol.charm.base.CharmModule;
 import svenhjol.charm.base.helper.ItemNBTHelper;
 import svenhjol.charm.base.helper.MobHelper;
 import svenhjol.charm.base.item.CharmItem;
+import svenhjol.charm.init.CharmAdvancements;
 import svenhjol.charm.module.BatBuckets;
 
+import java.util.List;
+import java.util.function.Predicate;
+
 public class BatBucketItem extends CharmItem {
-    public static final String STORED_BAT = "stored_bat";
+    public static final String STORED_BAT_NBT = "StoredBat";
 
     public BatBucketItem(CharmModule module) {
         super(module, "bat_bucket", new Item.Settings()
@@ -34,23 +42,14 @@ public class BatBucketItem extends CharmItem {
     }
 
     @Override
-    public ActionResult useOnBlock(ItemUsageContext context) {
-        if (context.getPlayer() == null || context.getWorld().isClient)
-            return ActionResult.FAIL;
-
-        PlayerEntity player = context.getPlayer();
-        World world = context.getWorld();
-        BlockPos pos = context.getBlockPos();
-        Direction facing = context.getSide();
-        Hand hand = context.getHand();
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         ItemStack held = player.getStackInHand(hand);
-
-        world.playSound(null, player.getBlockPos(), SoundEvents.ENTITY_BAT_TAKEOFF, SoundCategory.NEUTRAL, 1.0F, 1.0F);
+        BlockPos pos = player.getBlockPos();
+        Direction facing = player.getHorizontalFacing();
 
         if (!world.isClient && !player.isCreative()) {
-
             double x = pos.getX() + 0.5F + facing.getOffsetX();
-            double y = pos.getY() + 0.25F + (world.random.nextFloat() / 2.0F) + facing.getOffsetY();
+            double y = pos.getY() + 0.65F + (world.random.nextFloat() / 2.0F) + facing.getOffsetY();
             double z = pos.getZ() + 0.5F + facing.getOffsetZ();
             BlockPos spawnPos = new BlockPos(x, y, z);
 
@@ -58,30 +57,36 @@ public class BatBucketItem extends CharmItem {
             BatEntity bat = MobHelper.spawn(EntityType.BAT, (ServerWorld)world, spawnPos, SpawnReason.BUCKET);
             if (bat != null) {
 
-                NbtCompound data = ItemNBTHelper.getCompound(held, STORED_BAT);
+                NbtCompound data = ItemNBTHelper.getCompound(held, STORED_BAT_NBT);
                 if (!data.isEmpty())
                     bat.readCustomDataFromNbt(data);
 
                 world.spawnEntity(bat);
 
-                // damage the bat :(
-                float health = bat.getHealth();
-                bat.setHealth(health - 1.0F);
+                if (BatBuckets.damageBat) {
+                    // damage the bat :(
+                    float health = bat.getHealth();
+                    bat.setHealth(health - 1.0F);
+                }
             }
         }
+
         player.swingHand(hand);
 
-        // send message to client to start glowing
         if (!world.isClient) {
-            PacketByteBuf data = new PacketByteBuf(Unpooled.buffer());
-            data.writeDouble(BatBuckets.glowingRange);
-            data.writeInt(BatBuckets.glowingTime);
-            ServerPlayNetworking.send((ServerPlayerEntity)player, BatBuckets.MSG_CLIENT_SET_GLOWING, data);
+            CharmAdvancements.ACTION_PERFORMED.trigger((ServerPlayerEntity) player, BatBuckets.TRIGGER_USED_BAT_BUCKET);
+            world.playSound(null, player.getBlockPos(), SoundEvents.ENTITY_BAT_TAKEOFF, SoundCategory.NEUTRAL, 1.0F, 1.0F);
+
+            Box box = player.getBoundingBox().expand(BatBuckets.glowingRange, BatBuckets.glowingRange / 2.0, BatBuckets.glowingRange);
+            Predicate<LivingEntity> selector = entity -> true;
+            StatusEffectInstance effect = new StatusEffectInstance(StatusEffects.GLOWING, BatBuckets.glowingTime * 20);
+            List<LivingEntity> entities = player.world.getEntitiesByClass(LivingEntity.class, box, selector);
+            entities.forEach(entity -> entity.addStatusEffect(effect));
         }
 
         if (!player.isCreative())
             player.setStackInHand(hand, new ItemStack(Items.BUCKET));
 
-        return ActionResult.SUCCESS;
+        return TypedActionResult.consume(held);
     }
 }
