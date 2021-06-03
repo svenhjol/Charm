742ea5af8cf6ee428c286e0eb46770d01c67dd95
Coranthes <josa23oreo@gmail.com>
2021-06-03 19:22:36 -0400

    hitboxes fixed

--- src/main/java/svenhjol/charm/block/CharmLanternBlock.java
+++ src/main/java/svenhjol/charm/block/CharmLanternBlock.java
@@ -4,10 +4,15 @@ import net.minecraft.block.*;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import svenhjol.charm.module.CharmModule;
 
 public class CharmLanternBlock extends LanternBlock implements ICharmBlock {
     protected CharmModule module;
+    private static final VoxelShape STANDING_SHAPE = Block.createCuboidShape(5.0D, 0.0D, 5.0D, 11.0D, 11.0D, 11.0D);
+    private static final VoxelShape HANGING_SHAPE = Block.createCuboidShape(5.0D, 4.0D, 5.0D, 11.0D, 15.0D, 11.0D);
 
     public CharmLanternBlock(CharmModule module, String name, AbstractBlock.Settings settings) {
         super(settings);
@@ -17,6 +22,10 @@ public class CharmLanternBlock extends LanternBlock implements ICharmBlock {
 
     public CharmLanternBlock(CharmModule module, String name) {
         this(module, name, Settings.copy(Blocks.LANTERN));
+    }
+
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return state.get(HANGING) ? HANGING_SHAPE : STANDING_SHAPE;
     }
 
     @Override
