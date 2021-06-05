8d94e51fb3e9b8088c1ebdf58a45be56868b9bd7
Sven Hjól <sven.hjol@protonmail.com>
2021-06-05 17:27:04 +0000

    Simple static loot register

--- src/main/java/svenhjol/charm/init/CharmLoot.java
+++ src/main/java/svenhjol/charm/init/CharmLoot.java
@@ -4,14 +4,23 @@ import net.minecraft.util.Identifier;
 import svenhjol.charm.Charm;
 import svenhjol.charm.helper.LootHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CharmLoot {
-    public static Identifier VILLAGE_LUMBERJACK = new Identifier(Charm.MOD_ID, "chests/village/village_lumberjack");
-    public static Identifier VILLAGE_BEEKEEPER = new Identifier(Charm.MOD_ID, "chests/village/village_beekeeper");
-    public static Identifier VILLAGE_LIBRARIAN = new Identifier(Charm.MOD_ID, "chests/village/village_librarian");
+    public static List<Identifier> REGISTER = new ArrayList<>();
+
+    public static Identifier VILLAGE_LUMBERJACK = createLootTable("chests/village/village_lumberjack");
+    public static Identifier VILLAGE_BEEKEEPER = createLootTable("chests/village/village_beekeeper");
+    public static Identifier VILLAGE_LIBRARIAN = createLootTable("chests/village/village_librarian");
 
     public static void init() {
-        LootHelper.CUSTOM_LOOT_TABLES.add(VILLAGE_LUMBERJACK);
-        LootHelper.CUSTOM_LOOT_TABLES.add(VILLAGE_BEEKEEPER);
-        LootHelper.CUSTOM_LOOT_TABLES.add(VILLAGE_LIBRARIAN);
+        LootHelper.CUSTOM_LOOT_TABLES.addAll(REGISTER);
+    }
+
+    public static Identifier createLootTable(String name) {
+        Identifier id = new Identifier(Charm.MOD_ID, name);
+        REGISTER.add(id);
+        return id;
     }
 }
