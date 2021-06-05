df3b930ae84c5227229c0a7f789c65b05b561def
Sven Hjól <sven.hjol@protonmail.com>
2021-06-05 16:55:40 +0000

    Unused transformer method

--- src/main/java/svenhjol/charm/integration/rei/CharmReiClientPlugin.java
+++ src/main/java/svenhjol/charm/integration/rei/CharmReiClientPlugin.java
@@ -5,7 +5,6 @@ import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
-import me.shedaniel.rei.api.client.registry.transfer.TransferHandlerRegistry;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -38,10 +37,5 @@ public class CharmReiClientPlugin implements REIClientPlugin {
     @Override
     public void registerScreens(ScreenRegistry registry) {
         registry.registerContainerClickArea(new Rectangle(78, 32, 28, 23), KilnScreen.class, CharmReiCategories.FIRING);
-    }
-
-    @Override
-    public void registerTransferHandlers(TransferHandlerRegistry registry) {
-//        registry.register(new CharmReiTransferHandler());
     }
 }
