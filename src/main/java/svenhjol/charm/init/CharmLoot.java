29905e04b51e53b98f9d8508156ae2ee88c44890
Sven Hjól <sven.hjol@protonmail.com>
2021-06-05 00:27:33 +0000

    Standardize loot table names

--- src/main/java/svenhjol/charm/init/CharmLoot.java
+++ src/main/java/svenhjol/charm/init/CharmLoot.java
@@ -5,8 +5,8 @@ import svenhjol.charm.Charm;
 import svenhjol.charm.helper.LootHelper;
 
 public class CharmLoot {
-    public static Identifier VILLAGE_LUMBERJACK = new Identifier(Charm.MOD_ID, "chests/village/village_lumberjack_house");
-    public static Identifier VILLAGE_BEEKEEPER = new Identifier(Charm.MOD_ID, "chests/village/village_beekeeper_house");
+    public static Identifier VILLAGE_LUMBERJACK = new Identifier(Charm.MOD_ID, "chests/village/village_lumberjack");
+    public static Identifier VILLAGE_BEEKEEPER = new Identifier(Charm.MOD_ID, "chests/village/village_beekeeper");
     public static Identifier VILLAGE_LIBRARIAN = new Identifier(Charm.MOD_ID, "chests/village/village_librarian");
 
     public static void init() {
