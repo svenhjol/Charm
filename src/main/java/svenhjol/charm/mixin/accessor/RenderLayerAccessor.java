2db1902d87491696a24c3bbce12943a8ee89156e
Sven Hjól <sven.hjol@protonmail.com>
2021-06-12 00:05:57 +0000

    Mutable accessor required to stop exception when reloading textures

--- src/main/java/svenhjol/charm/mixin/accessor/RenderLayerAccessor.java
+++ src/main/java/svenhjol/charm/mixin/accessor/RenderLayerAccessor.java
@@ -5,12 +5,14 @@ import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import svenhjol.charm.annotation.CharmMixin;
 
 @Mixin(RenderLayer.class)
 @CharmMixin(required = true)
 public interface RenderLayerAccessor<T extends Entity, M extends EntityModel<T>> {
+    @Mutable
     @Accessor
     void setRenderer(RenderLayerParent<T, M> renderer);
 }
