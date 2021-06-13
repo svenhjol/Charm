c255516e492ba06fd2fde4991f6e32311ac1085d
Sven Hjól <sven.hjol@protonmail.com>
2021-06-13 02:38:05 +0000

    Restore oak bookshelf

--- src/main/java/svenhjol/charm/module/variant_bookshelves/VariantBookshelves.java
+++ src/main/java/svenhjol/charm/module/variant_bookshelves/VariantBookshelves.java
@@ -1,11 +1,9 @@
 package svenhjol.charm.module.variant_bookshelves;
 
-import net.minecraft.world.level.block.Blocks;
 import svenhjol.charm.Charm;
 import svenhjol.charm.annotation.Module;
 import svenhjol.charm.enums.IVariantMaterial;
 import svenhjol.charm.enums.VanillaVariantMaterial;
-import svenhjol.charm.helper.OverrideHandler;
 import svenhjol.charm.module.CharmModule;
 
 import java.util.HashMap;
@@ -17,14 +15,9 @@ public class VariantBookshelves extends CharmModule {
 
     @Override
     public void register() {
-        VanillaVariantMaterial.getTypesWithout(VanillaVariantMaterial.OAK).forEach(type -> {
+        VanillaVariantMaterial.getTypes().forEach(type -> {
             registerBookshelf(this, type);
         });
-    }
-
-    @Override
-    public void init() {
-        OverrideHandler.changeBlockTranslationKey(Blocks.BOOKSHELF, "block.charm.oak_bookshelf");
     }
 
     public static VariantBookshelfBlock registerBookshelf(CharmModule module, IVariantMaterial material) {
