5bf7376a90babc717f2abf72b68dea408c49cc0e
Sven Hjól <sven.hjol@protonmail.com>
2021-06-14 19:18:28 +0000

    Accessor is a required mixin

--- src/main/java/svenhjol/charm/mixin/accessor/AxeItemAccessor.java
+++ src/main/java/svenhjol/charm/mixin/accessor/AxeItemAccessor.java
@@ -5,10 +5,12 @@ import net.minecraft.world.level.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import svenhjol.charm.annotation.CharmMixin;
 
 import java.util.Map;
 
 @Mixin(AxeItem.class)
+@CharmMixin(required = true)
 public interface AxeItemAccessor {
     @Mutable @Accessor("STRIPPABLES")
     static Map<Block, Block> getStrippables() {
