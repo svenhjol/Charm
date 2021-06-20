a879f8c43b2ef90ac5dcf5468fc966bd62e2800e
Sven Hjól <sven.hjol@protonmail.com>
2021-06-20 22:42:25 +0000

    Wrong logic in anvil repair event.  svenhjol/Charm#587

--- src/main/java/svenhjol/charm/module/extra_recipes/ExtraRecipes.java
+++ src/main/java/svenhjol/charm/module/extra_recipes/ExtraRecipes.java
@@ -78,12 +78,12 @@ public class ExtraRecipes extends CharmModule {
     }
 
     private boolean handleCheckAnvilRepair(AnvilMenu handler, Player player, ItemStack leftStack, ItemStack rightStack) {
-        if (!useLeatherForElytra || player == null || player.level == null)
-            return false;
+        if (leftStack.getItem() != Items.ELYTRA || !useLeatherForElytra || player == null || player.level == null)
+            return true; // true to bypass
 
         // don't activate if insomnia is enabled
         if (!player.level.isClientSide && player.level.getGameRules().getBoolean(GameRules.RULE_DOINSOMNIA))
-            return false;
+            return false; // false to explicitly deny repair if insomnia is enabled
 
         return leftStack.getItem() == Items.ELYTRA && rightStack.getItem() == Items.LEATHER;
     }
