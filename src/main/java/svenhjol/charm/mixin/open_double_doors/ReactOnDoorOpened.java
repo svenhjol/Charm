feac21c6ca8faf773096d00f2b50bd162ec320fb
Sven Hjól <sven.hjol@protonmail.com>
2021-06-26 00:46:50 +0000

    Switch to mixin so that all entities can open double doors

---
+++ src/main/java/svenhjol/charm/mixin/open_double_doors/ReactOnDoorOpened.java
@@ -0,0 +1,38 @@
+package svenhjol.charm.mixin.open_double_doors;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.DoorBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import svenhjol.charm.module.open_double_doors.OpenDoubleDoors;
+
+@Mixin(DoorBlock.class)
+public class ReactOnDoorOpened {
+    @Inject(
+        method = "use",
+        at = @At("RETURN")
+    )
+    private void hookUse(BlockState state, Level level, BlockPos pos, Player player, InteractionHand interactionHand, BlockHitResult hit, CallbackInfoReturnable<InteractionResult> cir) {
+        if (cir.getReturnValue() == InteractionResult.CONSUME || cir.getReturnValue() == InteractionResult.SUCCESS)
+            OpenDoubleDoors.tryOpenNeighbour(level, state, pos, state.getValue(DoorBlock.OPEN));
+    }
+
+    @Inject(
+        method = "setOpen",
+        at = @At("RETURN")
+    )
+    private void hookSetOpen(Entity entity, Level level, BlockState state, BlockPos pos, boolean bl, CallbackInfo ci) {
+        if (entity != null)
+            OpenDoubleDoors.tryOpenNeighbour(level, state, pos, bl);
+    }
+}
--- src/main/java/svenhjol/charm/module/open_double_doors/OpenDoubleDoors.java
+++ src/main/java/svenhjol/charm/module/open_double_doors/OpenDoubleDoors.java
@@ -1,45 +1,20 @@
 package svenhjol.charm.module.open_double_doors;
 
-import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DoorBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.DoorHingeSide;
-import net.minecraft.world.phys.BlockHitResult;
 import svenhjol.charm.Charm;
 import svenhjol.charm.annotation.Module;
 import svenhjol.charm.module.CharmModule;
-
-import java.util.function.Function;
 
 import static net.minecraft.world.level.block.state.properties.DoorHingeSide.LEFT;
 
 @Module(mod = Charm.MOD_ID, description = "Automatically opens double doors.")
 public class OpenDoubleDoors extends CharmModule {
-    @Override
-    public void init() {
-        UseBlockCallback.EVENT.register(this::handleOpenDoor);
-    }
-
-    private InteractionResult handleOpenDoor(Player player, Level level, InteractionHand hand, BlockHitResult hitResult) {
-        BlockPos pos = hitResult.getBlockPos();
-        BlockState state = level.getBlockState(pos);
-        Function<DoorBlock, InteractionResult> use =
-            d -> d.use(state, level, pos, player, hand, hitResult);
-
-        if (!DoorBlock.isWoodenDoor(state)) {
-            Charm.LOG.debug("Door is not a wooden door");
-            return InteractionResult.PASS;
-        }
-
-        DoorBlock door = (DoorBlock) state.getBlock();
-
-        boolean isClosed = !door.isOpen(state);
+    public static void tryOpenNeighbour(Level level, BlockState state, BlockPos pos, boolean isClosed) {
         Direction facing = state.getValue(DoorBlock.FACING);
         DoorHingeSide hinge = state.getValue(DoorBlock.HINGE);
 
@@ -53,19 +28,19 @@ public class OpenDoubleDoors extends CharmModule {
 
         if (neighborPos == null) {
             Charm.LOG.debug("Neighbor blockpos not found");
-            return use.apply(door);
+            return;
         }
 
         BlockState neighborState = level.getBlockState(neighborPos);
         if (!DoorBlock.isWoodenDoor(neighborState)) {
             Charm.LOG.debug("Neighbor door is not a wooden door");
-            return use.apply(door);
+            return;
         }
 
         Direction neighborFacing = neighborState.getValue(DoorBlock.FACING);
         if (neighborFacing != facing) {
             Charm.LOG.debug("Neighbor door does not face the same way");
-            return use.apply(door);
+            return;
         }
 
         DoorBlock neighbor = (DoorBlock) neighborState.getBlock();
@@ -73,11 +48,9 @@ public class OpenDoubleDoors extends CharmModule {
 
         if (neighborHinge == hinge) {
             Charm.LOG.debug("Neighbor hinge is not opposite");
-            return use.apply(door);
+            return;
         }
 
-        use.apply(door);
-        neighbor.setOpen(player, level, neighborState, neighborPos, isClosed);
-        return InteractionResult.sidedSuccess(level.isClientSide);
+        neighbor.setOpen(null, level, neighborState, neighborPos, isClosed);
     }
 }
