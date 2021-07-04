2102a6520e25189a25a63d10375bef0cdd46bb6e
Sven Hjól <sven.hjol@protonmail.com>
2021-07-04 20:36:16 +0000

    Accessor to fetch hovered slot

--- src/main/java/svenhjol/charm/mixin/accessor/AbstractContainerScreenAccessor.java
+++ src/main/java/svenhjol/charm/mixin/accessor/AbstractContainerScreenAccessor.java
@@ -1,11 +1,15 @@
 package svenhjol.charm.mixin.accessor;
 
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.world.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(AbstractContainerScreen.class)
 public interface AbstractContainerScreenAccessor {
+    @Accessor("hoveredSlot")
+    Slot getHoveredSlot();
+
     @Accessor("leftPos")
     int getX();
 
