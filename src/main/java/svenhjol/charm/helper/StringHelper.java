37a6873b6f9131db38b5f8367ba876cfb5bac09a
Sven Hjól <sven.hjol@protonmail.com>
2021-07-05 13:52:52 +0000

    Add helper method to resolve lang strings on server

--- src/main/java/svenhjol/charm/helper/StringHelper.java
+++ src/main/java/svenhjol/charm/helper/StringHelper.java
@@ -1,12 +1,29 @@
 package svenhjol.charm.helper;
 
 import com.google.common.base.CaseFormat;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.metadata.ModMetadata;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * @version 1.0.0-charm
+ * @version 1.0.1-charm
  */
 @SuppressWarnings({"unused", "UnusedReturnValue"})
 public class StringHelper {
+    private static final Map<String, JsonObject> languageStrings = new HashMap<>();
+
     public static String snakeToUpperCamel(String string) {
         return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, string);
     }
@@ -41,5 +58,35 @@ public class StringHelper {
             out.append(currentChar);
         }
         return out.toString();
+    }
+
+    @Nullable
+    public static String tryResolveLanguageKey(String modId, String key) {
+        if (!languageStrings.containsKey(modId)) {
+            // try and parse the mod's en_us language file into a json object
+            FabricLoader.getInstance().getModContainer(modId).ifPresent(container -> {
+                ModMetadata metadata = container.getMetadata();
+                Path path = container.getPath("assets/" + modId + "/lang/en_us.json");
+
+                try (JsonReader reader = new JsonReader(new InputStreamReader(Files.newInputStream(path), StandardCharsets.UTF_8))) {
+                    JsonElement parsed = new JsonParser().parse(reader);
+                    languageStrings.put(modId, parsed.getAsJsonObject());
+                    LogHelper.debug(StringHelper.class, "Successfully opened language file for `" + modId + "`");
+                } catch (IOException e) {
+                    LogHelper.error(StringHelper.class, "Failed to open language file: " + e.getMessage());
+                }
+            });
+        }
+
+        if (languageStrings.containsKey(modId)) {
+            // try and fetch the resolved language key from the mod's language strings
+            JsonElement el = languageStrings.get(modId).get(key);
+            if (el != null) {
+                return el.getAsString();
+            }
+            LogHelper.debug(StringHelper.class, "Could not resolve key `" + key + "` for mod `" + modId + "`");
+        }
+
+        return null;
     }
 }
