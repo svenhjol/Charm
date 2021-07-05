1487b503d917ba49aa7be844c8cea07bf75c68b6
Sven Hjól <sven.hjol@protonmail.com>
2021-07-05 13:53:28 +0000

    Mixin hook to call advancement title translation

--- src/main/java/svenhjol/charm/helper/AdvancementHelper.java
+++ src/main/java/svenhjol/charm/helper/AdvancementHelper.java
@@ -1,16 +1,55 @@
 package svenhjol.charm.helper;
 
 import net.minecraft.advancements.Advancement;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentUtils;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import svenhjol.charm.loader.CharmModule;
+import svenhjol.charm.loader.ModuleLoader;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * @version 1.0.0-charm
+ */
 public class AdvancementHelper {
     private static final List<ResourceLocation> ADVANCEMENTS_TO_REMOVE = new ArrayList<>();
+
+    @Nullable
+    public static Component tryTranslateTitle(Component title) {
+        List<String> modIds = ModuleLoader.getModIds();
+
+        // the title is a translate component of `chat.square_brackets`; don't try and work with a non-translatable component
+        if (!(title instanceof TranslatableComponent trans))
+            return null;
+
+        // the text we are interested in is the only argument of the square_brackets component
+        TranslatableComponent first;
+        Object[] args = trans.getArgs();
+        if (args.length == 0 || !(args[0] instanceof TranslatableComponent)) {
+            first = trans;
+        } else {
+            first = (TranslatableComponent)args[0];
+        }
+
+        for (String modId : modIds) {
+            // we are only interested in translating charm and charm-based mods
+            if (first.getKey().contains(modId)) {
+                String resolved = StringHelper.tryResolveLanguageKey(modId, first.getKey());
+                if (resolved != null) {
+                    LogHelper.debug(AdvancementHelper.class, "Resolved key `" + first.getKey() + "` to `" + resolved + "`");
+                    return ComponentUtils.wrapInSquareBrackets(new TranslatableComponent(resolved));
+                }
+            }
+        }
+
+        return null;
+    }
 
     public static void removeAdvancement(ResourceLocation id) {
         LogHelper.debug(AdvancementHelper.class, "Adding `" + id + "` to list of advancements to remove");
@@ -26,7 +65,7 @@ public class AdvancementHelper {
             // remove exact matches
             AtomicInteger exactMatches = new AtomicInteger();
             keys.stream().filter(a -> a.equals(toRemove)).forEach(a -> {
-                LogHelper.debug(AdvancementHelper.class, " > Filtering out exact match `" + a + "`");
+                LogHelper.debug(AdvancementHelper.class, "> Filtering out exact match `" + a + "`");
                 exactMatches.getAndIncrement();
                 map.remove(a);
             });
@@ -38,7 +77,7 @@ public class AdvancementHelper {
                 .filter(a -> a.getNamespace().equals(toRemove.getNamespace()))
                 .filter(a -> a.getPath().startsWith(toRemove.getPath()))
                 .forEach(a -> {
-                    LogHelper.debug(AdvancementHelper.class, " > Filtering out fuzzy match `" + a + "`");
+                    LogHelper.debug(AdvancementHelper.class, "> Filtering out fuzzy match `" + a + "`");
                     map.remove(a);
                 });
         });
