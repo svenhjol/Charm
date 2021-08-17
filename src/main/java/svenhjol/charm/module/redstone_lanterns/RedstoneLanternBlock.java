5808a9c47b4c222c92c4854af138f7edf37bd4f9
Sven Hjól <sven.hjol@protonmail.com>
2021-08-17 23:52:08 +0100

    Don't warn about deprecation

--- src/main/java/svenhjol/charm/module/redstone_lanterns/RedstoneLanternBlock.java
+++ src/main/java/svenhjol/charm/module/redstone_lanterns/RedstoneLanternBlock.java
@@ -16,6 +16,7 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 
+@SuppressWarnings("deprecation")
 public class RedstoneLanternBlock extends CharmLanternBlock {
     public static BooleanProperty LIT = BlockStateProperties.LIT;
 
