cb9c771e70a726f0f32111b11ca2e13e31a3bd35
Sven Hjól <sven.hjol@protonmail.com>
2021-09-28 20:13:43 +0000

    Automatic recipe unlock works

---
+++ src/main/java/svenhjol/charm/module/automatic_recipe_unlock/AutomaticRecipeUnlock.java
@@ -0,0 +1,32 @@
+package svenhjol.charm.module.automatic_recipe_unlock;
+
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeManager;
+import svenhjol.charm.Charm;
+import svenhjol.charm.annotation.CommonModule;
+import svenhjol.charm.loader.CharmModule;
+
+import java.util.Collection;
+
+@CommonModule(mod = Charm.MOD_ID, description = "Unlocks all vanilla recipes.")
+public class AutomaticRecipeUnlock extends CharmModule {
+    @Override
+    public void runWhenEnabled() {
+        ServerEntityEvents.ENTITY_LOAD.register(this::handleServerJoin);
+    }
+
+    private void handleServerJoin(Entity entity, ServerLevel level) {
+        if (!Charm.LOADER.isEnabled(AutomaticRecipeUnlock.class))
+            return;
+
+        if (entity instanceof Player player) {
+            RecipeManager recipeManager = player.level.getRecipeManager();
+            Collection<Recipe<?>> allRecipes = recipeManager.getRecipes();
+            player.awardRecipes(allRecipes);
+        }
+    }
+}
