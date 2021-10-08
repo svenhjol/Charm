53849d7bab58f0df8945132608d908f47d141790
Sven Hjól <sven.hjol@protonmail.com>
2021-10-08 17:27:29 +0000

    Remove potion glint

---
+++ src/main/java/svenhjol/charm/mixin/remove_potion_glint/CheckPotionHasGlintMixin.java
@@ -0,0 +1,27 @@
+package svenhjol.charm.mixin.remove_potion_glint;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.PotionItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import svenhjol.charm.module.remove_potion_glint.RemovePotionGlint;
+
+@Mixin(PotionItem.class)
+public class CheckPotionHasGlintMixin {
+
+    /**
+     * Defer check to {@link RemovePotionGlint#shouldRemoveGlint()}.
+     * If check passes, return early from this method.
+     */
+    @Inject(
+        method = "isFoil",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    public void hookHasGlint(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        if (RemovePotionGlint.shouldRemoveGlint())
+            cir.setReturnValue(false);
+    }
+}
---
+++ src/main/java/svenhjol/charm/module/remove_potion_glint/RemovePotionGlint.java
@@ -0,0 +1,19 @@
+package svenhjol.charm.module.remove_potion_glint;
+
+import svenhjol.charm.Charm;
+import svenhjol.charm.annotation.CommonModule;
+import svenhjol.charm.loader.CharmModule;
+
+@CommonModule(mod = Charm.MOD_ID, description = "Removes the potion enchantment glint so you can see what the potion color is.")
+public class RemovePotionGlint extends CharmModule {
+    private static boolean isEnabled = false;
+
+    @Override
+    public void register() {
+        isEnabled = this.isEnabled();
+    }
+
+    public static boolean shouldRemoveGlint() {
+        return isEnabled;
+    }
+}
