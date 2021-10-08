41e0704a42a7552230b0e574ff9138aaa6acea80
Sven Hjól <sven.hjol@protonmail.com>
2021-10-08 19:10:46 +0000

    Base charm enum

---
+++ src/main/java/svenhjol/charm/enums/ICharmEnum.java
@@ -0,0 +1,18 @@
+package svenhjol.charm.enums;
+
+import net.minecraft.util.StringRepresentable;
+
+import java.util.Locale;
+
+@SuppressWarnings({"NullableProblems", "rawtypes", "unused"})
+public interface ICharmEnum extends StringRepresentable {
+    @Override
+    default String getSerializedName() {
+        return ((Enum)this).name().toLowerCase(Locale.ENGLISH);
+    }
+
+    default String getCapitalizedName() {
+        String name = getSerializedName();
+        return name.substring(0, 1).toUpperCase() + name.substring(1);
+    }
+}
