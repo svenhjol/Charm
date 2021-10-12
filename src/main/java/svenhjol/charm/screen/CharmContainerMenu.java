e11430e44418b04134907ecc19bf3693de1e9171
Sven Hjól <sven.hjol@protonmail.com>
2021-10-12 03:10:05 +0000

    Add abstract charm container menu

---
+++ src/main/java/svenhjol/charm/screen/CharmContainerMenu.java
@@ -0,0 +1,58 @@
+package svenhjol.charm.screen;
+
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+
+public abstract class CharmContainerMenu extends AbstractContainerMenu {
+    protected final Container inventory;
+
+    protected CharmContainerMenu(MenuType<?> type, int id, Inventory playerInventory, Container inventory) {
+        super(type, id);
+        this.inventory = inventory;
+        inventory.startOpen(playerInventory.player);
+    }
+
+    @Override
+    public void removed(Player player) {
+        super.removed(player);
+        this.inventory.stopOpen(player);
+    }
+
+    @Override
+    public boolean stillValid(Player player) {
+        return this.inventory.stillValid(player);
+    }
+
+    @Override
+    public ItemStack quickMoveStack(Player player, int index) {
+        ItemStack stack = ItemStack.EMPTY;
+        Slot slot = this.slots.get(index);
+
+        if (slot != null && slot.hasItem()) {
+            ItemStack stackInSlot = slot.getItem();
+            stack = stackInSlot.copy();
+
+            if (index < this.inventory.getContainerSize()) {
+
+                if (!this.moveItemStackTo(stackInSlot, this.inventory.getContainerSize(), this.slots.size(), true))
+                    return ItemStack.EMPTY;
+
+            } else if (!this.moveItemStackTo(stackInSlot, 0, this.inventory.getContainerSize(), false)) {
+                return ItemStack.EMPTY;
+            }
+
+            if (stackInSlot.isEmpty()) {
+                slot.set(ItemStack.EMPTY);
+            } else {
+                slot.setChanged();
+            }
+        }
+
+        return stack;
+    }
+}
