7ee54e9a20627f78d9b015402154fa7409fa79b2
Sven Hjól <sven.hjol@protonmail.com>
2021-11-27 02:02:21 +0000

    Add EditableSigns

---
+++ src/main/java/svenhjol/charm/module/editable_signs/EditableSigns.java
@@ -0,0 +1,44 @@
+package svenhjol.charm.module.editable_signs;
+
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.SignBlockEntity;
+import net.minecraft.world.phys.BlockHitResult;
+import svenhjol.charm.Charm;
+import svenhjol.charm.annotation.CommonModule;
+import svenhjol.charm.init.CharmAdvancements;
+import svenhjol.charm.loader.CharmModule;
+
+@CommonModule(mod = Charm.MOD_ID, description = "Right-click on a sign with an empty hand to edit its text.")
+public class EditableSigns extends CharmModule {
+    public static final ResourceLocation TRIGGER_EDITED_SIGN = new ResourceLocation(Charm.MOD_ID, "edited_sign");
+
+    @Override
+    public void runWhenEnabled() {
+        UseBlockCallback.EVENT.register(this::handleUseBlock);
+    }
+
+    private InteractionResult handleUseBlock(Player player, Level level, InteractionHand hand, BlockHitResult hitResult) {
+        BlockPos pos = hitResult.getBlockPos();
+        if (!level.isClientSide && player != null && player.getItemInHand(hand).isEmpty()) {
+            if (level.getBlockEntity(pos) instanceof SignBlockEntity signBlockEntity) {
+                signBlockEntity.setEditable(true);
+                player.openTextEdit(signBlockEntity);
+                triggerEditedSign((ServerPlayer) player);
+                return InteractionResult.SUCCESS;
+            }
+        }
+
+        return InteractionResult.PASS;
+    }
+
+    public static void triggerEditedSign(ServerPlayer player) {
+        CharmAdvancements.ACTION_PERFORMED.trigger(player, TRIGGER_EDITED_SIGN);
+    }
+}
