acb3f2329e2e3b04225fa83d854c07e7b3892522
Sven Hjól <sven.hjol@protonmail.com>
2021-12-01 19:28:34 +0000

    Port loot resource loader from strange

--- src/main/java/svenhjol/charm/helper/LootHelper.java
+++ src/main/java/svenhjol/charm/helper/LootHelper.java
@@ -1,12 +1,24 @@
 package svenhjol.charm.helper;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.packs.resources.Resource;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.storage.loot.BuiltInLootTables;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.annotation.Nullable;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -57,5 +69,43 @@ public class LootHelper {
         }
 
         return lootTable;
+    }
+
+    @Nullable
+    public static Map<Integer, List<Item>> loadItemsFromResource(MinecraftServer server, ResourceLocation table) {
+        Map<Integer, List<Item>> map = new HashMap<>();
+        Gson gson = (new GsonBuilder()).setPrettyPrinting().disableHtmlEscaping().create();
+
+        Resource resource;
+        try {
+            resource = server.getResourceManager().getResource(table);
+        } catch (IOException e) {
+            return null;
+        }
+
+        InputStream inputStream = resource.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+
+        JsonObject obj = GsonHelper.fromJson(gson, reader, JsonObject.class);
+        if (obj == null) return null;
+        JsonArray pools = GsonHelper.getAsJsonArray(obj, "pools");
+
+        for (int p = 0; p < pools.size(); p++) {
+            JsonArray elements = GsonHelper.getAsJsonArray((JsonObject) pools.get(p), "entries");
+            for (int e = 0; e < elements.size(); e++) {
+                JsonObject entry = (JsonObject) elements.get(e);
+                String name = entry.get("name").getAsString();
+                ResourceLocation res = new ResourceLocation(name);
+
+                // try instantiate
+                if (Registry.ITEM.getOptional(res).isPresent()) {
+                    map.computeIfAbsent(p, a -> new LinkedList<>()).add(Registry.ITEM.get(res));
+                } else {
+                    LogHelper.debug(LootHelper.class, "Could not find item in registry: " + res);
+                }
+            }
+        }
+
+        return map;
     }
 }
