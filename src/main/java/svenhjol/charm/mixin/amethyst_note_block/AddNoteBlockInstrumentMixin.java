21e237b73ea2df119930294954eb865f7267c98b
Sven Hjól <sven.hjol@protonmail.com>
2021-12-03 02:16:41 +0000

    Add AmethystNoteBlock

---
+++ src/main/java/svenhjol/charm/mixin/amethyst_note_block/AddNoteBlockInstrumentMixin.java
@@ -0,0 +1,52 @@
+package svenhjol.charm.mixin.amethyst_note_block;
+
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@SuppressWarnings({"SameParameterValue", "unused", "target"})
+@Mixin(NoteBlockInstrument.class)
+@Unique
+public class AddNoteBlockInstrumentMixin {
+    @Shadow
+    @Final
+    @Mutable
+    private static NoteBlockInstrument[] $VALUES;
+
+    static {
+        addVariant("AMETHYST", "amethyst", SoundEvents.AMETHYST_BLOCK_PLACE);
+    }
+
+    @Invoker("<init>")
+    public static NoteBlockInstrument invokeInit(String internalName, int internalId, String noteName, SoundEvent sound) {
+        throw new AssertionError();
+    }
+
+    @Inject(
+        method = "byState",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private static void hookByState(BlockState state, CallbackInfoReturnable<NoteBlockInstrument> cir) {
+        if (state.is(Blocks.AMETHYST_BLOCK)) {
+            cir.setReturnValue(NoteBlockInstrument.valueOf("AMETHYST"));
+        }
+    }
+
+    private static void addVariant(String newName, String noteName, SoundEvent sound) {
+        List<NoteBlockInstrument> variants = new ArrayList<>(Arrays.asList(AddNoteBlockInstrumentMixin.$VALUES));
+        variants.add(invokeInit(newName, variants.get(variants.size() - 1).ordinal() + 1, noteName, sound));
+        AddNoteBlockInstrumentMixin.$VALUES = variants.toArray(new NoteBlockInstrument[0]);
+    }
+}
---
+++ src/main/java/svenhjol/charm/module/amethyst_note_block/AmethystNoteBlock.java
@@ -0,0 +1,9 @@
+package svenhjol.charm.module.amethyst_note_block;
+
+import svenhjol.charm.Charm;
+import svenhjol.charm.annotation.CommonModule;
+import svenhjol.charm.loader.CharmModule;
+
+@CommonModule(mod = Charm.MOD_ID, description = "Place a block of amethyst under a note block to play its chime sound.")
+public class AmethystNoteBlock extends CharmModule {
+}
