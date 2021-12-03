3765d7cdc28312d984d385644b3e3377879d162c
Sven Hjól <sven.hjol@protonmail.com>
2021-12-03 02:20:48 +0000

    placement not chime

--- src/main/java/svenhjol/charm/module/amethyst_note_block/AmethystNoteBlock.java
+++ src/main/java/svenhjol/charm/module/amethyst_note_block/AmethystNoteBlock.java
@@ -4,6 +4,6 @@ import svenhjol.charm.Charm;
 import svenhjol.charm.annotation.CommonModule;
 import svenhjol.charm.loader.CharmModule;
 
-@CommonModule(mod = Charm.MOD_ID, description = "Place a block of amethyst under a note block to play its chime sound.")
+@CommonModule(mod = Charm.MOD_ID, description = "Place a block of amethyst under a note block to play its placement sound.")
 public class AmethystNoteBlock extends CharmModule {
 }
