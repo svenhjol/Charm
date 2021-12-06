f22fbcefdc2c345f4cff0d2149bc0c379124c133
Sven Hjól <sven.hjol@protonmail.com>
2021-12-06 00:45:54 +0000

    Respawn anchors in the End now disabled by default

--- src/main/java/svenhjol/charm/module/respawn_anchor_in_the_end/RespawnAnchorInTheEnd.java
+++ src/main/java/svenhjol/charm/module/respawn_anchor_in_the_end/RespawnAnchorInTheEnd.java
@@ -5,7 +5,7 @@ import svenhjol.charm.Charm;
 import svenhjol.charm.annotation.CommonModule;
 import svenhjol.charm.loader.CharmModule;
 
-@CommonModule(mod = Charm.MOD_ID, description = "The repsawn anchor can be used in the End.")
+@CommonModule(mod = Charm.MOD_ID, enabledByDefault = false, description = "The repsawn anchor can be used in the End.")
 public class RespawnAnchorInTheEnd extends CharmModule {
     public static boolean canSetSpawn(Level level) {
         return Charm.LOADER.isEnabled(RespawnAnchorInTheEnd.class) && level.dimension() == Level.END;
