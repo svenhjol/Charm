2bdfd283887b954429639ba75f2f9c8ce265e28d
Sven Hjól <sven.hjol@protonmail.com>
2021-12-07 00:21:38 +0000

    Vanilla metal props

--- src/main/java/svenhjol/charm/enums/VanillaMetalMaterial.java
+++ src/main/java/svenhjol/charm/enums/VanillaMetalMaterial.java
@@ -6,9 +6,42 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public enum VanillaMetalMaterial implements IMetalMaterial {
-    COPPER,
-    GOLD,
-    NETHERITE;
+    IRON(5.0F, 6.0F, true, false),
+    COPPER(3.5F, 6.0F, false, false),
+    GOLD(3.5F, 3.5F, true, false),
+    NETHERITE(50.0F, 2500.0F, false, true);
+
+    private final float destroyTime;
+    private final float resistance;
+    private final boolean immuneToLava;
+    private final boolean hasNuggets;
+
+    VanillaMetalMaterial(float destroyTime, float resistance, boolean hasNuggets, boolean immuneToLava) {
+        this.destroyTime = destroyTime;
+        this.resistance = resistance;
+        this.hasNuggets = hasNuggets;
+        this.immuneToLava = immuneToLava;
+    }
+
+    @Override
+    public float getDestroyTime() {
+        return destroyTime;
+    }
+
+    @Override
+    public float getResistance() {
+        return resistance;
+    }
+
+    @Override
+    public boolean isFireResistant() {
+        return immuneToLava;
+    }
+
+    @Override
+    public boolean hasNuggets() {
+        return hasNuggets;
+    }
 
     public static List<IMetalMaterial> getTypes() {
         return Arrays.stream(values()).collect(Collectors.toList());
