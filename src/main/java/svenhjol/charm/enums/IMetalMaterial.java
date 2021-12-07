3685c0dfc4a559117bca5a88d1df7e3b24bd1302
Sven Hjól <sven.hjol@protonmail.com>
2021-12-07 00:19:54 +0000

    Metal methods on interface

--- src/main/java/svenhjol/charm/enums/IMetalMaterial.java
+++ src/main/java/svenhjol/charm/enums/IMetalMaterial.java
@@ -1,4 +1,11 @@
 package svenhjol.charm.enums;
 
 public interface IMetalMaterial extends ICharmEnum {
+    float getDestroyTime();
+
+    float getResistance();
+
+    boolean isFireResistant();
+
+    boolean hasNuggets();
 }
