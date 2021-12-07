7587c109406a0e59fa7dd19c14132eef1ff39397
Sven Hjól <sven.hjol@protonmail.com>
2021-12-07 20:26:50 +0000

    Helper method to get a random wood material

--- src/main/java/svenhjol/charm/enums/CharmWoodMaterial.java
+++ src/main/java/svenhjol/charm/enums/CharmWoodMaterial.java
@@ -1,5 +1,14 @@
 package svenhjol.charm.enums;
 
+import java.util.List;
+import java.util.Random;
+
 public enum CharmWoodMaterial implements IWoodMaterial {
-    AZALEA
+    AZALEA;
+
+    public static IWoodMaterial getRandomMaterial(Random random) {
+        List<IWoodMaterial> types = VanillaWoodMaterial.getTypes();
+        types.add(AZALEA);
+        return types.get(random.nextInt(types.size()));
+    }
 }
