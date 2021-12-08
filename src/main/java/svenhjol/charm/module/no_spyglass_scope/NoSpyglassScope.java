d5a5361f572a4ad9ead96ccbe1e204de4a192203
Sven Hjól <sven.hjol@protonmail.com>
2021-12-08 00:42:25 +0000

    NoSpyglassScope now enabled by default

--- src/main/java/svenhjol/charm/module/no_spyglass_scope/NoSpyglassScope.java
+++ src/main/java/svenhjol/charm/module/no_spyglass_scope/NoSpyglassScope.java
@@ -4,7 +4,7 @@ import svenhjol.charm.Charm;
 import svenhjol.charm.annotation.CommonModule;
 import svenhjol.charm.loader.CharmModule;
 
-@CommonModule(mod = Charm.MOD_ID, enabledByDefault = false, description = "Removes the dark outer overlay when zooming in with the spyglass, giving you the full view.")
+@CommonModule(mod = Charm.MOD_ID, description = "Removes the dark outer overlay when zooming in with the spyglass, giving you the full view.")
 public class NoSpyglassScope extends CharmModule {
     public static boolean shouldRemoveHud() {
         return Charm.LOADER.isEnabled(NoSpyglassScope.class);
