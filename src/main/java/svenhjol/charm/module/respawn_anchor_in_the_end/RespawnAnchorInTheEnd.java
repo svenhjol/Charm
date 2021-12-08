b8eed693e6b6c75527211f4dbd66b2b050b30c44
Sven Hjól <sven.hjol@protonmail.com>
2021-12-08 01:10:29 +0000

    Update description

--- src/main/java/svenhjol/charm/module/respawn_anchor_in_the_end/RespawnAnchorInTheEnd.java
+++ src/main/java/svenhjol/charm/module/respawn_anchor_in_the_end/RespawnAnchorInTheEnd.java
@@ -6,7 +6,7 @@ import svenhjol.charm.annotation.CommonModule;
 import svenhjol.charm.loader.CharmModule;
 
 @CommonModule(mod = Charm.MOD_ID, enabledByDefault = false, description = "The repsawn anchor can be used in the End.\n" +
-    "This is an opinionated feature that changes a core gameplay feature and so is disabled by default.")
+    "This is an opinionated feature that changes core gameplay and so is disabled by default.")
 public class RespawnAnchorInTheEnd extends CharmModule {
     public static boolean canSetSpawn(Level level) {
         return Charm.LOADER.isEnabled(RespawnAnchorInTheEnd.class) && level.dimension() == Level.END;
