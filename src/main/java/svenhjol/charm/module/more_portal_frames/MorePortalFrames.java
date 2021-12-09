1ef4bf693435a6f92522857f8b2cede1fee5568c
Sven Hjól <sven.hjol@protonmail.com>
2021-12-09 00:21:21 +0000

    Disable MorePortalFrames if IP is present

--- src/main/java/svenhjol/charm/module/more_portal_frames/MorePortalFrames.java
+++ src/main/java/svenhjol/charm/module/more_portal_frames/MorePortalFrames.java
@@ -4,11 +4,17 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import svenhjol.charm.Charm;
 import svenhjol.charm.annotation.CommonModule;
+import svenhjol.charm.helper.ModHelper;
 import svenhjol.charm.init.CharmTags;
 import svenhjol.charm.loader.CharmModule;
 
 @CommonModule(mod = Charm.MOD_ID, description = "Crying obsidian can be used in the construction of nether portal frames.")
 public class MorePortalFrames extends CharmModule {
+    @Override
+    public void register() {
+        addDependencyCheck(m -> !ModHelper.isLoaded("immersiveportals"));
+    }
+
     public static boolean isValidBlock(BlockState blockState) {
         if (!Charm.LOADER.isEnabled(MorePortalFrames.class)) {
             return blockState.is(Blocks.OBSIDIAN); // vanilla
