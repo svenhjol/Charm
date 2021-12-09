deb75dc09b57d7672c8588f3f60e457ff17dff99
Sven Hjól <sven.hjol@protonmail.com>
2021-12-09 18:54:21 +0000

    A double quote

--- src/main/java/svenhjol/charm/module/discs_stop_background_music/DiscsStopBackgroundMusic.java
+++ src/main/java/svenhjol/charm/module/discs_stop_background_music/DiscsStopBackgroundMusic.java
@@ -4,6 +4,6 @@ import svenhjol.charm.Charm;
 import svenhjol.charm.annotation.CommonModule;
 import svenhjol.charm.loader.CharmModule;
 
-@CommonModule(mod = Charm.MOD_ID, description = "\"Playing a music disc in a jukebox prevents background music from playing at the same time.")
+@CommonModule(mod = Charm.MOD_ID, description = "Playing a music disc in a jukebox prevents background music from playing at the same time.")
 public class DiscsStopBackgroundMusic extends CharmModule {
 }
