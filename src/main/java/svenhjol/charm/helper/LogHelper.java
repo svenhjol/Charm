e9f552a69f81cf3ee867fdcda8e150f6fe5f2360
Sven Hjól <sven.hjol@protonmail.com>
2021-12-25 03:19:38 +0000

    Added multiple log ID support for LogHelper

--- src/main/java/svenhjol/charm/helper/LogHelper.java
+++ src/main/java/svenhjol/charm/helper/LogHelper.java
@@ -1,31 +1,62 @@
 package svenhjol.charm.helper;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * @version 1.0.0-charm
+ * @version 4.0.0-charm
  */
 public class LogHelper {
-    public static Logger INSTANCE;
+    public static String DEFAULT_INSTANCE;
+    public static Map<String, Logger> INSTANCES = new HashMap<>();
+
+    public static Logger instance(String modId) {
+        return INSTANCES.computeIfAbsent(modId, m -> LogManager.getFormatterLogger(StringHelper.capitalize(modId)));
+    }
 
     public static void info(Class<?> source, String message, Object... args) {
-        if (INSTANCE != null)
-            INSTANCE.info(assembleMessage(source, message), args);
+        if (DEFAULT_INSTANCE != null) {
+            info(DEFAULT_INSTANCE, source, message, args);
+        }
+    }
+
+    public static void info(String modId, Class<?> source, String message, Object... args) {
+        instance(modId).info(assembleMessage(source, message), args);
     }
 
     public static void warn(Class<?> source, String message, Object... args) {
-        if (INSTANCE != null)
-            INSTANCE.warn(assembleMessage(source, message), args);
+        if (DEFAULT_INSTANCE != null) {
+            warn(DEFAULT_INSTANCE, source, message, args);
+        }
+    }
+
+    public static void warn(String modId, Class<?> source, String message, Object... args) {
+        instance(modId).warn(assembleMessage(source, message), args);
     }
 
     public static void error(Class<?> source, String message, Object... args) {
-        if (INSTANCE != null)
-            INSTANCE.error(assembleMessage(source, message), args);
+        if (DEFAULT_INSTANCE != null) {
+            error(DEFAULT_INSTANCE, source, message, args);
+        }
+    }
+
+    public static void error(String modId, Class<?> source, String message, Object... args) {
+        instance(modId).error(assembleMessage(source, message), args);
     }
 
     public static void debug(Class<?> source, String message, Object... args) {
-        if (INSTANCE != null && DebugHelper.isDebugMode())
-            INSTANCE.info(assembleMessage(source, message), args);
+        if (DEFAULT_INSTANCE != null) {
+            debug(DEFAULT_INSTANCE, source, message, args);
+        }
+    }
+
+    public static void debug(String modId, Class<?> source, String message, Object... args) {
+        if (DebugHelper.isDebugMode()) {
+            instance(modId).debug(assembleMessage(source, message), args);
+        }
     }
 
     private static String assembleMessage(Class<?> source, String message) {
--- src/main/java/svenhjol/charm/init/CharmLog.java
+++ src/main/java/svenhjol/charm/init/CharmLog.java
@@ -2,7 +2,6 @@ package svenhjol.charm.init;
 
 import com.moandjiezana.toml.Toml;
 import net.fabricmc.loader.api.FabricLoader;
-import org.apache.logging.log4j.LogManager;
 import svenhjol.charm.Charm;
 import svenhjol.charm.helper.ConfigHelper;
 import svenhjol.charm.helper.LogHelper;
@@ -10,7 +9,7 @@ import svenhjol.charm.module.core.Core;
 
 public class CharmLog {
     public static void init() {
-        LogHelper.INSTANCE = LogManager.getFormatterLogger("Charm");
+        LogHelper.DEFAULT_INSTANCE = Charm.MOD_ID;
         Toml toml = ConfigHelper.readConfig(Charm.MOD_ID);
         Core.debug = ConfigHelper.isDebugMode(toml) || FabricLoader.getInstance().isDevelopmentEnvironment();
     }
