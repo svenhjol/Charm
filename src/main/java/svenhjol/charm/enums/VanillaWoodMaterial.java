e7b3530c6e258ac98e63d25c5c5ee02aa9e27f15
Sven Hjól <sven.hjol@protonmail.com>
2021-12-25 13:21:17 +0000

    Vanilla wood enum constructor

--- src/main/java/svenhjol/charm/enums/VanillaWoodMaterial.java
+++ src/main/java/svenhjol/charm/enums/VanillaWoodMaterial.java
@@ -5,15 +5,27 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@SuppressWarnings("unused")
 public enum VanillaWoodMaterial implements IWoodMaterial {
-    OAK,
-    SPRUCE,
-    BIRCH,
-    JUNGLE,
-    ACACIA,
-    DARK_OAK,
-    CRIMSON,
-    WARPED;
+    OAK(true),
+    SPRUCE(true),
+    BIRCH(true),
+    JUNGLE(true),
+    ACACIA(true),
+    DARK_OAK(true),
+    CRIMSON(false),
+    WARPED(false);
+
+    private final boolean flammable;
+
+    VanillaWoodMaterial(boolean flammable) {
+        this.flammable = flammable;
+    }
+
+    @Override
+    public boolean isFlammable() {
+        return flammable;
+    }
 
     public static List<IWoodMaterial> getTypes() {
         return Arrays.stream(values()).collect(Collectors.toList());
@@ -22,11 +34,5 @@ public enum VanillaWoodMaterial implements IWoodMaterial {
     public static List<IWoodMaterial> getTypesWithout(IWoodMaterial... types) {
         List<IWoodMaterial> typesList = new ArrayList<>(Arrays.asList(types));
         return Arrays.stream(values()).filter(t -> !typesList.contains(t)).collect(Collectors.toList());
-    }
-
-    @Override
-    public boolean isFlammable() {
-        // TODO: non_flammable_wood is not available at reg point
-        return !this.equals(CRIMSON) && !this.equals(WARPED);
     }
 }
