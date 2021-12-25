1d56393782961ef1a00d37c0d69730249e798137
Sven Hjól <sven.hjol@protonmail.com>
2021-12-25 13:28:46 +0000

    Remove unneeded tag refs

--- src/main/java/svenhjol/charm/init/CharmTags.java
+++ src/main/java/svenhjol/charm/init/CharmTags.java
@@ -7,16 +7,11 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class CharmTags {
-    public static Tag.Named<Block> BARRELS;
-    public static Tag.Named<Block> IMMOVABLE_BY_PISTONS;
     public static Tag.Named<Block> NETHER_PORTAL_FRAMES;
     public static Tag.Named<Block> PROVIDE_ENCHANTING_POWER;
     public static Tag.Named<Item> CRAFTING_TABLES;
 
     public static void init() {
-        // TODO: restore
-//        BARRELS = BlockTagsAccessor.invokeBind("charm:barrels");
-//        IMMOVABLE_BY_PISTONS = BlockTagsAccessor.invokeBind("charm:immovable_by_pistons");
         PROVIDE_ENCHANTING_POWER = BlockTags.bind("c:provides_enchanting_power");
         CRAFTING_TABLES = ItemTags.bind("charm:crafting_tables");
         NETHER_PORTAL_FRAMES = BlockTags.bind("charm:nether_portal_frames");
