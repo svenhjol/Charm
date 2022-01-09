30f8ee5ad8a241ada93fb37950809baa12f63de2
Sven Hjól <sven.hjol@protonmail.com>
2022-01-09 05:12:59 +0000

    Reduced rate of snow accumulation to 1/750

--- src/main/java/svenhjol/charm/module/snow_accumulation/SnowAccumulation.java
+++ src/main/java/svenhjol/charm/module/snow_accumulation/SnowAccumulation.java
@@ -36,7 +36,7 @@ public class SnowAccumulation extends CharmModule {
     public static void tryPlaceSnow(ServerLevel level, int chunkX, int chunkZ) {
         if (!shouldAccumulateSnow(level)) return;
 
-        if (level.random.nextDouble() < 0.01D) {
+        if (level.random.nextInt(750) == 0) {
             BlockPos pos = level.getHeightmapPos(Heightmap.Types.MOTION_BLOCKING, level.getBlockRandomPos(chunkX, 0, chunkZ, 15));
             BlockPos belowPos = pos.below();
             BlockState belowState = level.getBlockState(belowPos);
