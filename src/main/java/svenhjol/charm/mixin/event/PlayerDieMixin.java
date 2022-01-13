c94fcc964684a16894f3ad18ca9e7aecb0721480
Sven Hjól <sven.hjol@protonmail.com>
2022-01-13 02:16:14 +0000

    Player death message sent in event

--- src/main/java/svenhjol/charm/mixin/event/PlayerDieMixin.java
+++ src/main/java/svenhjol/charm/mixin/event/PlayerDieMixin.java
@@ -1,7 +1,11 @@
 package svenhjol.charm.mixin.event;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,7 +13,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import svenhjol.charm.api.event.PlayerDieCallback;
 
 @Mixin(ServerPlayer.class)
-public class PlayerDieMixin {
+public abstract class PlayerDieMixin extends LivingEntity {
+    protected PlayerDieMixin(EntityType<? extends LivingEntity> entityType, Level level) {
+        super(entityType, level);
+    }
+
     /**
      * Fires the {@link PlayerDieCallback} event when the player is killed.
      */
@@ -18,6 +26,7 @@ public class PlayerDieMixin {
         at = @At("HEAD")
     )
     private void hookOnDeath(DamageSource source, CallbackInfo ci) {
-        PlayerDieCallback.EVENT.invoker().interact((ServerPlayer)(Object)this, source);
+        var deathMessage = getCombatTracker().getDeathMessage();
+        PlayerDieCallback.EVENT.invoker().interact((ServerPlayer)(Object)this, deathMessage);
     }
 }
