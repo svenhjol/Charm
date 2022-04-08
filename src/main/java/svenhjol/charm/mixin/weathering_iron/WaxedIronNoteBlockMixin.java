a0835bcf1b7e3f3a8f66bca11742e42381a702cc
Sven Hjól <sven.hjol@protonmail.com>
2022-04-08 13:30:36 +0000

    Waxed iron noteblock mixin

---
+++ src/main/java/svenhjol/charm/mixin/weathering_iron/WaxedIronNoteBlockMixin.java
@@ -0,0 +1,27 @@
+package svenhjol.charm.mixin.weathering_iron;
+
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import svenhjol.charm.module.weathering_iron.WeatheringIron;
+
+/**
+ * Waxed iron should make the same sound as a normal iron block.
+ */
+@Mixin(NoteBlockInstrument.class)
+public class WaxedIronNoteBlockMixin {
+
+    @Inject(
+        method = "byState",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private static void hookByState(BlockState state, CallbackInfoReturnable<NoteBlockInstrument> cir) {
+        if (state.is(WeatheringIron.WAXED_IRON)) {
+            cir.setReturnValue(NoteBlockInstrument.IRON_XYLOPHONE);
+        }
+    }
+}
