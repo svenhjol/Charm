76701cd029fa7fcb146d1138be6ec6f53645d0b1
Sven Hjól <sven.hjol@protonmail.com>
2022-04-18 18:35:00 +0100

    Remove crimson and warped boat items and refs

--- src/main/java/svenhjol/charm/module/extra_boats/ExtraBoats.java
+++ src/main/java/svenhjol/charm/module/extra_boats/ExtraBoats.java
@@ -1,20 +1,16 @@
 package svenhjol.charm.module.extra_boats;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.vehicle.Boat;
 import svenhjol.charm.Charm;
 import svenhjol.charm.annotation.CommonModule;
-import svenhjol.charm.enums.VanillaWoodMaterial;
-import svenhjol.charm.helper.RecipeHelper;
 import svenhjol.charm.loader.CharmModule;
 
-@CommonModule(mod = Charm.MOD_ID, priority = 5, description = "Adds crimson and warped wood boats.")
+@CommonModule(mod = Charm.MOD_ID, priority = 5, alwaysEnabled = true, description = "Allows Charm to register extra boat types.")
 public class ExtraBoats extends CharmModule {
-    public static final String CRIMSON = VanillaWoodMaterial.CRIMSON.getSerializedName();
-    public static final String WARPED = VanillaWoodMaterial.WARPED.getSerializedName();
+    public static Boat.Type AZALEA;
 
     @Override
-    public void runWhenDisabled() {
-        RecipeHelper.removeRecipe(new ResourceLocation(Charm.MOD_ID, "extra_boats/crimson_boat_woodcutting"));
-        RecipeHelper.removeRecipe(new ResourceLocation(Charm.MOD_ID, "extra_boats/warped_boat_woodcutting"));
+    public void register() {
+        AZALEA = Boat.Type.byName("AZALEA");
     }
 }
