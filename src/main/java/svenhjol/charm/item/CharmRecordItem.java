f2dc6ef577798d3a2f17e5c237036c06b6ea7003
Sven Hjól <sven.hjol@protonmail.com>
2022-05-08 14:39:46 +0000

    Register record item

--- src/main/java/svenhjol/charm/item/CharmRecordItem.java
+++ src/main/java/svenhjol/charm/item/CharmRecordItem.java
@@ -7,9 +7,10 @@ import svenhjol.charm.loader.CharmModule;
 public class CharmRecordItem extends RecordItem implements ICharmItem {
     protected CharmModule module;
 
-    protected CharmRecordItem(CharmModule module, int redstoneStrength, SoundEvent soundEvent, Properties properties) {
+    protected CharmRecordItem(CharmModule module, String name, int redstoneStrength, SoundEvent soundEvent, Properties properties) {
         super(redstoneStrength, soundEvent, properties);
         this.module = module;
+        this.register(module, name);
     }
 
     @Override
