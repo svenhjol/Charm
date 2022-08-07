2a087c2c4d02bec2bed5e68553817e9ffa734395
Sven Hjól <sven.hjol@protonmail.com>
2022-08-07 00:08:41 +0000

    Testing fix for #800 missing chest boat contents

--- src/main/java/svenhjol/charm/mixin/variant_chests/VariantChestBoatMixin.java
+++ src/main/java/svenhjol/charm/mixin/variant_chests/VariantChestBoatMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.entity.vehicle.ChestBoat;
+import net.minecraft.world.entity.vehicle.ContainerEntity;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import svenhjol.charm.module.variant_chests.IVariantChestBoat;
@@ -15,7 +16,7 @@ import svenhjol.charm.module.variant_chests.IVariantChestBoat;
  * Adds an extra VARIANT_CHEST property to all ChestBoat entities.
  */
 @Mixin(ChestBoat.class)
-public class VariantChestBoatMixin extends Boat implements IVariantChestBoat {
+public abstract class VariantChestBoatMixin extends Boat implements IVariantChestBoat, ContainerEntity {
     private static final String VARIANT_CHEST_TAG = "variant_chest";
     private static final EntityDataAccessor<String> DATA_ID_VARIANT_CHEST = SynchedEntityData.defineId(Boat.class, EntityDataSerializers.STRING);
 
@@ -32,12 +33,14 @@ public class VariantChestBoatMixin extends Boat implements IVariantChestBoat {
     @Override
     public void readAdditionalSaveData(CompoundTag compoundTag) {
         super.readAdditionalSaveData(compoundTag);
+        this.readChestVehicleSaveData(compoundTag);
         setVariantChest(compoundTag.getString(VARIANT_CHEST_TAG));
     }
 
     @Override
     public void addAdditionalSaveData(CompoundTag compoundTag) {
         super.addAdditionalSaveData(compoundTag);
+        this.addChestVehicleSaveData(compoundTag);
         compoundTag.putString(VARIANT_CHEST_TAG, getVariantChest());
     }
 
