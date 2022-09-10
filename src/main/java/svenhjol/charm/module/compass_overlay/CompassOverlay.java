a31a008febede53d9986533797c70a61a230d94d
Sven Hjól <sven.hjol@protonmail.com>
2022-09-10 16:16:44 +0000

    Add biome to compass overlay

--- src/main/java/svenhjol/charm/module/compass_overlay/CompassOverlay.java
+++ src/main/java/svenhjol/charm/module/compass_overlay/CompassOverlay.java
@@ -13,6 +13,9 @@ public class CompassOverlay extends CharmModule {
     @Config(name = "Co-ordinates", description = "If true, shows the player's XYZ coordinates.")
     public static boolean showCoords = true;
 
+    @Config(name = "Biome", description = "If true, shows the player's biome.")
+    public static boolean showBiome = true;
+
     @Config(name = "Only show X and Z", description = "If true, only show the player's X and Z coordinates (not their height/depth).")
     public static boolean onlyXZ = false;
 
