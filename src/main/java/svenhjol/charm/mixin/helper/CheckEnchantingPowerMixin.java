c1e58816f251aa91c787bd36f82a7ef3fdb7a991
Sven Hjól <sven.hjol@protonmail.com>
2022-09-22 16:48:31 +0000

    Redundant ref

--- src/main/java/svenhjol/charm/mixin/helper/CheckEnchantingPowerMixin.java
+++ src/main/java/svenhjol/charm/mixin/helper/CheckEnchantingPowerMixin.java
@@ -7,7 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import svenhjol.charm.annotation.CharmMixin;
 import svenhjol.charm.init.CharmTags;
 
 @Mixin(EnchantmentTableBlock.class)
