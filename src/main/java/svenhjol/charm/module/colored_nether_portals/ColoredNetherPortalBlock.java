a2324321a5b119def929499cec9fde80f3bd60b7
Sven Hjól <sven.hjol@protonmail.com>
2022-09-26 19:31:52 +0000

    Can right-click on a portal with dye to change its color. Portal overlay now shows the correct color when travelling.

--- src/main/java/svenhjol/charm/module/colored_nether_portals/ColoredNetherPortalBlock.java
+++ src/main/java/svenhjol/charm/module/colored_nether_portals/ColoredNetherPortalBlock.java
@@ -1,16 +1,20 @@
 package svenhjol.charm.module.colored_nether_portals;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.DyeItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.NetherPortalBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import svenhjol.charm.block.ICharmBlock;
 import svenhjol.charm.init.CharmParticles;
 import svenhjol.charm.loader.CharmModule;
@@ -36,6 +40,27 @@ public class ColoredNetherPortalBlock extends NetherPortalBlock implements IChar
     @Override
     public void createBlockItem(ResourceLocation id, Item.Properties properties) {
         // don't
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+        var held = player.getItemInHand(hand);
+        var hitPos = hitResult.getBlockPos();
+        var blockState = level.getBlockState(hitPos);
+        var block = blockState.getBlock();
+
+        if (block instanceof NetherPortalBlock && held.getItem() instanceof DyeItem dye) {
+            var color = dye.getDyeColor();
+            ColoredNetherPortals.replacePortal(level, hitPos, blockState, color);
+
+            if (!player.getAbilities().instabuild) {
+                held.shrink(1);
+            }
+
+            return InteractionResult.sidedSuccess(level.isClientSide);
+        }
+
+        return InteractionResult.PASS;
     }
 
     public DyeColor getColor() {
