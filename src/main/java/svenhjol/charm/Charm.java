10b612d69c4be328b7949ae9307b79ed78557507
Sven Hjól <sven.hjol@protonmail.com>
2022-09-29 18:35:12 +0000

    Restore old logging init

--- src/main/java/svenhjol/charm/Charm.java
+++ src/main/java/svenhjol/charm/Charm.java
@@ -3,6 +3,7 @@ package svenhjol.charm;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import svenhjol.charm.helper.ConfigHelper;
+import svenhjol.charm.helper.LogHelper;
 import svenhjol.charm.init.*;
 import svenhjol.charm.loader.CharmModule;
 import svenhjol.charm.loader.CommonLoader;
@@ -11,7 +12,6 @@ import svenhjol.charm.module.core.Core;
 @SuppressWarnings("unused")
 public class Charm implements ModInitializer {
     public static final String MOD_ID = "charm";
-    public static final LogWrapper LOG = new LogWrapper(MOD_ID);
     public static final CommonLoader<CharmModule> LOADER = new CommonLoader<>(MOD_ID, "svenhjol.charm.module");
 
     private static boolean hasStartedCharm = false;
@@ -24,13 +24,13 @@ public class Charm implements ModInitializer {
     public static void init() {
         if (hasStartedCharm) return;
 
+        setupDebugMode();
+
         CharmHacks.init();
         CharmResources.init();
         CharmParticles.init();
         CharmTags.init();
         CharmAdvancements.init();
-
-        setupDebugMode();
 
         LOADER.init();
 
@@ -38,6 +38,7 @@ public class Charm implements ModInitializer {
     }
 
     private static void setupDebugMode() {
+        LogHelper.DEFAULT_INSTANCE = Charm.MOD_ID;
         var toml = ConfigHelper.readConfig(Charm.MOD_ID);
         Core.debug = ConfigHelper.isDebugMode(toml) || FabricLoader.getInstance().isDevelopmentEnvironment();
     }
