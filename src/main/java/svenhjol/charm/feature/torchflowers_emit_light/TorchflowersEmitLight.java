624ad7a3e5c26b564d2783c4dbb0367accb7e611
Sven H <sven.hjol@protonmail.com>
2023-08-26 13:30:49 +0000

    Added torchflowers emit light feature

---
+++ src/main/java/svenhjol/charm/feature/torchflowers_emit_light/TorchflowersEmitLight.java
@@ -0,0 +1,9 @@
+package svenhjol.charm.feature.torchflowers_emit_light;
+
+import svenhjol.charm.CharmClient;
+import svenhjol.charm_core.annotation.ClientFeature;
+import svenhjol.charm_core.base.CharmFeature;
+
+@ClientFeature(mod = CharmClient.MOD_ID)
+public class TorchflowersEmitLight extends CharmFeature {
+}
---
+++ src/main/java/svenhjol/charm/mixin/torchflowers_emit_light/BlockBehaviourPropertiesMixin.java
@@ -0,0 +1,26 @@
+package svenhjol.charm.mixin.torchflowers_emit_light;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BlockBehaviour.BlockStateBase.class)
+public abstract class BlockBehaviourPropertiesMixin {
+    @Shadow public abstract boolean is(Block block);
+
+    @Inject(
+        method = "getLightEmission",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void hookGetLightEmission(CallbackInfoReturnable<Integer> cir) {
+        if (is(Blocks.TORCHFLOWER)) {
+            cir.setReturnValue(8);
+        }
+    }
+}
