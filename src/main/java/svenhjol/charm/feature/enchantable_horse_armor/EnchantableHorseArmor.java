993a53e18ca4ccde44090cf4752dd4c378ea5c91
Sven H <sven.hjol@protonmail.com>
2023-08-28 15:01:53 +0000

    Add enchantable horse armor

---
+++ src/main/java/svenhjol/charm/feature/enchantable_horse_armor/EnchantableHorseArmor.java
@@ -0,0 +1,58 @@
+package svenhjol.charm.feature.enchantable_horse_armor;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.enchantment.Enchantment;
+import svenhjol.charm.Charm;
+import svenhjol.charm_api.CharmApi;
+import svenhjol.charm_api.iface.IProvidesHorseArmorEnchantments;
+import svenhjol.charm_core.annotation.Configurable;
+import svenhjol.charm_core.annotation.Feature;
+import svenhjol.charm_core.base.CharmFeature;
+import svenhjol.charm_core.helper.ApiHelper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Feature(mod = Charm.MOD_ID, description = "Horse armor can be enchanted.")
+public class EnchantableHorseArmor extends CharmFeature implements IProvidesHorseArmorEnchantments {
+    private static final List<Enchantment> VALIDATED_ENCHANTMENTS = new ArrayList<>();
+    
+    @Configurable(name = "Enchantments", description = "Enchantments that will function on horse armor.")
+    public static List<String> enchantments = Arrays.asList(
+        "minecraft:protection",
+        "minecraft:fire_protection",
+        "minecraft:blast_protection",
+        "minecraft:projectile_protection",
+        "minecraft:thorns",
+        "minecraft:frost_walker",
+        "minecraft:feather_falling",
+        "minecraft:respiration"
+    );
+
+    @Override
+    public void register() {
+        CharmApi.registerProvider(this);
+    }
+
+    @Override
+    public List<Enchantment> getEnchantments() {
+        if (VALIDATED_ENCHANTMENTS.isEmpty()) {
+            // Validate configured enchantments and cache them.
+            for (var enchantment : enchantments) {
+                BuiltInRegistries.ENCHANTMENT.getOptional(new ResourceLocation(enchantment)).ifPresent(e -> {
+                    Charm.LOG.debug(getClass(), "Adding enchantment " + enchantment);
+                    VALIDATED_ENCHANTMENTS.add(e);
+                });
+            }
+        }
+
+        return VALIDATED_ENCHANTMENTS;
+    }
+
+    public static List<Enchantment> getAllEnchantments() {
+        return ApiHelper.getProviderData(IProvidesHorseArmorEnchantments.class,
+                provider -> provider.getEnchantments().stream());
+    }
+}
---
+++ src/main/java/svenhjol/charm/mixin/enchantable_horse_armor/EnchantmentMixin.java
@@ -0,0 +1,28 @@
+package svenhjol.charm.mixin.enchantable_horse_armor;
+
+import net.minecraft.world.item.HorseArmorItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import svenhjol.charm.feature.enchantable_horse_armor.EnchantableHorseArmor;
+
+@SuppressWarnings("ConstantConditions")
+@Mixin(Enchantment.class)
+public class EnchantmentMixin {
+    @Inject(
+        method = "canEnchant",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void hookCanEnchant(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        var enchantment = (Enchantment) (Object) this;
+        
+        if (stack.getItem() instanceof HorseArmorItem
+            && EnchantableHorseArmor.getAllEnchantments().contains(enchantment)) {
+            cir.setReturnValue(true);
+        }
+    }
+}
