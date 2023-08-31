8abf9668f414b4a9339483f4a51f315776a5a1e5
Sven H <sven.hjol@protonmail.com>
2023-08-31 08:27:00 +0000

    Add open both doors

---
+++ src/main/java/svenhjol/charm/feature/open_both_doors/OpenBothDoors.java
@@ -0,0 +1,42 @@
+package svenhjol.charm.feature.open_both_doors;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.DoorBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.DoorHingeSide;
+import svenhjol.charm.Charm;
+import svenhjol.charm_core.annotation.Feature;
+import svenhjol.charm_core.base.CharmFeature;
+
+@Feature(mod = Charm.MOD_ID, description = "Automatically opens double doors.")
+public class OpenBothDoors extends CharmFeature {
+    public static void tryOpenNeighbour(Level level, BlockState state, BlockPos pos, boolean isClosed) {
+        var facing = state.getValue(DoorBlock.FACING);
+        var hinge = state.getValue(DoorBlock.HINGE);
+
+        var neighborPos = switch (facing) {
+            case NORTH -> hinge == DoorHingeSide.LEFT ? pos.east() : pos.west();
+            case SOUTH -> hinge == DoorHingeSide.LEFT ? pos.west() : pos.east();
+            case WEST -> hinge == DoorHingeSide.LEFT ? pos.north() : pos.south();
+            case EAST -> hinge == DoorHingeSide.LEFT ? pos.south() : pos.north();
+            default -> null;
+        };
+
+        if (neighborPos == null) return;
+
+        var neighborState = level.getBlockState(neighborPos);
+        if (!(neighborState.getBlock() instanceof DoorBlock neighbor)) return;
+
+        var neighborPowered = neighborState.getValue(DoorBlock.POWERED);
+        if (neighborPowered) return;
+
+        var neighborFacing = neighborState.getValue(DoorBlock.FACING);
+        if (neighborFacing != facing) return;
+
+        var neighborHinge = neighborState.getValue(DoorBlock.HINGE);
+        if (neighborHinge == hinge) return;
+
+        neighbor.setOpen(null, level, neighborState, neighborPos, isClosed);
+    }
+}
---
+++ src/main/java/svenhjol/charm/mixin/open_both_doors/DoorBlockMixin.java
@@ -0,0 +1,52 @@
+package svenhjol.charm.mixin.open_both_doors;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DoorBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import svenhjol.charm.feature.open_both_doors.OpenBothDoors;
+
+@Mixin(DoorBlock.class)
+public class DoorBlockMixin {
+    @Inject(
+        method = "use",
+        at = @At("RETURN")
+    )
+    private void hookUse(BlockState state, Level level, BlockPos pos, Player player, InteractionHand interactionHand, BlockHitResult hit, CallbackInfoReturnable<InteractionResult> cir) {
+        if (cir.getReturnValue() == InteractionResult.CONSUME || cir.getReturnValue() == InteractionResult.SUCCESS) {
+            OpenBothDoors.tryOpenNeighbour(level, state, pos, state.getValue(DoorBlock.OPEN));
+        }
+    }
+
+    @Inject(
+        method = "setOpen",
+        at = @At("RETURN")
+    )
+    private void hookSetOpen(Entity entity, Level level, BlockState state, BlockPos pos, boolean bl, CallbackInfo ci) {
+        if (entity != null) {
+            OpenBothDoors.tryOpenNeighbour(level, state, pos, bl);
+        }
+    }
+
+    @Inject(
+        method = "neighborChanged",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/level/Level;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z"
+        )
+    )
+    private void hookNeighborChanged(BlockState state, Level level, BlockPos pos, Block block, BlockPos pos2, boolean bl, CallbackInfo ci) {
+        OpenBothDoors.tryOpenNeighbour(level, state, pos, !state.getValue(DoorBlock.OPEN));
+    }
+}
