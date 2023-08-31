588a2475654cf9cedfd3770e5c90024759b86cdc
Sven H <sven.hjol@protonmail.com>
2023-08-31 10:42:47 +0000

    Add villagers follow emerald blocks

---
+++ src/main/java/svenhjol/charm/feature/villagers_follow_emerald_blocks/VillagersFollowEmeraldBlocks.java
@@ -0,0 +1,63 @@
+package svenhjol.charm.feature.villagers_follow_emerald_blocks;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.ai.goal.TemptGoal;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.phys.AABB;
+import svenhjol.charm.Charm;
+import svenhjol.charm_core.annotation.Feature;
+import svenhjol.charm_api.event.EntityJoinEvent;
+import svenhjol.charm_api.event.PlayerTickEvent;
+import svenhjol.charm_core.base.CharmFeature;
+import svenhjol.charm.mixin.accessor.MobAccessor;
+
+import java.util.List;
+
+@SuppressWarnings("UnusedReturnValue")
+@Feature(mod = Charm.MOD_ID, description = "Villagers are attracted when the player holds a block of emeralds.")
+public class VillagersFollowEmeraldBlocks extends CharmFeature {
+    public static final ResourceLocation TRIGGER_LURED_VILLAGER = new ResourceLocation(Charm.MOD_ID, "lured_villager");
+
+    @Override
+    public void runWhenEnabled() {
+        EntityJoinEvent.INSTANCE.handle(this::handleEntityJoin);
+        PlayerTickEvent.INSTANCE.handle(this::handlePlayerTick);
+    }
+
+    private InteractionResult handleEntityJoin(Entity entity, Level level) {
+        if (entity instanceof Villager villager) {
+            var ingredient = Ingredient.of(Blocks.EMERALD_BLOCK);
+            var goalSelector = ((MobAccessor)villager).getGoalSelector();
+
+            if (goalSelector.getAvailableGoals().stream().noneMatch(g -> g.getGoal() instanceof TemptGoal)) {
+                goalSelector.addGoal(3, new TemptGoal(villager, 0.6, ingredient, false));
+            }
+        }
+
+        return InteractionResult.PASS;
+    }
+
+    private void handlePlayerTick(Player player) {
+        if (!player.level().isClientSide
+            && player.level().getGameTime() % 40 == 0
+            && player.getMainHandItem().getItem() == Items.EMERALD_BLOCK
+        ) {
+            List<Villager> villagers = player.level().getEntitiesOfClass(Villager.class, new AABB(player.blockPosition()).inflate(8.0D));
+            if (!villagers.isEmpty()) {
+                triggerLuredVillager((ServerPlayer) player);
+            }
+        }
+    }
+
+    public static void triggerLuredVillager(ServerPlayer player) {
+        // TODO: advancement
+    }
+}
---
+++ src/main/java/svenhjol/charm/mixin/accessor/MobAccessor.java
@@ -0,0 +1,12 @@
+package svenhjol.charm.mixin.accessor;
+
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.goal.GoalSelector;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(Mob.class)
+public interface MobAccessor {
+    @Accessor("goalSelector")
+    GoalSelector getGoalSelector();
+}
