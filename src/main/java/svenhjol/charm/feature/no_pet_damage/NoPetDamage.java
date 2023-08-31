14bd96956b9f7b6e7b4cafbd7a0be5addfe9a828
Sven H <sven.hjol@protonmail.com>
2023-08-31 08:14:38 +0000

    Add no pet damage

---
+++ src/main/java/svenhjol/charm/feature/no_pet_damage/NoPetDamage.java
@@ -0,0 +1,56 @@
+package svenhjol.charm.feature.no_pet_damage;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+import svenhjol.charm.Charm;
+import svenhjol.charm_core.annotation.Feature;
+import svenhjol.charm_api.event.EntityAttackEvent;
+import svenhjol.charm_api.event.EntityHurtEvent;
+import svenhjol.charm_core.base.CharmFeature;
+
+@Feature(mod = Charm.MOD_ID, description = "Tamed animals do not take direct damage from players.")
+public class NoPetDamage extends CharmFeature {
+    @Override
+    public void runWhenEnabled() {
+        EntityAttackEvent.INSTANCE.handle(this::handleEntityAttack);
+        EntityHurtEvent.INSTANCE.handle(this::handleEntityHurt);
+    }
+
+    private InteractionResult handleEntityHurt(LivingEntity entity, DamageSource damageSource, float damage) {
+        if (!(entity instanceof Player)) {
+            var attacker = damageSource.getEntity();
+            var source = damageSource.getDirectEntity();
+
+            Player player = null;
+
+            if (source instanceof Player) player = (Player) source;
+            if (attacker instanceof Player) player = (Player) attacker;
+
+            if (player != null && !player.getAbilities().instabuild) {
+                if (entity instanceof TamableAnimal && ((TamableAnimal) entity).isTame()) {
+                    return InteractionResult.FAIL; // the positive outcome!
+                }
+            }
+        }
+
+        return InteractionResult.PASS;
+    }
+
+    private InteractionResult handleEntityAttack(Player player, Level level, InteractionHand hand, Entity target, EntityHitResult hitResult) {
+        if (target instanceof TamableAnimal
+            && ((TamableAnimal)target).isTame()
+            && !player.isCreative()
+        ) {
+            return InteractionResult.FAIL; // the positive outcome!
+        }
+
+        return InteractionResult.PASS;
+    }
+}
