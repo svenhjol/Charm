1cbbabd3f1a5aadd8bdf7942ee43c3b8a693b38f
Sven H <sven.hjol@protonmail.com>
2023-09-04 22:42:09 +0000

    No dupe

--- src/main/java/svenhjol/charmony/api/iface/IVariantBarrelProvider.java
+++
@@ -1,7 +0,0 @@
-package svenhjol.charmony.api.iface;
-
-import java.util.List;
-
-public interface IVariantBarrelProvider {
-    List<IVariantMaterial> getVariantBarrels();
-}
--- src/main/java/svenhjol/charmony/api/iface/IVariantChestProvider.java
+++
@@ -1,7 +0,0 @@
-package svenhjol.charmony.api.iface;
-
-import java.util.List;
-
-public interface IVariantChestProvider {
-    List<IVariantMaterial> getVariantChests();
-}
