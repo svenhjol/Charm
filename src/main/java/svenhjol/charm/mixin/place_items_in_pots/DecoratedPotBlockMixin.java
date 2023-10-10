b7f832cec40d0168877bd635b977f4872dd04e34
Sven H <sven.hjol@protonmail.com>
2023-10-10 21:42:51 +0000

    Fix pistons breaking pots (https://trello.com/c/clWAC2jD)

--- src/main/java/svenhjol/charm/mixin/place_items_in_pots/DecoratedPotBlockMixin.java
+++ src/main/java/svenhjol/charm/mixin/place_items_in_pots/DecoratedPotBlockMixin.java
@@ -1,26 +1,46 @@
 package svenhjol.charm.mixin.place_items_in_pots;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.DecoratedPotBlock;
 import net.minecraft.world.level.block.entity.DecoratedPotBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import svenhjol.charm.feature.place_items_in_pots.PlaceItemsInPots;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mixin(DecoratedPotBlock.class)
-public class DecoratedPotBlockMixin {
+public abstract class DecoratedPotBlockMixin extends BaseEntityBlock {
+    protected DecoratedPotBlockMixin(Properties properties) {
+        super(properties);
+    }
+
+    /**
+     * Add the blockentity item to the list of drops calculated from the loot table.
+     * Vanilla method super.getDrops() is deprecated (?!) hence the suppression.
+     */
+    @SuppressWarnings("deprecation")
     @Inject(
-        method = "playerWillDestroy",
-        at = @At("TAIL")
+        method = "getDrops",
+        at = @At("RETURN"),
+        cancellable = true
     )
-    private void hookPlayerWillDestroy(Level level, BlockPos pos, BlockState state, Player player, CallbackInfo ci) {
-        if (level.getBlockEntity(pos) instanceof DecoratedPotBlockEntity pot && !level.isClientSide) {
-            PlaceItemsInPots.dropItemStack(level, pos, pot);
+    private void hookGetDrops(BlockState state, LootParams.Builder builder, CallbackInfoReturnable<List<ItemStack>> cir) {
+        var blockEntity = builder.getOptionalParameter(LootContextParams.BLOCK_ENTITY);
+        if (blockEntity instanceof DecoratedPotBlockEntity pot) {
+            var opt = PlaceItemsInPots.getItemStack(pot);
+            if (opt.isPresent()) {
+                var drops = new ArrayList<>(super.getDrops(state, builder));
+                drops.add(opt.get());
+                cir.setReturnValue(drops);
+            }
         }
     }
 }
