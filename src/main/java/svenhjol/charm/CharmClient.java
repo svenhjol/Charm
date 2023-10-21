c3794553601f8145e8e64177a2758ce93e69b98f
Sven H <sven.hjol@protonmail.com>
2023-10-21 00:44:47 +0000

    Charmony 6.5.0 now using manual classloading

--- src/main/java/svenhjol/charm/CharmClient.java
+++ src/main/java/svenhjol/charm/CharmClient.java
@@ -1,6 +1,41 @@
 package svenhjol.charm;
 
+import svenhjol.charm.feature.atlases.AtlasesClient;
+import svenhjol.charm.feature.azalea_wood.AzaleaWoodClient;
+import svenhjol.charm.feature.bat_buckets.BatBucketsClient;
+import svenhjol.charm.feature.chairs.ChairsClient;
+import svenhjol.charm.feature.clear_item_frames.ClearItemFramesClient;
+import svenhjol.charm.feature.colored_glint_smithing_templates.ColoredGlintSmithingTemplatesClient;
+import svenhjol.charm.feature.compass_overlay.CompassOverlay;
+import svenhjol.charm.feature.copper_pistons.CopperPistonsClient;
+import svenhjol.charm.feature.coral_sea_lanterns.CoralSeaLanternsClient;
+import svenhjol.charm.feature.coral_squids.CoralSquidsClient;
+import svenhjol.charm.feature.discs_stop_background_music.DiscsStopBackgroundMusic;
+import svenhjol.charm.feature.endermite_powder.EndermitePowderClient;
+import svenhjol.charm.feature.extra_tooltips.ExtraTooltips;
+import svenhjol.charm.feature.extractable_enchantments.ExtractableEnchantmentsClient;
+import svenhjol.charm.feature.gentle_potion_particles.GentlePotionParticles;
+import svenhjol.charm.feature.hover_sorting.HoverSortingClient;
+import svenhjol.charm.feature.inventory_tidying.InventoryTidyingClient;
+import svenhjol.charm.feature.kilns.KilnsClient;
+import svenhjol.charm.feature.mooblooms.MoobloomsClient;
+import svenhjol.charm.feature.no_chat_unverified_message.NoChatUnverifiedMessage;
+import svenhjol.charm.feature.no_experimental_screen.NoExperimentalScreen;
+import svenhjol.charm.feature.no_spyglass_scope.NoSpyglassScope;
+import svenhjol.charm.feature.player_pressure_plates.PlayerPressurePlatesClient;
+import svenhjol.charm.feature.proximity_workstations.ProximityWorkstationsClient;
+import svenhjol.charm.feature.redstone_sand.RedstoneSandClient;
+import svenhjol.charm.feature.show_repair_cost.ShowRepairCost;
+import svenhjol.charm.feature.smooth_glowstone.SmoothGlowstoneClient;
+import svenhjol.charm.feature.storage_blocks.StorageBlocksClient;
+import svenhjol.charm.feature.totem_of_preserving.TotemOfPreservingClient;
+import svenhjol.charm.feature.variant_mob_textures.VariantMobTextures;
+import svenhjol.charm.feature.variant_wood.VariantWoodClient;
+import svenhjol.charm.feature.woodcutters.WoodcuttersClient;
+import svenhjol.charmony.base.CharmonyFeature;
 import svenhjol.charmony.base.DefaultClientMod;
+
+import java.util.List;
 
 public class CharmClient extends DefaultClientMod {
     public static final String MOD_ID = "charm";
@@ -16,5 +51,43 @@ public class CharmClient extends DefaultClientMod {
     @Override
     public String modId() {
         return MOD_ID;
+    }
+
+    @Override
+    public List<Class<? extends CharmonyFeature>> features() {
+        return List.of(
+            AtlasesClient.class,
+            AzaleaWoodClient.class,
+            BatBucketsClient.class,
+            ChairsClient.class,
+            ClearItemFramesClient.class,
+            ColoredGlintSmithingTemplatesClient.class,
+            CompassOverlay.class,
+            CopperPistonsClient.class,
+            CoralSeaLanternsClient.class,
+            CoralSquidsClient.class,
+            DiscsStopBackgroundMusic.class,
+            EndermitePowderClient.class,
+            ExtraTooltips.class,
+            ExtractableEnchantmentsClient.class,
+            GentlePotionParticles.class,
+            HoverSortingClient.class,
+            InventoryTidyingClient.class,
+            KilnsClient.class,
+            MoobloomsClient.class,
+            NoChatUnverifiedMessage.class,
+            NoExperimentalScreen.class,
+            NoSpyglassScope.class,
+            PlayerPressurePlatesClient.class,
+            ProximityWorkstationsClient.class,
+            RedstoneSandClient.class,
+            ShowRepairCost.class,
+            SmoothGlowstoneClient.class,
+            StorageBlocksClient.class,
+            TotemOfPreservingClient.class,
+            VariantMobTextures.class,
+            VariantWoodClient.class,
+            WoodcuttersClient.class
+        );
     }
 }
