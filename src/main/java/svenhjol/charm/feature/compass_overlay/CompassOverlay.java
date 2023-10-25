2de620830d0dc622772e9d3a191ca331805ba481
Sven H <sven.hjol@protonmail.com>
2023-10-25 19:10:43 +0000

    Added "always show" and "compact view" to compass overlay

--- src/main/java/svenhjol/charm/feature/compass_overlay/CompassOverlay.java
+++ src/main/java/svenhjol/charm/feature/compass_overlay/CompassOverlay.java
@@ -11,20 +11,54 @@ import svenhjol.charmony.client.ClientFeature;
 import svenhjol.charmony_api.event.HudRenderEvent;
 
 public class CompassOverlay extends ClientFeature {
-    @Configurable(name = "Facing", description = "If true, shows the cardinal direction that the player is facing.")
+    @Configurable(
+        name = "Facing",
+        description = "If true, shows the cardinal direction that the player is facing.",
+        requireRestart = false
+    )
     public static boolean showFacing = true;
 
-    @Configurable(name = "Co-ordinates", description = "If true, shows the player's XYZ coordinates.")
+    @Configurable(
+        name = "Co-ordinates",
+        description = "If true, shows the player's XYZ coordinates.",
+        requireRestart = false
+    )
     public static boolean showCoords = true;
 
-    @Configurable(name = "Biome", description = "If true, shows the player's biome.")
+    @Configurable(
+        name = "Biome",
+        description = "If true, shows the player's biome.",
+        requireRestart = false
+    )
     public static boolean showBiome = true;
 
-    @Configurable(name = "Only show X and Z", description = "If true, only show the player's X and Z coordinates (not their height/depth).")
+    @Configurable(
+        name = "Only show X and Z",
+        description = "If true, only show the player's X and Z coordinates (not their height/depth).",
+        requireRestart = false
+    )
     public static boolean onlyXZ = false;
 
-    @Configurable(name = "Show when sneaking", description = "If true, only show the compass overlay if the player is sneaking.")
+    @Configurable(
+        name = "Show when sneaking",
+        description = "If true, only show the compass overlay if the player is sneaking.",
+        requireRestart = false
+    )
     public static boolean onlyShowWhenSneaking = false;
+
+    @Configurable(
+        name = "Always show",
+        description = "If true, the overlay will always be shown even if the player is not holding a compass.",
+        requireRestart = false
+    )
+    public static boolean alwaysShow = false;
+
+    @Configurable(
+        name = "Compact view",
+        description = "If true, the overlay will be shown in smaller text  and in top left of the screen.",
+        requireRestart = false
+    )
+    public static boolean compactView = false;
 
     @Override
     public String description() {
@@ -40,18 +74,21 @@ public class CompassOverlay extends ClientFeature {
     private void handleHudRender(GuiGraphics guiGraphics, float tickDelta) {
         var minecraft = Minecraft.getInstance();
         if (minecraft == null || minecraft.player == null) return;
-
-        // Only render hud if player is holding a compass.
         var player = minecraft.player;
-        if (player.getOffhandItem().getItem() != Items.COMPASS && player.getMainHandItem().getItem() != Items.COMPASS) {
-            return;
+
+        if (!alwaysShow) {
+            // Only render hud if player is holding a compass.
+            if (player.getOffhandItem().getItem() != Items.COMPASS && player.getMainHandItem().getItem() != Items.COMPASS) {
+                return;
+            }
         }
 
         if (CompassOverlay.onlyShowWhenSneaking && !player.isCrouching()) {
             return;
         }
 
-        var y = 40;
+        float y;
+        float lineHeight;
         var gui = minecraft.gui;
         var font = gui.getFont();
         var direction = player.getDirection();
@@ -71,16 +108,26 @@ public class CompassOverlay extends ClientFeature {
         var facing = I18n.get("gui.charm.facing", direction.getName());
         var facingLength = font.width(facing);
         var facingColor = 0xFFEEDD;
-        var midX = gui.screenWidth / 2.0F;
+        float midX;
+
+        if (compactView) {
+            midX = gui.screenWidth / 9.0F;
+            y = 10;
+            lineHeight = 9;
+        } else {
+            midX = gui.screenWidth / 2.0F;
+            y = 40;
+            lineHeight = 12;
+        }
 
         if (CompassOverlay.showFacing) {
             renderText(guiGraphics, font, facing, midX, y, -facingLength / 2, 0, facingColor | alpha);
-            y += 12;
+            y += lineHeight;
         }
 
         if (CompassOverlay.showCoords) {
             renderText(guiGraphics, font, coords, midX, y, -coordsLength / 2, 0, coordsColor | alpha);
-            y += 12;
+            y += lineHeight;
         }
 
         if (CompassOverlay.showBiome) {
@@ -96,12 +143,21 @@ public class CompassOverlay extends ClientFeature {
 
     @SuppressWarnings("SameParameterValue")
     private void renderText(GuiGraphics guiGraphics, Font font, String text, float translateX, float translateY, int offsetX, int offsetY, int color) {
-        guiGraphics.pose().pushPose();
-        guiGraphics.pose().translate(translateX, translateY, 0.0);
+        var pose = guiGraphics.pose();
+
+        pose.pushPose();
+        pose.translate(translateX, translateY, 0.0);
+
+        if (compactView) {
+            var scaleFactor = 0.75f;
+            pose.scale(scaleFactor, scaleFactor, scaleFactor);
+        }
+
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
         guiGraphics.drawString(font, text, offsetX, offsetY, color);
         RenderSystem.disableBlend();
-        guiGraphics.pose().popPose();
+
+        pose.popPose();
     }
 }
