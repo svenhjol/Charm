ae0a68eb636a568242646466e1dbd6ebf552bbb5
Sven H <sven.hjol@protonmail.com>
2024-04-24 00:44:49 +0100

    Remove redundant wrapping methods

--- src/main/java/svenhjol/charm/foundation/helper/TextHelper.java
+++ src/main/java/svenhjol/charm/foundation/helper/TextHelper.java
@@ -2,23 +2,10 @@ package svenhjol.charm.foundation.helper;
 
 import com.google.common.base.CaseFormat;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 
 public final class TextHelper {
     public static Component empty() {
         return Component.empty();
-    }
-
-    public static MutableComponent literal(String string) {
-        return Component.literal(string);
-    }
-
-    public static MutableComponent translatable(String string) {
-        return Component.translatable(string);
-    }
-
-    public static MutableComponent translatable(String string, Object ... objects) {
-        return Component.translatable(string, objects);
     }
 
     public static String snakeToUpperCamel(String string) {
