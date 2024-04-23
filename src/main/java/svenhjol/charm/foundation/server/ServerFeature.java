8e286df6f403790a72ede7e265225300cdd6f92b
Sven H <sven.hjol@protonmail.com>
2024-04-23 22:10:43 +0100

    Placeholder server feature and loader

---
+++ src/main/java/svenhjol/charm/foundation/server/ServerFeature.java
@@ -0,0 +1,6 @@
+package svenhjol.charm.foundation.server;
+
+import svenhjol.charm.foundation.Feature;
+
+public class ServerFeature extends Feature {
+}
---
+++ src/main/java/svenhjol/charm/foundation/server/ServerLoader.java
@@ -0,0 +1,10 @@
+package svenhjol.charm.foundation.server;
+
+import svenhjol.charm.foundation.Loader;
+import svenhjol.charm.foundation.Log;
+
+public class ServerLoader extends Loader<ServerFeature> {
+    protected ServerLoader(String id, Log log) {
+        super(id, log);
+    }
+}
