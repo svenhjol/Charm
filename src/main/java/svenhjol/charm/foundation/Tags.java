3baf8b4d05261953afce9a58113e8ecad2615a45
Sven H <sven.hjol@protonmail.com>
2024-04-26 23:07:56 +0100

    Simplify tag references

--- src/main/java/svenhjol/charm/foundation/Tags.java
+++ src/main/java/svenhjol/charm/foundation/Tags.java
@@ -1,7 +1,6 @@
 package svenhjol.charm.foundation;
 
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import svenhjol.charm.Charm;
@@ -9,23 +8,23 @@ import svenhjol.charm.Charm;
 @SuppressWarnings("unused")
 public class Tags {
     public static final TagKey<Block> BARRELS = TagKey.create(Registries.BLOCK,
-        new ResourceLocation(Charm.ID, "barrels"));
+        Charm.id("barrels"));
 
     public static final TagKey<Block> CHISELED_BOOKSHELVES = TagKey.create(Registries.BLOCK,
-        new ResourceLocation(Charm.ID, "chiseled_bookshelves"));
+        Charm.id("chiseled_bookshelves"));
 
     public static final TagKey<Block> LADDERS = TagKey.create(Registries.BLOCK,
-        new ResourceLocation(Charm.ID, "ladders"));
+        Charm.id("ladders"));
 
     public static final TagKey<Block> MOVING_PISTONS = TagKey.create(Registries.BLOCK,
-            new ResourceLocation(Charm.ID, "moving_pistons"));
+        Charm.id("moving_pistons"));
 
     public static final TagKey<Block> PISTONS = TagKey.create(Registries.BLOCK,
-            new ResourceLocation(Charm.ID, "pistons"));
+        Charm.id("pistons"));
 
     public static final TagKey<Block> PISTON_HEADS = TagKey.create(Registries.BLOCK,
-            new ResourceLocation(Charm.ID, "piston_heads"));
+        Charm.id("piston_heads"));
 
     public static final TagKey<Block> STICKY_PISTONS = TagKey.create(Registries.BLOCK,
-            new ResourceLocation(Charm.ID, "sticky_pistons"));
+        Charm.id("sticky_pistons"));
 }
