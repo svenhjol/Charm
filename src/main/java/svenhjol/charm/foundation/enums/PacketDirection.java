1abed3fe21bd14d989174505ca0384b6d42ce0db
Sven H <sven.hjol@protonmail.com>
2024-05-12 18:03:19 +0000

    Remove deprecated network stubs

--- src/main/java/svenhjol/charm/foundation/enums/PacketDirection.java
+++
@@ -1,6 +0,0 @@
-package svenhjol.charm.foundation.enums;
-
-public enum PacketDirection {
-    CLIENT_TO_SERVER,
-    SERVER_TO_CLIENT
-}
--- src/main/java/svenhjol/charm/foundation/network/Network.java
+++
@@ -1,8 +0,0 @@
-package svenhjol.charm.foundation.network;
-
-import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
-import net.minecraft.world.entity.player.Player;
-
-public interface Network {
-    void send(Player player, CustomPacketPayload payload);
-}
