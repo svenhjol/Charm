2d707f04c21213cb45950bc9037c8d72adad07a6
Sven H <sven.hjol@protonmail.com>
2024-05-12 22:05:53 +0000

    VillagerMixin for fabric to match forge's language key implementation

---
+++ src/main/java/svenhjol/charm/mixin/feature/lumberjacks/VillagerMixin.java
@@ -0,0 +1,33 @@
+package svenhjol.charm.mixin.feature.lumberjacks;
+
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.npc.VillagerData;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import svenhjol.charm.feature.lumberjacks.Lumberjacks;
+import svenhjol.charm.foundation.Resolve;
+
+@Mixin(Villager.class)
+public abstract class VillagerMixin {
+    @Shadow public abstract VillagerData getVillagerData();
+
+    /**
+     * Simple hook to trigger the advancement when a player interacts with a lumberjack.
+     * TODO: make a custom event for villager interaction.
+     */
+    @Inject(
+        method = "startTrading",
+        at = @At("TAIL")
+    )
+    private void hookStartTrading(Player player, CallbackInfo ci) {
+        var lumberjacks = Resolve.feature(Lumberjacks.class);
+        var data = getVillagerData();
+        if (data.getProfession() == lumberjacks.registers.profession.get()) {
+            lumberjacks.advancements.tradedWithLumberjack(player);
+        }
+    }
+}
