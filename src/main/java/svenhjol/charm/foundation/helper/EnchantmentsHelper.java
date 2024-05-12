cef6053eac5795de7c30959933d72dec03a8c062
Sven H <sven.hjol@protonmail.com>
2024-05-12 23:12:02 +0000

    Add helper method to convert an enchantment resourcekey to a holder. Rename Charm's enchantment helper to keep it distinct

--- src/main/java/svenhjol/charm/foundation/helper/EnchantmentsHelper.java
+++ src/main/java/svenhjol/charm/foundation/helper/EnchantmentsHelper.java
@@ -1,16 +1,19 @@
 package svenhjol.charm.foundation.helper;
 
 import net.minecraft.core.Holder;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.ItemEnchantments;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
-public final class EnchantmentHelper {
+public final class EnchantmentsHelper {
     public static boolean hasEnchantment(ItemStack stack, ResourceKey<Enchantment> enchantment) {
         if (stack.has(DataComponents.ENCHANTMENTS)) {
             var enchantments = stack.get(DataComponents.ENCHANTMENTS);
@@ -24,6 +27,17 @@ public final class EnchantmentHelper {
         }
 
         return false;
+    }
+
+    @Nullable
+    public static Holder<Enchantment> holder(RegistryAccess registryAccess, ResourceKey<Enchantment> key) {
+        var registry = registryAccess.registry(Registries.ENCHANTMENT).orElse(null);
+        if (registry == null) return null;
+
+        var resolved = registry.getHolder(key).orElse(null);
+        if (resolved == null) return null;
+
+        return resolved;
     }
 
     public static boolean containsSameEnchantments(ItemEnchantments aa, ItemEnchantments bb) {
