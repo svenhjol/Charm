dbcd774df6ac6f49dd1f32c4514cb524c72ca759
Sven H <sven.hjol@protonmail.com>
2024-05-13 19:32:43 +0000

    RevivePets now allows toggling of loot and XP drop

--- src/main/java/svenhjol/charm/feature/revive_pets/RevivePets.java
+++ src/main/java/svenhjol/charm/feature/revive_pets/RevivePets.java
@@ -3,6 +3,7 @@ package svenhjol.charm.feature.revive_pets;
 import svenhjol.charm.feature.revive_pets.common.Advancements;
 import svenhjol.charm.feature.revive_pets.common.Handlers;
 import svenhjol.charm.feature.revive_pets.common.Registers;
+import svenhjol.charm.foundation.annotation.Configurable;
 import svenhjol.charm.foundation.annotation.Feature;
 import svenhjol.charm.foundation.common.CommonFeature;
 import svenhjol.charm.foundation.common.CommonLoader;
@@ -14,6 +15,22 @@ public final class RevivePets extends CommonFeature {
     public final Advancements advancements;
     public final Handlers handlers;
     public final Registers registers;
+
+    @Configurable(
+        name = "Drop loot on death",
+        description = """
+            If true, named pets drop items from their loot table (such as leather from horses) when they die.
+            This could be unwanted behavior as it can be used to dupe items by killing and resurrecting a pet repeatedly."""
+    )
+    public static final boolean dropLootOnDeath = false;
+
+    @Configurable(
+        name = "Drop experience on death",
+        description = """
+            If true, named pets drop experience when they die. This could be unwanted behavior as it can be used
+            to receive an increasing amount experience by killing and resurrecting a pet repeatedly."""
+    )
+    public static final boolean dropExperienceOnDeath = false;
 
     public RevivePets(CommonLoader loader) {
         super(loader);
