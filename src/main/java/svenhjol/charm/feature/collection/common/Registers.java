976f61a8c56a2d4704160d1d19315de1298dc5a0
Sven H <sven.hjol@protonmail.com>
2024-05-19 22:13:42 +0000

    Update collection enchantment to use the new registry method

--- src/main/java/svenhjol/charm/feature/collection/common/Registers.java
+++ src/main/java/svenhjol/charm/feature/collection/common/Registers.java
@@ -5,7 +5,6 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.enchantment.Enchantment;
 import svenhjol.charm.feature.collection.Collection;
 import svenhjol.charm.foundation.feature.RegisterHolder;
@@ -22,9 +21,9 @@ public final class Registers extends RegisterHolder<Collection> {
 
         enchantment = registry.enchantment("collection");
         attribute = registry.attribute("player.collection", () -> new RangedAttribute(
-            "attribute.name.player.collection", 0.0, 0.0, 1.0).setSyncable(true));
+            "attribute.name.player.charm.collection", 0.0, 0.0, 1.0).setSyncable(true));
 
         // Must attach the collection attribute to the player for it to function.
-        registry.entityAttributes(() -> EntityType.PLAYER, () -> Player.createAttributes().add(attribute.get()));
+        registry.entityAttribute(() -> EntityType.PLAYER, attribute);
     }
 }
