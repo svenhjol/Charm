07c557d68ae79651f3fbfb4c4b02ce59143ed9fa
Sven H <sven.hjol@protonmail.com>
2024-05-19 02:47:02 +0000

    Update endermite powder with new looting level fetch

--- src/main/java/svenhjol/charm/feature/endermite_powder/common/Handlers.java
+++ src/main/java/svenhjol/charm/feature/endermite_powder/common/Handlers.java
@@ -8,18 +8,19 @@ import net.minecraft.world.entity.monster.Endermite;
 import net.minecraft.world.item.ItemStack;
 import svenhjol.charm.feature.endermite_powder.EndermitePowder;
 import svenhjol.charm.foundation.feature.FeatureHolder;
+import svenhjol.charm.foundation.helper.EnchantmentsHelper;
 
 public final class Handlers extends FeatureHolder<EndermitePowder> {
     public Handlers(EndermitePowder feature) {
         super(feature);
     }
 
-    public InteractionResult entityKilledDrop(LivingEntity entity, DamageSource source, int lootingLevel) {
+    public InteractionResult entityKilledDrop(LivingEntity entity, DamageSource source) {
         if (!entity.level().isClientSide() && entity instanceof Endermite) {
             var random = entity.getRandom();
             var level = entity.getCommandSenderWorld();
             var pos = entity.blockPosition();
-            var amount = random.nextInt(2) + random.nextInt(lootingLevel + 1);
+            var amount = random.nextInt(2 + EnchantmentsHelper.lootingLevel(source));
             level.addFreshEntity(new ItemEntity(level, pos.getX(), pos.getY(), pos.getZ(),
                 new ItemStack(feature().registers.item.get(), amount)));
         }
