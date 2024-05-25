66d345763278e9a0b189bc1c03aaa08eedab1370
Sven H <sven.hjol@protonmail.com>
2024-05-25 17:13:15 +0000

    Update feature description and tag strings

--- src/main/java/svenhjol/charm/feature/piglin_pointing/PiglinPointing.java
+++ src/main/java/svenhjol/charm/feature/piglin_pointing/PiglinPointing.java
@@ -9,7 +9,8 @@ import svenhjol.charm.foundation.common.CommonLoader;
 
 @Feature(description = """
     Piglins turn and point in the rough direction of a nether fortress or bastion remnant.
-    By default, give them a chiseled nether brick block for a fortress and a gilded blackstone block for a bastion.""")
+    By default, give them a chiseled nether brick block for a fortress and a gilded blackstone block for a bastion.
+    The item tags 'piglin_barters_for_bastions' and 'piglin_barters_for_fortresses' can be used to configure the blocks.""")
 public class PiglinPointing extends CommonFeature {
     public final Registers registers;
     public final Handlers handlers;
