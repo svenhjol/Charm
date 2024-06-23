50c562824fc2bb0a355b9ac0b2fbf0b1e804c2aa
Sven H <sven.hjol@protonmail.com>
2024-06-23 19:03:33 +0000

    Additional checks for background music suppression while music discs are playing

--- src/main/java/svenhjol/charm/feature/discs_stop_background_music/client/Handlers.java
+++ src/main/java/svenhjol/charm/feature/discs_stop_background_music/client/Handlers.java
@@ -10,15 +10,14 @@ import net.minecraft.core.component.DataComponents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.JukeboxBlock;
 import net.minecraft.world.phys.BlockHitResult;
-import svenhjol.charm.feature.discs_stop_background_music.DiscsStopBackgroundMusic;
 import svenhjol.charm.charmony.feature.FeatureHolder;
+import svenhjol.charm.feature.discs_stop_background_music.DiscsStopBackgroundMusic;
 
 @SuppressWarnings("unused")
 public final class Handlers extends FeatureHolder<DiscsStopBackgroundMusic> {
@@ -31,43 +30,48 @@ public final class Handlers extends FeatureHolder<DiscsStopBackgroundMusic> {
     }
 
     public void clientTick(Minecraft minecraft) {
-        checkActuallyStopMusic();
+        tryStopMusic();
     }
 
     public void soundPlay(SoundEngine soundEngine, SoundInstance soundInstance) {
-        checkShouldStopMusic(soundInstance);
+        checkMusicToStop(soundInstance);
     }
 
     public InteractionResult blockUse(Player player, Level level, InteractionHand hand, BlockHitResult hitResult) {
-        stopRecord(player, hitResult.getBlockPos(), player.getItemInHand(hand));
+        checkMusicToStop(level, hitResult.getBlockPos(), player.getItemInHand(hand));
         return InteractionResult.PASS;
     }
 
-    public void stopRecord(Entity entity, BlockPos pos, ItemStack stack) {
-        if (entity.level().isClientSide
-            && entity instanceof Player
-            && stack.has(DataComponents.JUKEBOX_PLAYABLE)
-        ) {
-            var state = entity.level().getBlockState(pos);
+    public void checkMusicToStop(Level level, BlockPos pos, ItemStack stack) {
+        if (level.isClientSide() && stack.has(DataComponents.JUKEBOX_PLAYABLE)) {
+            var state = level.getBlockState(pos);
             if (state.getBlock() == Blocks.JUKEBOX && !state.getValue(JukeboxBlock.HAS_RECORD)) {
                 getSoundManager().stop(null, SoundSource.MUSIC);
             }
         }
     }
 
-    public void checkShouldStopMusic(SoundInstance sound) {
+    public void checkMusicToStop(SoundInstance sound) {
         if (sound.getSource() == SoundSource.MUSIC) {
             // check if there are any records playing
             getPlayingSounds().forEach((category, s) -> {
                 if (category == SoundSource.RECORDS) {
                     musicToStop = sound;
-                    feature().log().debug("Triggered background music while music disc playing");
+                    feature().log().debug("Sound source check music: Background music suppressed");
+                }
+            });
+        }
+        if (sound.getSource() == SoundSource.RECORDS) {
+            getPlayingSounds().forEach((category, s) -> {
+                if (category == SoundSource.MUSIC) {
+                    musicToStop = s;
+                    feature().log().debug("Sound source check record: Background music suppressed");
                 }
             });
         }
     }
 
-    public void checkActuallyStopMusic() {
+    public void tryStopMusic() {
         if (musicToStop != null && ++ticksBeforeStop % CHECK_TICKS == 0) {
             getSoundManager().stop(musicToStop);
             ticksBeforeStop = 0;
